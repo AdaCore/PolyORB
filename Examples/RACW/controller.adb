19fe32d8776838810353cde52862ba57f22034ca
Samuel Tardieu <sam@inf.enst.fr>
1996-10-28 13:42:02 +0000

    Updated headers.
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 45906

--- Examples/RACW/controller.adb
+++ Examples/RACW/controller.adb
@@ -1,3 +1,31 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE EXAMPLES                                --
+--                                                                          --
+--                          C O N T R O L L E R                             --
+--                                                                          --
+--                                B o d y                                   --
+--                                                                          --
+--                           $Revision$                              --
+--                                                                          --
+--           Copyright (C) 1996 Free Software Foundation, Inc.              --
+--                                                                          --
+-- GLADE  is  free software;  you  can redistribute  it  and/or  modify  it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 2,  or  (at your option) any later --
+-- version.  GLADE  is  distributed in the hope that it will be useful, but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or FITNESS  FOR A PARTICULAR PURPOSE.  See the  GNU General  Public --
+-- License  for more details.  You should  have received a copy of the  GNU --
+-- General  Public  License  distributed with GLADE;  see file COPYING.  If --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+--                GLADE is maintained by ACT Europe.                        --
+--            (email:distribution@act-europe.gnat.com).                     --
+--                                                                          --
+------------------------------------------------------------------------------
+
 with Ada.Text_IO;
 with Ada.Integer_Text_IO;
 package body Controller is
