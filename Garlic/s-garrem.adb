547973b1464e676dfa602b0bc484c6653eba6103
Laurent Pautet <pautet@adacore.com>
1996-10-29 18:54:25 +0000

    Back with text_io :-((
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 45924

--- Garlic/s-garrem.adb
+++ Garlic/s-garrem.adb
@@ -33,7 +33,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-with System.IO;
+with Ada.Text_IO;
 with Interfaces.C.Strings;
 with System.Garlic.Constants; use System.Garlic.Constants;
 with System.Garlic.Thin; use System.Garlic.Thin;
@@ -145,8 +145,8 @@ package body System.Garlic.Remote is
       Buffer : String (1 .. 64);
       Last   : Natural;
    begin
-      System.IO.Put ("Host for """ & Partition & """: ");
-      System.IO.Get_Line (Buffer, Last);
+      Ada.Text_IO.Put ("Host for """ & Partition & """: ");
+      Ada.Text_IO.Get_Line (Buffer, Last);
       return Buffer (1 .. Last);
    end Get_Host;
 
