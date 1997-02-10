ab52b3a1cad6e25cff7977fb945c662def4cbb64
Samuel Tardieu <sam@inf.enst.fr>
1997-02-10 19:18:29 +0000

    * 2xgtplsp.ads: New (untested) SNI version of this package.
    * 2wgtplsp.ads: New (untested) Win32 version of this package.
    
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46090

---
+++ Garlic/2wgtplsp.ads
@@ -0,0 +1,48 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE COMPONENTS                              --
+--                                                                          --
+--  S Y S T E M . G A R L I C . T C P . P L A T F O R M _ S P E C I F I C   --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--                            $Revision$                             --
+--                                                                          --
+--         Copyright (C) 1996,1997 Free Software Foundation, Inc.           --
+--                                                                          --
+-- GARLIC is free software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 2,  or (at your option)  any later ver- --
+-- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License  distributed with GARLIC;  see file COPYING.  If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--               GLADE  is maintained by ACT Europe.                        --
+--               (email: glade-report@act-europe.fr)                        --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with System.Garlic.Thin;
+
+private package System.Garlic.TCP.Platform_Specific is
+
+   --  Windows NT version of this package.
+
+   package Net renames System.Garlic.Thin;
+
+private
+
+   pragma Linker_Options ("-lwsock32");
+
+end System.Garlic.TCP.Platform_Specific;
