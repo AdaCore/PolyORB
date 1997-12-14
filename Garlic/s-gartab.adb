2e5155ec6cc2d635743b79f347d9c18877047443
Samuel Tardieu <sam@inf.enst.fr>
1997-12-14 15:30:57 +0000

    Added standard GLADE headers
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46584

--- Garlic/s-gartab.adb
+++ Garlic/s-gartab.adb
@@ -1,3 +1,38 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE COMPONENTS                              --
+--                                                                          --
+--                  S Y S T E M . G A R L I C . T A B L E                   --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                            $Revision$                             --
+--                                                                          --
+--         Copyright (C) 1996,1997 Free Software Foundation, Inc.           --
+--                                                                          --
+-- GARLIC is free software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 2,  or (at your option)  any later ver- --
+-- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License  distributed with GARLIC;  see file COPYING.  If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--               GLADE  is maintained by ACT Europe.                        --
+--               (email: glade-report@act-europe.fr)                        --
+--                                                                          --
+------------------------------------------------------------------------------
+
 with Ada.Unchecked_Deallocation;
 with System.Garlic.Utils;      use System.Garlic.Utils;
 with System.Garlic.Table;
@@ -151,6 +186,27 @@ package body System.Garlic.Table is
 
          Enter;
          Sequential.Set (N, (Sequential.Get (N).Name, C));
+         Leave;
+      end Set;
+
+      ---------
+      -- Set --
+      ---------
+
+      procedure Set (N : Index_Type; S : String) is
+         Name : Name_Id;
+         Data : Fat_Component_Type;
+      begin
+         pragma Abort_Defer;
+
+         Enter;
+         Name := Get (S);
+         Set_Info (Name, Integer (Index_Type'Pos (N)));
+
+         Data := Sequential.Get (N);
+         Data.Name := Name;
+
+         Sequential.Set (N, Data);
          Leave;
       end Set;
 
