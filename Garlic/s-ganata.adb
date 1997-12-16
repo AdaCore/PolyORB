77a9f43ffd131e73294847abac0130849554f7cf
Samuel Tardieu <sam@inf.enst.fr>
1997-12-16 10:22:08 +0000

    Qualify parameters of instantiation Nodes. Remove useless use clause.
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46587

--- Garlic/s-ganata.adb
+++ Garlic/s-ganata.adb
@@ -39,10 +39,8 @@ with System.Garlic.Table;
 
 package body System.Garlic.Name_Table is
 
-   use Ascii;
-
    Size  : constant := 2 ** 9;
-   --  Size of actual string names table.
+   --  Size of actual string names table
 
    Min   : constant Natural := 1;
    Max   : Natural := Size;
@@ -51,12 +49,12 @@ package body System.Garlic.Name_Table is
    Table : String_Access := new String (Min .. Max);
 
    Hash_Max : constant Name_Id := 2 ** 8;
-   --  Indexes in the hash header table run from 0 to Hash_Max.
+   --  Indexes in the hash header table run from 0 to Hash_Max
 
    subtype Hash_Id is Name_Id range 0 .. Hash_Max;
-   --  Range of hash index values.
+   --  Range of hash index values
 
-   Hash_Nodes : array (1 .. Hash_Max) of Name_Id;
+   Hash_Nodes : array (1 .. Hash_Max) of Name_Id := (others => Null_Name);
    --  The hash table is used to locate existing entries in the names table.
    --  The entries point to the first names table entry whose hash value
    --  matches the hash code. Then subsequent names table entries with the
@@ -66,30 +64,37 @@ package body System.Garlic.Name_Table is
    --  Return an hash entry which corresponds to the first entry in the
    --  hash table.
 
-   type Node_Type is
-      record
-         First  : Name_Id;
-         --  Starting location of characters in Table.
+   type Node_Type is record
+      First  : Name_Id;
+      --  Starting location of characters in Table
 
-         Length : Natural;
-         --  Length of this name in characters.
+      Length : Natural;
+      --  Length of this name in characters
 
-         Info   : Integer;
-         --  Int Value associated with this name.
+      Info   : Integer;
+      --  Int Value associated with this name
 
-         Next   : Name_Id;
-         --  Next entry in names table for same hash Code.
+      Next   : Name_Id;
+      --  Next entry in names table for same hash Code
 
-      end record;
-   Null_Node : constant Node_Type := (Null_Name, 0, 0, Null_Name);
+   end record;
 
-   --  This is the table that is referenced by Name_Id entries.
-   --  It contains one entry for each unique name in the table.
+   Null_Node : constant Node_Type :=
+     (First  => Null_Name,
+      Length => 0,
+      Info   => 0,
+      Next   => Null_Name);
 
-   package Nodes is new System.Garlic.Table.Sequential
-     (Name_Id, Hash_Max, Hash_Max, Node_Type, Null_Node);
-
-   procedure Reallocate;
+   package Nodes is new System.Garlic.Table.Simple
+     (Index_Type     => Name_Id,
+      Null_Index     => Null_Name,
+      First_Index    => First_Name,
+      Initial_Size   => Hash_Max,
+      Increment_Size => Hash_Max,
+      Component_Type => Node_Type,
+      Null_Component => Null_Node);
+   --  This is the table that is referenced by Name_Id entries. It
+   --  contains one entry for each unique name in the table.
 
    ----------
    -- Hash --
@@ -98,7 +103,6 @@ package body System.Garlic.Name_Table is
    function Hash (Name : String) return Hash_Id is
       type T is mod 2 ** 8;
       X : T := 0;
-
    begin
       for J in Name'Range loop
          X := 2 * X + Character'Pos (Name (J));
@@ -112,84 +116,58 @@ package body System.Garlic.Name_Table is
    ---------
 
    function Get (S : String) return Name_Id is
-      Len  : constant Natural := S'Length;
-      HId  : constant Hash_Id := Hash (S);
-      Id   : Name_Id;
-      Node : Node_Type;
-
-      Found : Boolean;
-      --  The string name is already there.
-
+      L : constant Natural := S'Length;
+      H : constant Hash_Id := Hash (S);
+      I : Name_Id;
+      N : Node_Type;
    begin
+      --  Set to the first entry whose hash value matches S hash code
 
-      --  Handles the case where the hash entry is still empty.
+      I := Hash_Nodes (H);
 
-      if Hash_Nodes (HId) = Null_Name then
-         Id := Nodes.Allocate;
-         Hash_Nodes (HId) := Id;
+      while I /= Null_Name loop
+         N := Nodes.Table (I);
+
+         --  Check whether the string of this entry matches with S
+
+         if N.Length = L and then Table (N.First .. N.First + L - 1) = S then
+            return I;
+         end if;
+
+         I := N.Next;
+      end loop;
+
+      --  Check whether the table is large enough or not
+
+      if Max < Last + L then
+         declare
+            Old : String_Access := Table;
+         begin
+            Max := Max + Size;
+            Table := new String (Min .. Max);
+            for Index in Min .. Last loop
+               Table (Index) := Old (Index);
+            end loop;
+            for Index in Last + 1 .. Max loop
+               Table (Index) := Ascii.Nul;
+            end loop;
+            Free (Old);
+         end;
       end if;
 
-      --  Set to the first entry whose hash value matches S hash code.
+      --  Enter the name in the first entry whose hash code matches S
 
-      Id   := Hash_Nodes (HId);
-      Node := Nodes.Get (Id);
+      I := Nodes.Allocate;
 
-      if Node.Length = 0 then
-         Found := False;
+      Nodes.Table (I).Next   := Hash_Nodes (H);
+      Hash_Nodes (H)         := I;
 
-      else
-         loop
+      Nodes.Table (I).Length := L;
+      Nodes.Table (I).First  := Last + 1;
+      Table (Last + 1 .. Last + L) := S;
+      Last := Last + L;
 
-            --  Check whether the string of this entry matches with S.
-
-            if Node.Length = Len then
-               Found := True;
-
-               for I in 0 .. Len - 1 loop
-                  if S (S'First + I) /= Table (Node.First + I) then
-                     Found := False;
-                     exit;
-                  end if;
-               end loop;
-
-               exit when Found;
-            end if;
-
-            --  When no more entry available, create a new one.
-
-            if Node.Next = Null_Name then
-               Found := False;
-
-               Id        := Nodes.Allocate;
-               Node.Next := Id;
-               Node      := Nodes.Get (Id);
-               exit;
-            end if;
-
-            --  Try with next entry.
-
-            Id   := Node.Next;
-            Node := Nodes.Get (Id);
-
-         end loop;
-      end if;
-
-      --  Initialize the new entry.
-
-      if not Found then
-         Node.Next   := Null_Name;
-         Node.Length := Len;
-         Node.First  := Last + 1;
-         Last        := Last + Len;
-
-         for I in 0 .. Len - 1 loop
-            Table (Node.First + I) := S (S'First + I);
-         end loop;
-
-         Nodes.Set (Id, Node);
-      end if;
-
-      return Id;
+      return I;
    end Get;
 
    ---------
@@ -197,20 +175,11 @@ package body System.Garlic.Name_Table is
    ---------
 
    function Get (N : Name_Id) return String is
-      Node : Node_Type;
+      F : constant Natural := Nodes.Table (N).First;
+      L : constant Natural := Nodes.Table (N).Length;
 
    begin
-      Node := Nodes.Get (N);
-      declare
-         S : String (1 .. Node.Length);
-         F : Natural := Node.First - 1;
-      begin
-         for I in S'Range loop
-            S (I) := Table (F + I);
-         end loop;
-
-         return S;
-      end;
+      return Table (F .. F + L - 1);
    end Get;
 
    --------------
@@ -219,38 +188,16 @@ package body System.Garlic.Name_Table is
 
    function Get_Info (N : Name_Id) return Integer is
    begin
-      return Nodes.Get (N).Info;
+      return Nodes.Table (N).Info;
    end Get_Info;
-
-   ----------------
-   -- Reallocate --
-   ----------------
-
-   procedure Reallocate is
-      New_Max   : Name_Id       := Max + Size;
-      New_Table : String_Access := new String (Min .. New_Max);
-
-      begin
-         for Index in Min .. Last loop
-            New_Table (Index) := Table (Index);
-         end loop;
-         for Index in Last + 1 .. Max loop
-            New_Table (Index) := Ascii.Nul;
-         end loop;
-         Free (Table);
-         Table := New_Table;
-      end Reallocate;
 
    --------------
    -- Set_Info --
    --------------
 
    procedure Set_Info (N : Name_Id; I : Integer) is
-      Node : Node_Type;
    begin
-      Node := Nodes.Get (N);
-      Node.Info := I;
-      Nodes.Set (N, Node);
+      Nodes.Table (N).Info := I;
    end Set_Info;
 
 end System.Garlic.Name_Table;
