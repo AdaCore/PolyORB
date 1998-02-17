a636e48dac91a618e2cadde1bbbb19ba2d926aa9
Laurent Pautet <pautet@adacore.com>
1998-02-17 23:52:18 +0000

    Use all the features of filters
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46668

--- Examples/Filters/s-gafish.adb
+++ Examples/Filters/s-gafish.adb
@@ -1,93 +1,103 @@
 with Ada.Streams; use Ada.Streams;
+with Ada.Numerics.Discrete_Random;
 
+with System.Garlic.Debug;   use System.Garlic.Debug;
+with System.Garlic.Heart;   use System.Garlic.Heart;
 with System.Garlic.Filters;
 pragma Elaborate (System.Garlic.Filters);
-
-with System.Garlic.Utils;   use System.Garlic.Utils;
 with System.Garlic.Streams; use System.Garlic.Streams;
 
-package body System.Garlic.Filters.Shuffling is
+package body System.Garlic.Filters.Shift is
 
-   Dummy : aliased No_Filter;
-   --  Dummy instance of No_Filter, only used for dispatching
+   Private_Debug_Key : constant Debug_Key :=
+     Debug_Initialize ("SHIFT", "(s-gafish): ");
 
-   Dummy_Params : aliased No_Filter_Params;
-   --  Dummy filter parameters, allowing the use of this filter even as
-   --  the default filter.
+   procedure D
+     (Level   : in Debug_Level;
+      Message : in String;
+      Key     : in Debug_Key := Private_Debug_Key)
+     renames Print_Debug_Info;
 
-   Dummy_Name : constant String := "shuffling";
+   package Shift_Filter_Params is
+     new Ada.Numerics.Discrete_Random (Stream_Element);
 
-   ---------------------
-   -- Filter_Outgoing --
-   ---------------------
+   Generator : Shift_Filter_Params.Generator;
 
-   function Filter_Outgoing
-     (Filter : in     No_Filter;
-      Params : in     Filter_Params_Access;
-      Stream : access System.RPC.Params_Stream_Type)
-      return Stream_Element_Access is
-      Result  : Stream_Element_Access  := To_Stream_Element_Access (Stream);
-      Middle  : Stream_Element_Offset := (Result'Last + Result'First) / 2;
-      Element : Stream_Element;
-   begin
-      for I in Result'First .. Middle loop
-         Element := Result (Result'Last + Result'First - I);
-         Result (Result'Last + Result'First - I) := Result (I);
-         Result (I) := Element;
-      end loop;
-      return Result;
-   end Filter_Outgoing;
+   Shift_Filter : aliased Shift_Filter_Type;
 
    ---------------------
    -- Filter_Incoming --
    ---------------------
 
    function Filter_Incoming
-     (Filter : in No_Filter;
+     (Filter : in Shift_Filter_Type;
       Params : in Filter_Params_Access;
       Stream : in Ada.Streams.Stream_Element_Array)
       return Stream_Element_Access is
-      Result  : Stream_Element_Access := new Stream_Element_Array'(Stream);
-      Middle  : Stream_Element_Offset := (Result'Last + Result'First) / 2;
-      Element : Stream_Element;
+      Shift_Params  : Shift_Filter_Params_Type;
+      Target_Buffer : Stream_Element_Access;
+
    begin
-      for I in Result'First .. Middle loop
-         Element := Result (Result'Last + Result'First - I);
-         Result (Result'Last + Result'First - I) := Result (I);
-         Result (I) := Element;
+      D (D_Debug, "Entering shift incoming filter");
+
+      Shift_Params  := Shift_Filter_Params_Type (Params.all);
+      D (D_Debug, "Use shift params" & Shift_Params.Times'Img);
+
+      Target_Buffer := new Stream_Element_Array'(Stream);
+      Dump (D_Debug, Target_Buffer.all, Private_Debug_Key);
+
+      for I in Target_Buffer'Range loop
+         Target_Buffer (I) := Target_Buffer (I) xor Shift_Params.Times;
       end loop;
-      return Result;
+
+      D (D_Debug, "Leaving shift incoming filter");
+      Dump (D_Debug, Target_Buffer.all, Private_Debug_Key);
+
+      return Target_Buffer;
    end Filter_Incoming;
 
    ---------------------
-   -- Generate_Params --
+   -- Filter_Outgoing --
    ---------------------
 
-   procedure Generate_Params
-     (Filter : in No_Filter;
-      Params : out Filter_Params_Access;
-      Private_Params : out Filter_Params_Access;
-      Needs_Key_Exchange : out Boolean) is
+   function Filter_Outgoing
+     (Filter : in     Shift_Filter_Type;
+      Params : in     Filter_Params_Access;
+      Stream : access System.RPC.Params_Stream_Type)
+      return Stream_Element_Access is
+      Shift_Params  : Shift_Filter_Params_Type;
+      Target_Buffer : Stream_Element_Access;
+
    begin
-      Params := Dummy_Params'Access;
-      Private_Params := Dummy_Params'Access;
-      Needs_Key_Exchange := false;
-   end Generate_Params;
+      D (D_Debug, "Entering shift outgoing filter");
+
+      Shift_Params  := Shift_Filter_Params_Type (Params.all);
+      D (D_Debug, "Use shift params" & Shift_Params.Times'Img);
+
+      Target_Buffer := To_Stream_Element_Access (Stream);
+      Dump (D_Debug, Target_Buffer.all, Private_Debug_Key);
+
+      for I in Target_Buffer'Range loop
+         Target_Buffer (I) := Target_Buffer (I) xor Shift_Params.Times;
+      end loop;
+
+      D (D_Debug, "Leaving shift outgoing filter");
+      Dump (D_Debug, Target_Buffer.all, Private_Debug_Key);
+
+      return Target_Buffer;
+   end Filter_Outgoing;
 
    ------------------------
    -- Filter_Params_Read --
    ------------------------
 
    function Filter_Params_Read
-     (Filter : No_Filter;
+     (Filter : Shift_Filter_Type;
       Stream : Ada.Streams.Stream_Element_Array)
       return Filter_Params_Access is
-      S : aliased System.RPC.Params_Stream_Type (Stream'Length);
-      P : No_Filter_Params;
    begin
-      To_Params_Stream_Type (Stream, S'Access);
-      No_Filter_Params'Read (S'Access, P);
-      return new No_Filter_Params'(P);
+      D (D_Debug, "Read filter params" & Stream (Stream'First)'Img);
+      return new Shift_Filter_Params_Type'(Times => Stream (Stream'First));
    end Filter_Params_Read;
 
    -------------------------
@@ -95,36 +105,38 @@ package body System.Garlic.Filters.Shuffling is
    -------------------------
 
    function Filter_Params_Write
-     (Filter : No_Filter;
-      P : Filter_Params_Access) return
+     (Filter : Shift_Filter_Type;
+      Params : Filter_Params_Access) return
       Streams.Stream_Element_Access is
-      S : aliased System.RPC.Params_Stream_Type (32);
+      Shift_Filter_Params : Shift_Filter_Params_Type;
+
    begin
-      No_Filter_Params'Write (S'Access, No_Filter_Params (P.all));
-      return To_Stream_Element_Access (S'Access);
+      Shift_Filter_Params := Shift_Filter_Params_Type (Params.all);
+      D (D_Debug, "Write filter params" & Shift_Filter_Params.Times'Img);
+      return new Stream_Element_Array'(1 .. 1 => Shift_Filter_Params.Times);
    end Filter_Params_Write;
 
-   --------------
-   -- Get_Name --
-   --------------
+   ---------------------
+   -- Generate_Params --
+   ---------------------
 
-   function Get_Name
-     (Filter : No_Filter)
-      return String is
+   procedure Generate_Params
+     (Filter          : in Shift_Filter_Type;
+      Public_Params   : out Filter_Params_Access;
+      Private_Params  : out Filter_Params_Access;
+      Exchange_Params : out Boolean) is
+      Element : Stream_Element;
+
    begin
-      return Dummy_Name;
-   end Get_Name;
-
-   ------------------
-   -- Print_Params --
-   ------------------
-
-   procedure Print_Params (Params : No_Filter_Params) is
-   begin
-      null;
-   end Print_Params;
-
+      for I in 1 .. Get_My_Partition_ID loop
+         Element := Shift_Filter_Params.Random (Generator);
+      end loop;
+      D (D_Debug, "Generate shift params" & Element'Img);
+      Public_Params   := new Shift_Filter_Params_Type'(Times => Element);
+      Private_Params  := new Shift_Filter_Params_Type'(Times => Element);
+      Exchange_Params := True;
+   end Generate_Params;
 
 begin
-   Register_Filter (Dummy'Access);
-end System.Garlic.Filters.Shuffling;
+   Register_Filter (Shift_Filter'Access, "shift");
+end System.Garlic.Filters.Shift;
--- Examples/Filters/s-gafish.ads
+++ Examples/Filters/s-gafish.ads
@@ -1,46 +1,43 @@
 with Ada.Streams;
 with System.Garlic.Streams;
 
-package System.Garlic.Filters.Shuffling is
+package System.Garlic.Filters.Shift is
 
 private
 
-   type No_Filter is new Filter_Type with null record;
+   type Shift_Filter_Type is new Filter_Type with null record;
 
-   type No_Filter_Params is new Filter_Params with null record;
-
-   function Filter_Outgoing
-     (Filter : in     No_Filter;
-      Params : in     Filter_Params_Access;
-      Stream : access System.RPC.Params_Stream_Type)
-      return Streams.Stream_Element_Access;
+   type Shift_Filter_Params_Type is new Filter_Params_Type with
+     record
+        Times : Ada.Streams.Stream_Element;
+     end record;
 
    function Filter_Incoming
-     (Filter : in No_Filter;
+     (Filter : in Shift_Filter_Type;
       Params : in Filter_Params_Access;
       Stream : in Ada.Streams.Stream_Element_Array)
       return Streams.Stream_Element_Access;
 
-   procedure Generate_Params
-     (Filter : in No_Filter;
-      Params : out Filter_Params_Access;
-      Private_Params : out Filter_Params_Access;
-      Needs_Key_Exchange : out Boolean);
+   function Filter_Outgoing
+     (Filter : in     Shift_Filter_Type;
+      Params : in     Filter_Params_Access;
+      Stream : access System.RPC.Params_Stream_Type)
+      return Streams.Stream_Element_Access;
 
    function Filter_Params_Read
-     (Filter : No_Filter;
+     (Filter : Shift_Filter_Type;
       Stream : Ada.Streams.Stream_Element_Array)
       return Filter_Params_Access;
 
    function Filter_Params_Write
-     (Filter : No_Filter;
-      P : Filter_Params_Access)
+     (Filter : Shift_Filter_Type;
+      Params : Filter_Params_Access)
       return Streams.Stream_Element_Access;
 
-   function Get_Name
-     (Filter : No_Filter)
-     return String;
+   procedure Generate_Params
+     (Filter          : in  Shift_Filter_Type;
+      Public_Params   : out Filter_Params_Access;
+      Private_Params  : out Filter_Params_Access;
+      Exchange_Params : out Boolean);
 
-   procedure Print_Params (Params : No_Filter_Params);
-
-end System.Garlic.Filters.Shuffling;
+end System.Garlic.Filters.Shift;
