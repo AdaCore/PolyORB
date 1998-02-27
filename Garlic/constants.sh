144dbe49c2a27664d43c92a68607e7d745353bd0
Samuel Tardieu <sam@inf.enst.fr>
1998-02-27 09:33:48 +0000

    Fix stupid typo introduced by last checkin
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46696

--- Garlic/constants.sh
+++ Garlic/constants.sh
@@ -12,7 +12,7 @@ fname=`gnatkr ${name}`.ads
 
 # List of include files to look for (from /usr/include)
 
-incfiles="stdio.h sys/types.h sys/socoket.h errno.h netdb.h netinet/in.h signal.h fcntl.h termio.h termios.h sys/file.h sys/ioctl.h netinet/tcp.h sys/systeminfo.h poll.h stropts.h sys/conf.h"
+incfiles="stdio.h sys/types.h sys/socket.h errno.h netdb.h netinet/in.h signal.h fcntl.h termio.h termios.h sys/file.h sys/ioctl.h netinet/tcp.h sys/systeminfo.h poll.h stropts.h sys/conf.h"
 
 # List of constants we need to know
 
