34af2d1f488280712203f207f9609f9761d6705a
Samuel Tardieu <sam@inf.enst.fr>
1998-04-05 13:04:56 +0000

    Use new version from Pascal Obry
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46731

--- Garlic/5wgarcon.ads
+++ Garlic/5wgarcon.ads
@@ -33,44 +33,23 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  This package has been generated automatically on:
---  CYGWIN32/95 COBRA 4.0 17.4 i6395286
---  Generation date: Tue Feb 11 12:30:34 GMT--1:00 1997
---  Any change you make here is likely to be lost !
+--  This package is for a MINGW32 NT environment.
+
 package System.Garlic.Constants is
-   Tcp_Nodelay : constant := 1;
-   Af_Inet : constant := 2;
-   Sock_Stream : constant := 1;
-   Sock_Dgram : constant := 2;
-   Eintr : constant := 4;
-   Eagain : constant := 11;
-   Ewouldblock : constant := 11;
-   Einprogress : constant := -1;
-   Ealready : constant := -1;
-   Eisconn : constant := -1;
-   Econnrefused : constant := 111;
-   Fndelay : constant := 4;
-   Fasync : constant := 64;
-   F_Getfl : constant := 3;
-   F_Setfl : constant := 4;
-   F_Setown : constant := 6;
-   So_Rcvbuf : constant := -1;
-   So_Reuseaddr : constant := -1;
-   Sol_Socket : constant := -1;
-   Sigterm : constant := 6;
-   Sigkill : constant := 2;
-   O_Rdonly : constant := 0;
-   O_Wronly : constant := 1;
-   O_Rdwr : constant := 2;
-   Host_Not_Found : constant := 1;
-   Try_Again : constant := 2;
-   No_Recovery : constant := 3;
-   No_Data : constant := 4;
-   No_Address : constant := 4;
-   Pollin : constant := -1;
-   Pollpri : constant := -1;
-   Pollout : constant := -1;
-   Pollerr : constant := -1;
-   Pollhup : constant := -1;
-   Pollnval : constant := -1;
+
+   Af_Inet        : constant := 2;
+   Sock_Dgram     : constant := 2;
+   Sock_Stream    : constant := 1;
+   So_Reuseaddr   : constant := 4;
+   Sol_Socket     : constant := 16#ffff#;
+   Host_Not_Found : constant := 11001;
+   Try_Again      : constant := 11002;
+   No_Recovery    : constant := 11003;
+   No_Data        : constant := 11004;
+   No_Address     : constant := 11004;
+
+   O_Rdonly       : constant := 0;
+   O_Wronly       : constant := 1;
+   O_Rdwr         : constant := 2;
+
 end System.Garlic.Constants;
