9d9e273f87205798f69347c08139c00ac8fd41de
Samuel Tardieu <sam@inf.enst.fr>
1998-06-15 14:05:19 +0000

    Use new method that works in all the cases to compute constants
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46833

--- Garlic/constants.sh
+++ Garlic/constants.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# 1.7
+# $Id$
 #
 # This file generates any necessary constant for a given platform
 #
@@ -8,15 +8,19 @@
 # Name of package
 
 name=$1
+srcdir=$2
 fname=`gnatkr ${name}`.ads
-
-# List of include files to look for (from /usr/include)
-
-incfiles="stdio.h sys/types.h sys/socket.h errno.h netdb.h netinet/in.h signal.h fcntl.h termio.h termios.h sys/file.h sys/ioctl.h netinet/tcp.h sys/systeminfo.h poll.h stropts.h sys/conf.h"
 
 # List of constants we need to know
 
-constants="TCP_NODELAY AF_INET SOCK_STREAM SOCK_DGRAM EINTR EAGAIN EWOULDBLOCK EINPROGRESS EALREADY EISCONN ECONNREFUSED FNDELAY FASYNC-FIOASYNC F_GETFL F_SETFL F_SETOWN-FIOSSAIOOWN SO_RCVBUF SO_REUSEADDR SOL_SOCKET SIGTERM SIGKILL O_RDONLY O_WRONLY O_RDWR HOST_NOT_FOUND TRY_AGAIN NO_RECOVERY NO_DATA NO_ADDRESS POLLIN POLLPRI POLLOUT POLLERR POLLHUP POLLNVAL I_SETSIG S_RDNORM S_WRNORM"
+constants="TCP_NODELAY AF_INET SOCK_STREAM SOCK_DGRAM EINTR EAGAIN"
+constants="${constants} EWOULDBLOCK EINPROGRESS EALREADY EISCONN"
+constants="${constants} ECONNREFUSED FNDELAY FASYNC-FIOASYNC F_GETFL F_SETFL"
+constants="${constants} F_SETOWN-FIOSSAIOOWN SO_RCVBUF SO_REUSEADDR"
+constants="${constants} SOL_SOCKET SIGTERM SIGKILL O_RDONLY O_WRONLY"
+constants="${constants} O_RDWR HOST_NOT_FOUND TRY_AGAIN NO_RECOVERY"
+constants="${constants} NO_DATA NO_ADDRESS POLLIN POLLPRI POLLOUT POLLERR"
+constants="${constants} POLLHUP POLLNVAL I_SETSIG S_RDNORM S_WRNORM"
 
 # Debug
 
@@ -24,80 +28,11 @@ debug=$1
 
 # Look for any header file found
 
-tmph=./tmph$$.c
 tmpe=./tmpe$$
-trap "rm -f ${tmpe} ${tmph}" 0 1 2 3 15
-for i in ${incfiles}; do
-  if [ -f "/usr/include/$i" ]; then
-    echo "#include <$i>" >> ${tmph}
-  fi
-done
+trap "rm -f ${tmpe}" 0 1 2 3 15
 
-# Generate a list of constants
+# Header of generated file
 
-cat >> ${tmph} << EOF
-struct cons {
-  char *name;
-  int value;
-} list[] = {
-EOF
-for i in ${constants}; do
-  case $i in
-  *-*)
-      OFS=${IFS}
-      IFS='-'
-      set $i
-      j=$1
-      k=$2
-      IFS=${OFS}
-      echo "#ifdef $j" >> ${tmph}
-      echo "{\"$j\",$j}," >> ${tmph}
-      echo "#else" >> ${tmph}
-      echo "#ifdef $k" >> ${tmph}
-      echo "{\"$j\",$k}," >> ${tmph}
-      echo "#else" >> ${tmph}
-      echo "{\"$j\",-1}," >> ${tmph}
-      echo "#endif" >> ${tmph}
-      echo "#endif" >> ${tmph}
-      ;;
-  *)
-      echo "#ifdef $i" >> ${tmph}
-      echo "{\"$i\",$i}," >> ${tmph}
-      echo "#else" >> ${tmph}
-      echo "{\"$i\",-1}," >> ${tmph}
-      echo "#endif" >> ${tmph}
-      ;;
-  esac
-done
-cat >> ${tmph} << EOF
-{0,0}
-};
-void loosecase(char *s)
-{
-  int f;
-  for (f=1;*s;s++) {
-    if (*s=='_') {
-      f=1;
-    } else if (f) {
-      f=0;
-    } else {
-      *s+='a'-'A';
-    }
-  }
-}
-main()
-{
-  struct cons *i; for (i=list;i->name;i++) {loosecase(i->name);
-  if (i->value != -1)
-    printf("   %-20s : constant := 16#%04X#;\n", i->name, i->value);
-  else
-    printf("   %-20s : constant := -1;        --  Unavailable\n", i->name);}
-}
-EOF
-if [ "${debug}" = "-d" ]; then
-  cat ${tmph} >& 2
-fi
-gcc -o ${tmpe} -fwritable-strings ${tmph}
 cat > ${fname} << EOF
 --  This package has been generated automatically on:
 EOF
@@ -107,7 +42,34 @@ cat >> ${fname} << EOF
 --  Any change you make here is likely to be lost !
 package ${name} is
 EOF
-${tmpe} >> ${fname}
+
+# For each constant, try to output its value or -1 if undefined
+
+for c in ${constants}; do
+    case ${c} in
+	*-*)
+            OIFS=${IFS}; IFS="-"; set ${c}; IFS=${OIFS}; \
+	    echo "Checking value of $1 (or $2 as a substitute)"; \
+            (gcc -DCONSTANT_NAME=$1 -I. -o ${tmpe} ${srcdir}/constants.c \
+	            2>/dev/null &&
+	            ${tmpe} $1 >> ${fname}) ||
+            (gcc -DCONSTANT_NAME=$2 -I. -o ${tmpe} ${srcdir}/constants.c \
+	            2>/dev/null &&
+	            ${tmpe} $1 >> ${fname}) ||
+            ./constants_nodef $1 >> ${fname}
+    ;;
+	*)
+	    echo "Checking value of $c"; \
+            (gcc -DCONSTANT_NAME=${c} -I. -o ${tmpe} ${srcdir}/constants.c \
+	            2>/dev/null && \
+	            ${tmpe} ${c} >> ${fname}) || \
+             ./constants_nodef ${c} >> ${fname}
+    ;;
+    esac
+done
+
+# Trailer of generated file
+
 cat >> ${fname} << EOF
 end ${name};
 EOF
