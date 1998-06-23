f5114904148c52ceb3509613dda15e36eabf4e87
Laurent Pautet <pautet@adacore.com>
1998-06-23 16:38:32 +0000

    See ChangeLog
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46845

---
+++ Garlic/s-garexc.adb
@@ -0,0 +1,90 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE COMPONENTS                              --
+--                                                                          --
+--            S Y S T E M . G A R L I C . E X C E P T I O N S               --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                            $Revision$                             --
+--                                                                          --
+--         Copyright (C) 1996-1998 Free Software Foundation, Inc.           --
+--                                                                          --
+-- GARLIC is free software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 2,  or (at your option)  any later ver- --
+-- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License  distributed with GARLIC;  see file COPYING.  If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--               GLADE  is maintained by ACT Europe.                        --
+--               (email: glade-report@act-europe.fr)                        --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with GNAT.OS_Lib; use GNAT.OS_Lib;
+with System.Garlic.Types;
+
+package body System.Garlic.Exceptions is
+
+   use Ada.Exceptions;
+
+   ---------------
+   -- Different --
+   ---------------
+
+   function Different (V1, V2 : String) return Boolean is
+
+      function Not_Null_Version (V : in String) return Boolean;
+      --  Return true when V is not a string of blank characters
+
+      ----------------------
+      -- Not_Null_Version --
+      ----------------------
+
+      function Not_Null_Version (V : in String) return Boolean is
+         Null_String : constant String (V'Range) := (others => ' ');
+      begin
+         return V /= Null_String;
+      end Not_Null_Version;
+
+   begin
+      return     Not_Null_Version (V1)
+        and then Not_Null_Version (V2)
+        and then V1 /= V2;
+   end Different;
+
+   -------------------------------
+   -- Raise_Communication_Error --
+   -------------------------------
+
+   procedure Raise_Communication_Error (Msg : in String := "") is
+   begin
+      if Msg = "" then
+         Raise_With_Errno (Types.Communication_Error'Identity);
+      else
+         Raise_Exception (Types.Communication_Error'Identity, Msg);
+      end if;
+   end Raise_Communication_Error;
+
+   ----------------------
+   -- Raise_With_Errno --
+   ----------------------
+
+   procedure Raise_With_Errno (Id : in Exception_Id) is
+   begin
+      Raise_Exception (Id, "Error" & Integer'Image (Errno));
+   end Raise_With_Errno;
+
+end System.Garlic.Exceptions;
---
+++ Garlic/s-garexc.ads
@@ -0,0 +1,55 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE COMPONENTS                              --
+--                                                                          --
+--            S Y S T E M . G A R L I C . E X C E P T I O N S               --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--                            $Revision$                             --
+--                                                                          --
+--         Copyright (C) 1996-1998 Free Software Foundation, Inc.           --
+--                                                                          --
+-- GARLIC is free software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 2,  or (at your option)  any later ver- --
+-- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License  distributed with GARLIC;  see file COPYING.  If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--               GLADE  is maintained by ACT Europe.                        --
+--               (email: glade-report@act-europe.fr)                        --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Exceptions;
+
+package System.Garlic.Exceptions is
+
+   pragma Elaborate_Body;
+
+   procedure Raise_With_Errno (Id : in Ada.Exceptions.Exception_Id);
+   pragma Inline (Raise_With_Errno);
+   --  Raise an exception with a message corresponding to errno
+
+   procedure Raise_Communication_Error (Msg : in String := "");
+   pragma Inline (Raise_Communication_Error);
+   --  Idem, but with the specific exception System.RPC.Communication_Error.
+   --  If an alternate message is given, it will be used instead.
+
+   function Different (V1, V2 : String) return Boolean;
+   --  Compare two version ids. If one of these version ids is a string
+   --  of blank characters then they will be considered as identical.
+
+end System.Garlic.Exceptions;
