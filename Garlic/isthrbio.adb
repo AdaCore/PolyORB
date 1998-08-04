fb7c26bd39820a19f58c16b08362e263c3f2339c
Samuel Tardieu <sam@inf.enst.fr>
1998-08-04 11:28:20 +0000

    Use GNAT style and add copyright information
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 46910

--- Garlic/isthrbio.adb
+++ Garlic/isthrbio.adb
@@ -1,10 +1,45 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE COMPONENTS                              --
+--                                                                          --
+--                             I S T H R B I O                              --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                            $Revision$                             --
+--                                                                          --
+--         Copyright (C) 1996-1998 Free Software Foundation, Inc.           --
+--                                                                          --
+-- GARLIC is free software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 2,  or (at your option)  any later ver- --
+-- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License  distributed with GARLIC;  see file COPYING.  If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--               GLADE  is maintained by ACT Europe.                        --
+--               (email: glade-report@act-europe.fr)                        --
+--                                                                          --
+------------------------------------------------------------------------------
+
 with Ada.Command_Line;   use Ada.Command_Line;
 with Ada.Text_IO;        use Ada.Text_IO;
 with GNAT.OS_Lib;        use GNAT.OS_Lib;
 with Interfaces.C;       use Interfaces.C;
 with System.Garlic.Thin; use System.Garlic.Thin;
 
--- Is Thread Blocking IO
+--  Is Thread Blocking IO
 
 procedure IsThrBIO is
 
@@ -14,6 +49,15 @@ procedure IsThrBIO is
       entry Start;
       entry Stop;
    end A_Task;
+
+   Result  : int;
+   Input   : constant Fd_Set_Access  := new Fd_Set'(0);
+   Timeout : constant Timeval_Access := new Timeval'(1, 0);
+   File    : File_Type;
+
+   ------------
+   -- A_Task --
+   ------------
 
    task body A_Task is
    begin
@@ -32,13 +76,6 @@ procedure IsThrBIO is
          terminate;
       end select;
    end A_Task;
-
-   Result  : int;
-   Input   : Fd_Set_Access := new Fd_Set'(0);
-
-   Timeout : Timeval_Access := new Timeval'(1, 0);
-
-   File    : File_Type;
 
 begin
    A_Task.Start;
