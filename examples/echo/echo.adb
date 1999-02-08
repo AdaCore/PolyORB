f9d194ef00a4aceb7f0ded58069ee56592370564
Fabien Azavant <azavant@enst.fr>
1999-02-08 15:25:28 +0000

    renamed echo_impl
    [imported from CVS by cvs2p4 at 2000/03/21 17:57:47]
    
    [Imported from Perforce change 27 at 2006-12-01 18:13:25]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 30783

--- examples/echo/echo.adb
+++ examples/echo/echo.adb
@@ -42,9 +42,11 @@ package body Echo is
 
 
    --------------------------------------------------
-   ----              not in  spec                ----
+   ----    not in  spec omniORB specific         ----
    --------------------------------------------------
 
+   -- AlignedSize
+   --------------
    function AlignedSize(Self: in OmniProxyCallDesc_Echo;
                           MsgSize: in Corba.Unsigned_Long)
                         return Corba.Unsigned_Long is
@@ -53,7 +55,8 @@ package body Echo is
       return MsgSize;
    end;
 
-
+   -- MarshalArguments
+   -------------------
    procedure MarshalArguments(Self: in OmniProxyCallDesc_Echo;
                                 Giop_Client: in out Giop_C) is
       Len : CORBA.Unsigned_Long;
@@ -67,11 +70,31 @@ package body Echo is
       end if;
    end;
 
+   -- UnMarshalReturnValues
+   ------------------------
    procedure UnmarshalReturnedValues(Self: in OmniProxyCallDesc_Echo;
                                      Giop_Client: in out Giop_C) is
    begin
       Result :=  UnMarshall(Giop_Client);
-   end
+   end ;
+
+
+   --------------------------------------------------
+   ----    not in  spec AdaBroker specific       ----
+   --------------------------------------------------
+
+   -- AdaBroker_Cast_To_Parent
+   ---------------------------
+   procedure AdaBroker_Cast_To_Parent(Real_Object: in Ref;
+                                      Result: out Corba.Object'Class) is
+      Data_Conversion_Error : Corba.Data_Conversion ;
+   begin
+      if Real_Object'Tag = Result'Tag then
+         Result := Real_Object ;
+      else
+         raise Data_Conversion_Error;
+      end if ;
+   end ;
 
 
 End Echo ;
---
+++ examples/echo/server.adb
@@ -0,0 +1,36 @@
+----------------------------------------------------------------------------
+----                                                                    ----
+----     This in an example which is hand-written                       ----
+----     for the echo object (corresponds to eg2_impl.cc in omniORB     ----
+----                                                                    ----
+----                server                                              ----
+----                                                                    ----
+----                authors : Fabien Azavant, Sebastien Ponce           ----
+----                                                                    ----
+----------------------------------------------------------------------------
+
+with Corba.Orb, Corba.Boa ;
+with Text_IO ;
+with Echo.Impl ;
+
+procedure server is
+   Server : Echo.Impl.Object ;
+begin
+
+   -- ORB initialisation
+   Corba.Orb.Init("omniORB2") ;
+
+   -- BOA initialisation
+   Corba.Boa.Init("omniORB2_BOA") ;
+
+   -- telling the BOA there is a new object
+   Corba.Boa.Object_Is_Ready(Server) ;
+
+   -- getting this object's IOR
+   Text_IO.Putline(Corba.Orb.Object_To_String(Server)) ;
+
+   -- telling the BOA we are ready
+   -- the BOA blocks indefinitely on this call
+   Corba.Boa.Impl_Is_Ready(server) ;
+
+end ;
