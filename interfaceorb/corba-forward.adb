006e494e6354956901b9c6a661c6d10731b326f4
Fabien Azavant <azavant@enst.fr>
1999-02-12 17:40:02 +0000

    suite de l'implementation de corba-forward
    [imported from CVS by cvs2p4 at 2000/03/21 17:58:44]
    
    [Imported from Perforce change 57 at 2006-12-01 18:13:48]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 30813

--- interfaceorb/corba-forward.adb
+++ interfaceorb/corba-forward.adb
@@ -23,18 +23,16 @@ package body Corba.Forward is
       ---------------
       function From_Forward(The_Forward : in Ref)
                             return Ref_Type is
-         Result : Ref_Type ;
       begin
-         return Result ;
+         return Ref_Type(The_Forward) ;
       end ;
 
       -- To_Forward
       -------------
       function To_Forward(The_Ref : in Ref_Type)
                           return Ref is
-         Result : Ref ;
       begin
-         return Result ;
+         return Ref(The_Ref) ;
       end ;
 
 
