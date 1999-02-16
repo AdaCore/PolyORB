4686f3e30ddeffe51a9d86b82d011a903d8492a2
Vincent Niebel <niebel@enst.fr>
1999-02-16 19:34:16 +0000

    changing a bad file
    [imported from CVS by cvs2p4 at 2000/03/21 18:00:47]
    
    [Imported from Perforce change 70 at 2006-12-01 18:15:18]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 30826

--- adabroker/ada_be/adabe_field.cc
+++ adabroker/ada_be/adabe_field.cc
@@ -1,24 +1,29 @@
-// File adabe_struct.cc
+// File adabe_field.cc
 
-adabe_structure::adabe_structure(UTL_ScopedName *n, UTL_StrList *p);
+adabe_field::adabe_field(UTL_ScopedName *n, UTL_StrList *p);
 //constructor
 
-adabe_structure::produce_ads(dep_list with,string &String, string &previousdefinition) {
+adabe_field::produce_ads(dep_list with,string &String, string &previousdefinition) {
   /*
     ada_name.compute;
-    String += "type " + ada_name + "is record\n"
-    for each node in the structure:
-       {
-         cast of the item to the real adabe type
-         field.produce_ads (with, String, previousdefinition)
-       }
-    String += "end record"   
+    String +=  ada_name;
+    String += " : ";
+    cast le field_type en NT puis
+    NT.dump_name(dep_list with,string &String, string &previousdefinition);
   */
 }
 
-IMPL_NARROW_METHODS1(adabe_structure, AST_Structure);
-IMPL_NARROW_FROM_DECL(adabe_structure);
-IMPL_NARROW_FROM_SCOPE(adabe_structure);
+IMPL_NARROW_METHODS1(adabe_field, AST_Field);
+IMPL_NARROW_FROM_DECL(adabe_field);
+IMPL_NARROW_FROM_SCOPE(adabe_field);
+
+
+
+
+
+
+
+
 
 
 
