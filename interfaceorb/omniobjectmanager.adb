78fc3ee02f125d04c7ea6ac69d04e81f8e0e3ab7
Sébastien Ponce <ponce@enst.fr>
1999-02-19 00:02:44 +0000

    Implementation of Ada object OmniObjectManager.
    [imported from CVS by cvs2p4 at 2000/03/21 18:01:28]
    
    [Imported from Perforce change 95 at 2006-12-01 18:15:46]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 30851

--- interfaceorb/omniobjectmanager.adb
+++ interfaceorb/omniobjectmanager.adb
@@ -9,7 +9,7 @@
 ----   one function : NilObjectManager).                           ----
 ----                                                               ----
 ----                                                               ----
-----                  package omniObjectManager                    ----
+----                  package body omniObjectManager               ----
 ----                                                               ----
 ----   authors : Sebastien Ponce, Fabien Azavant                   ----
 ----   date    : 02/18/99                                          ----
@@ -17,37 +17,29 @@
 ----                                                               ----
 -----------------------------------------------------------------------
 
-with Interfaces.CPP ;
-with System ;
 
-package OmniObjectManager is
+with System.Address_To_Access_Conversions ;
 
-   type Object is tagged record
-      Table : Interfaces.CPP.Vtable_Ptr ;
-   end record ;
+package body OmniObjectManager is
 
-   pragma CPP_Class (Object);
-   pragma CPP_Vtable (Object,Table,1);
-   -- This object is wrapped around OmniObjectManager (see omniInternal.h)
+   -- Address_To_OmniObjectManager
+   -------------------------------
+   package Address_To_OmniObjectManager is
+     new System.Address_To_Access_Conversions (Object) ;
+   -- needed to interface System.Address and Object
 
-   type Object_Ptr is access all Object ;
-   -- just to give a name to pointers on Object
 
-   function C_Nil_Object_Manager return System.Address ;
-   pragma Import (C,C_Nil_Object_Manager,"_nilObjectManager") ;
-   -- wrapper around static omniObjectManager* nilObjectManager();
-   -- (see omniInternal.h L 514)
+   -- Nil_Object_Manager
+   ---------------------
+   function Nil_Object_Manager return Object is
+      C_Result : System.Address ;
+      Ada_Result_Ptr : Address_To_OmniObjectManager.Object_Pointer ;
+   begin
+      -- calls the C function C_Nil_Object_Manager ...
+      C_Result := C_Nil_Object_Manager ;
+      -- ... and tranforms the result in Ada type
+      Ada_Result_Ptr := Address_To_OmniObjectManager.To_Pointer (C_Result) ;
+      return Ada_Result_Ptr.all ;
+   end;
 
-   function Nil_Object_Manager return Object ;
-   pragma CPP_Virtual(Nil_Object_Manager) ;
-   -- Ada equivalent to C function C_Nil_Object_Manager
-
-private
-
-   function Constructor return Object'Class;
-   pragma CPP_Constructor (Constructor);
-   pragma Import (CPP,Constructor,"__17omniObjectManager");
-   -- wrapped around the C constructor of OmniObjectManager
-
-end OmniObjectManager ;
-
+end OmniObjectManager;
