042178dc8e3b82291471ea9954ae460add63a024
Sébastien Ponce <ponce@enst.fr>
1999-03-03 21:02:15 +0000

    implementation of omni class : Ada equivalent of omni_C2Ada.
    [imported from CVS by cvs2p4 at 2000/03/21 18:05:12]
    
    [Imported from Perforce change 145 at 2006-12-01 18:16:31]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 30901

--- interfaceorb/omni.adb
+++ interfaceorb/omni.adb
@@ -10,21 +10,60 @@
 ----                                                               ----
 -----------------------------------------------------------------------
 
-with Ada.Exceptions ;
+
+with System.Address_To_Access_Conversions ;
 
 Package body Omni is
+
+   function Object_To_String (Obj : in OmniObject.AdaObject)
+                              return String is
+      C_Obj : System.Address ;
+      C_Result : Interfaces.C.Strings.Chars_Ptr ;
+   begin
+      -- transforms the arguments in a C type ...
+      C_Obj := Obj'Address ;
+      -- ... calls the C function ...
+      C_Result := C_Object_To_String (C_Obj) ;
+      -- ... and transforms the result in Ada type
+      return Interfaces.C.Strings.Value (C_Result) ;
+   end ;
+
+
+   -- Address_To_Octet
+   -------------------
+   package Address_To_OmniObject is
+     new System.Address_To_Access_Conversions (OmniObject.AdaObject) ;
+   -- needed to interface System.Address and Corba.Octet
+
+
+   function String_To_Object (Str : in String)
+                              return OmniObject.AdaObject is
+      C_Str : Interfaces.C.Strings.Chars_Ptr ;
+      C_Result : System.Address ;
+      Ada_Result_Ptr : Address_To_OmniObject.Object_Pointer ;
+   begin
+      -- transforms the arguments in a C type ...
+      C_Str := Interfaces.C.Strings.New_String (Str) ;
+      -- ... calls the C function ...
+      C_Result := C_String_To_Object (C_Str) ;
+      -- ... and transforms the result in Ada type
+      Ada_Result_Ptr :=  Address_To_OmniObject.To_Pointer (C_Result) ;
+      return Ada_Result_Ptr.all ;
+   end ;
+
+
 
 
    -- Align_To
    -----------
-   function Align_To(P: in Corba.Unsigned_Long ;
-                     Align : in Alignment_T)
-                     return Corba.Unsigned_Long is
-   begin
-      Ada.Exceptions.Raise_Exception(Corba.AdaBroker_Not_Implemented_Yet'Identity,
-                                     "Omni.Align_To") ;
-      return Corba.Unsigned_Long(0) ;
-   end ;
+--   function Align_To(P: in Corba.Unsigned_Long ;
+--                     Align : in Alignment_T)
+--                     return Corba.Unsigned_Long is
+--   begin
+--      Ada.Exceptions.Raise_Exception(Corba.AdaBroker_Not_Implemented_Yet'Identity,
+--                                     "Omni.Align_To") ;
+--      return Corba.Unsigned_Long(0) ;
+--   end ;
 
 
 end Omni ;
--- interfaceorb/omni.ads
+++ interfaceorb/omni.ads
@@ -2,6 +2,11 @@
 ----                                                               ----
 ----                  AdaBroker                                    ----
 ----                                                               ----
+----     This package is wrapped around the C class omni_C2Ada     ----
+----   declared in omni_C2Ada.hh;                                  ----
+----     It provides the 2 functions of omni_C2Ada and their       ----
+----   equivalent in ADA.                                          ----
+----                                                               ----
 ----                  package omni                                 ----
 ----                                                               ----
 ----   authors : Sebastien Ponce, Fabien Azavant                   ----
@@ -10,29 +15,53 @@
 ----                                                               ----
 -----------------------------------------------------------------------
 
-with Corba ;
+with Interfaces.CPP ;
+with Interfaces.C.Strings ;
+with System ;
+with OmniObject ;
 
 package Omni is
 
+   function C_Object_To_String (Obj : in System.Address)
+                                return Interfaces.C.Strings.Chars_Ptr;
+   pragma Import (C,C_Object_To_String,"objectToString__10omni_C2AdaP14Ada_OmniObject") ;
+   -- wrapper around omni_C2Ada function objectToString
+   -- (see omni_C2Ada.hh)
 
-   type Alignment_T is new Integer;
-   ALIGN_1 : constant Alignment_T := 1;
-   ALIGN_2 : constant Alignment_T := 2;
-   ALIGN_4 : constant Alignment_T := 4;
-   ALIGN_8 : constant Alignment_T := 8;
+   function Object_To_String (Obj : in OmniObject.AdaObject)
+                              return String ;
+   -- Ada equivalent of C function C_Init
+
+
+   function C_String_To_Object (str : in Interfaces.C.Strings.Chars_Ptr)
+                                return System.Address ;
+   pragma Import (C,C_String_To_Object,"stringToObject__10omni_C2AdaPCc") ;
+   -- wrapper around omni_C2Ada function stringToObject
+   -- (see omni_C2Ada.hh)
+
+   function String_To_Object (Str : in String)
+                              return OmniObject.AdaObject ;
+    -- Ada equivalent of C function C_Init
+
+
+--   type Alignment_T is new Integer;
+--   ALIGN_1 : constant Alignment_T := 1;
+--   ALIGN_2 : constant Alignment_T := 2;
+--   ALIGN_4 : constant Alignment_T := 4;
+--   ALIGN_8 : constant Alignment_T := 8;
    -- corresponds to enum alignment_t { ALIGN_1 = 1, ALIGN_2 = 2, ALIGN_4 = 4, ALIGN_8 = 8 };
    -- In Internal.h L 162
 
 
-   function Align_To(P: in Corba.Unsigned_Long ;
-                     Align : in Alignment_T)
-                     return Corba.Unsigned_Long ;
+--   function Align_To(P: in Corba.Unsigned_Long ;
+--                     Align : in Alignment_T)
+--                     return Corba.Unsigned_Long ;
    -- wrapper around   static inline ptr_arith_t align_to(
    --                     ptr_arith_t p,
    --                     alignment_t align)
    -- In Internal.h L 166
 
-private
+--private
 
 
 
