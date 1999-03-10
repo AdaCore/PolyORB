75d7fa35e206a74c26104943a8d054a2a96a26db
Sébastien Ponce <ponce@enst.fr>
1999-03-10 19:57:45 +0000

    presentation model
    [imported from CVS by cvs2p4 at 2000/03/21 18:09:11]
    
    [Imported from Perforce change 182 at 2006-12-01 18:17:33]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 30938

--- interfaceorb/giop.ads
+++ interfaceorb/giop.ads
@@ -1,28 +1,57 @@
 -----------------------------------------------------------------------
+-----------------------------------------------------------------------
 ----                                                               ----
-----                  AdaBroker                                    ----
+----                         AdaBroker                             ----
+----                                                               ----
+----                       package Giop                            ----
+----                                                               ----
+----                                                               ----
+----   Copyright (C) 1999 ENST                                     ----
+----                                                               ----
+----   This file is part of the AdaBroker library                  ----
+----                                                               ----
+----   The AdaBroker library is free software; you can             ----
+----   redistribute it and/or modify it under the terms of the     ----
+----   GNU Library General Public License as published by the      ----
+----   Free Software Foundation; either version 2 of the License,  ----
+----   or (at your option) any later version.                      ----
+----                                                               ----
+----   This library is distributed in the hope that it will be     ----
+----   useful, but WITHOUT ANY WARRANTY; without even the implied  ----
+----   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR     ----
+----   PURPOSE.  See the GNU Library General Public License for    ----
+----   more details.                                               ----
+----                                                               ----
+----   You should have received a copy of the GNU Library General  ----
+----   Public License along with this library; if not, write to    ----
+----   the Free Software Foundation, Inc., 59 Temple Place -       ----
+----   Suite 330, Boston, MA 02111-1307, USA                       ----
+----                                                               ----
+----                                                               ----
+----                                                               ----
+----   Description                                                 ----
+----   -----------                                                 ----
 ----                                                               ----
 ----     This package is wrapped around the C++ class GIOP         ----
 ----   declared in GIOP.h.                                         ----
 ----     It provides some Ada equivalents of C++ types and the     ----
 ----   corresponding translation fonctions.                        ----
 ----                                                               ----
-----                  package giop                                 ----
 ----                                                               ----
 ----   authors : Sebastien Ponce, Fabien Azavant                   ----
 ----   date    : 02/28/99                                          ----
 ----                                                               ----
-----                                                               ----
+-----------------------------------------------------------------------
 -----------------------------------------------------------------------
 
-with Interfaces.C ;
 
+with Interfaces.C ;
 
 package Giop is
 
    type Reply_Status_Type is (NO_EXCEPTION, USER_EXCEPTION,
                               SYSTEM_EXCEPTION, LOCATION_FORWARD);
-   --  corresponds to enum ReplyStatusType { NO_EXCEPTION, USER_EXCEPTION,
+   -- corresponds to enum ReplyStatusType { NO_EXCEPTION, USER_EXCEPTION,
    --                                      SYSTEM_EXCEPTION, LOCATION_FORWARD }
    -- in GIOP.h L 81
 
@@ -38,9 +67,6 @@ package Giop is
    -- according to the C way of defining enumerations.
    -- It means 0 -> NO_EXCEPTION, 1 -> USER_EXCEPTION ...
    -- An Ada Exception C_Out_Of_Range is raised if the C value is incorrect.
-
-
-private
 
 end Giop ;
 
