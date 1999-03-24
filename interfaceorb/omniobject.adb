8947b0e059fcffda87bd29a5447c70927faaec8f
Fabien Azavant <azavant@enst.fr>
1999-03-24 21:46:53 +0000

    setting implobj to 0 in the C++ code (ctd')
    [imported from CVS by cvs2p4 at 2000/03/21 18:15:22]
    
    [Imported from Perforce change 378 at 2006-12-01 18:22:02]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31134

--- interfaceorb/omniobject.adb
+++ interfaceorb/omniobject.adb
@@ -404,7 +404,7 @@ package body OmniObject is
       C_Arg : System.Address ;
       Result : Object_Ptr ;
    begin
-      pragma Debug(Output(Omniobject,"Omniobject.Omniobject_Duplicate"))
+      pragma Debug(Output(Omniobject,"Omniobject.Omniobject_Duplicate")) ;
       C_Arg := Address_To_Object.To_Address(From_Object_Ptr(Self)) ;
       C_Result := C_Omniobject_Duplicate(C_Arg) ;
       Result := To_Object_Ptr(Address_To_Object.To_Pointer(C_Result)) ;
