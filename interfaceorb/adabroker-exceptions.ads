6ec41cacab7de87109bc16824f4c8c4b58ad93bb
Laurent Pautet <pautet@adacore.com>
1999-05-03 22:10:25 +0000

    minor reformatting
    [imported from CVS by cvs2p4 at 2000/03/21 18:25:15]
    
    [Imported from Perforce change 580 at 2006-12-01 18:25:56]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31336

--- interfaceorb/adabroker-exceptions.ads
+++ interfaceorb/adabroker-exceptions.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---                            $Revision: 1.5 $
+--                            $Revision: 1.6 $
 --                                                                          --
 --         Copyright (C) 1999-2000 ENST Paris University, France.           --
 --                                                                          --
@@ -41,242 +41,269 @@
 --  Ada. Both translate exceptions in the other language.
 
 with Ada.Exceptions;
+with Ada.Unchecked_Deallocation;
 
+with Interfaces;   use Interfaces;
 with Interfaces.C;
 with Interfaces.C.Strings;
 
-with CORBA;
+with CORBA;        use CORBA;
 
 package AdaBroker.Exceptions is
 
+   To_Status : constant array (C.int range 0 .. 2) of Completion_Status
+     := (0 => Completed_Yes,
+         1 => Completed_No,
+         2 => Completed_Maybe);
+
+   To_Int    : constant array (Completion_Status) of C.int
+     := (Completed_Yes   => 0,
+         Completed_No    => 1,
+         Completed_Maybe => 2);
+
+   type IDL_Exception_Members_Ptr is access all IDL_Exception_Members'Class;
+
+   procedure Raise_CORBA_Exception
+     (Ex_Id     : in Ada.Exceptions.Exception_Id;
+      Ex_Member : in IDL_Exception_Members'Class);
+
+   --     procedure Get_Members
+   --       (From : in Ada.Exceptions.Exception_Occurrence;
+   --        To   : out IDL_Exception_Members'Class);
+
+   function Get_Members
+     (From : in Ada.Exceptions.Exception_Occurrence)
+     return IDL_Exception_Members'Class;
+
+   procedure Free is
+     new Ada.Unchecked_Deallocation
+     (IDL_Exception_Members'Class,
+      IDL_Exception_Members_Ptr);
+
    procedure C_Raise_Ada_Fatal_Exception
-     (File    : in Interfaces.C.Strings.chars_ptr;
-      Line    : in Interfaces.C.int;
-      Err_Msg : in Interfaces.C.Strings.chars_ptr);
+     (File    : in C.Strings.chars_ptr;
+      Line    : in C.int;
+      Err_Msg : in C.Strings.chars_ptr);
    pragma Export
      (Cpp, C_Raise_Ada_Fatal_Exception,
-      "Raise_Ada_FatalException__FPCciT0");
+      "Raise_Ada_Fatal_Exception__FPCciT0");
 
    procedure C_Raise_Ada_UNKNOWN_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_UNKNOWN_Exception,
       "Raise_Ada_UNKNOWN_Exception__FUlQ25CORBA16CompletionStatus");
-   --  Wrapped around C function Raise_Ada_UNKNOWN_Exception declared in
-   --  Ada_exceptions.hh Called by C code.  Handles in Ada a Corba
-   --  exception that was raised in C.
-
 
    procedure C_Raise_Ada_BAD_PARAM_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_BAD_PARAM_Exception,
       "Raise_Ada_BAD_PARAM_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_NO_MEMORY_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_NO_MEMORY_Exception,
       "Raise_Ada_NO_MEMORY_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_IMP_LIMIT_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_IMP_LIMIT_Exception,
       "Raise_Ada_IMP_LIMIT_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_COMM_FAILURE_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_COMM_FAILURE_Exception,
       "Raise_Ada_COMM_FAILURE_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_INV_OBJREF_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_INV_OBJREF_Exception,
       "Raise_Ada_INV_OBJREF_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_OBJECT_NOT_EXIST_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_OBJECT_NOT_EXIST_Exception,
       "Raise_Ada_OBJECT_NOT_EXIST_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_NO_PERMISSION_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_NO_PERMISSION_Exception,
       "Raise_Ada_NO_PERMISSION_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_INTERNAL_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_INTERNAL_Exception,
       "Raise_Ada_INTERNAL_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_MARSHAL_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_MARSHAL_Exception,
       "Raise_Ada_MARSHAL_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_INITIALIZATION_FAILURE_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_INITIALIZATION_FAILURE_Exception,
       "Raise_Ada_INITIALIZE_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_NO_IMPLEMENT_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_NO_IMPLEMENT_Exception,
       "Raise_Ada_NO_IMPLEMENT_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_BAD_TYPECODE_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_BAD_TYPECODE_Exception,
       "Raise_Ada_BAD_TYPECODE_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_BAD_OPERATION_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_BAD_OPERATION_Exception,
       "Raise_Ada_BAD_OPERATION_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_NO_RESOURCES_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_NO_RESOURCES_Exception,
       "Raise_Ada_NO_RESOURCES_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_NO_RESPONSE_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_NO_RESPONSE_Exception,
       "Raise_Ada_NO_RESPONSE_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_PERSIST_STORE_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_PERSIST_STORE_Exception,
       "Raise_Ada_PERSIST_STORE_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_BAD_INV_ORDER_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_BAD_INV_ORDER_Exception,
       "Raise_Ada_BAD_INV_ORDER_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_TRANSIENT_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_TRANSIENT_Exception,
       "Raise_Ada_TRANSIENT_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_FREE_MEM_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_FREE_MEM_Exception,
       "Raise_Ada_FREE_MEM_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_INV_IDENT_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_INV_IDENT_Exception,
       "Raise_Ada_INV_IDENT_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_INV_FLAG_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_INV_FLAG_Exception,
       "Raise_Ada_INV_FLAG_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_INTF_REPOS_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_INTF_REPOS_Exception,
       "Raise_Ada_INTF_REPOS_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_BAD_CONTEXT_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_BAD_CONTEXT_Exception,
       "Raise_Ada_BAD_CONTEXT_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_OBJ_ADAPTER_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_OBJ_ADAPTER_Exception,
       "Raise_Ada_OBJ_ADAPTER_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_DATA_CONVERSION_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_DATA_CONVERSION_Exception,
       "Raise_Ada_DATA_CONVERSION_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_TRANSACTION_REQUIRED_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_TRANSACTION_REQUIRED_Exception,
       "Raise_Ada_TRANSACTION_REQUIRED_Exception_" &
       "_FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_TRANSACTION_ROLLEDBACK_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_TRANSACTION_ROLLEDBACK_Exception,
       "Raise_Ada_TRANSACTION_ROLLEDBACK_Exception_"  &
       "_FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_INVALID_TRANSACTION_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_INVALID_TRANSACTION_Exception,
       "Raise_Ada_INVALID_TRANSACTION_Exception_" &
       "_FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_WRONG_TRANSACTION_Exception
-     (Pd_Minor : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_WRONG_TRANSACTION_Exception,
       "Raise_Ada_WRONG_TRANSACTION_Exception__FUlQ25CORBA16CompletionStatus");
 
    procedure C_Raise_Ada_Fatal_Exception
-     (Pd_Minor  : in Interfaces.C.unsigned_long;
-      Pd_Status : in Interfaces.C.int);
+     (Pd_Minor  : in C.unsigned_long;
+      Pd_Status : in C.int);
    pragma Export
      (Cpp, C_Raise_Ada_Fatal_Exception,
       "Raise_Ada_Fatal_Exception__FUlQ25CORBA16CompletionStatus");
