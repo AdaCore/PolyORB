cf6155e005f3f50fb5cb48a55b64e3655b2159a0
Laurent Pautet <pautet@adacore.com>
1999-05-31 15:06:23 +0000

    Add copyright header
    [imported from CVS by cvs2p4 at 2000/03/21 18:27:19]
    
    [Imported from Perforce change 636 at 2006-12-01 18:26:46]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31392

--- adabroker/ada_be/adabe_array.cc
+++ adabroker/ada_be/adabe_array.cc
@@ -1,3 +1,35 @@
+//--------------------------------------------------------------------------//
+//                                                                          //
+//                          ADABROKER COMPONENTS                            //
+//                                                                          //
+//                            A D A B R O K E R                             //
+//                                                                          //
+//                            $Revision: 1.29 $
+//                                                                          //
+//         Copyright (C) 1999-2000 ENST Paris University, France.           //
+//                                                                          //
+// AdaBroker is free software; you  can  redistribute  it and/or modify it  //
+// under terms of the  GNU General Public License as published by the  Free //
+// Software Foundation;  either version 2,  or (at your option)  any  later //
+// version. AdaBroker  is distributed  in the hope that it will be  useful, //
+// but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- //
+// TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public //
+// License  for more details.  You should have received  a copy of the GNU  //
+// General Public License distributed with AdaBroker; see file COPYING. If  //
+// not, write to the Free Software Foundation, 59 Temple Place - Suite 330, //
+// Boston, MA 02111-1307, USA.                                              //
+//                                                                          //
+// As a special exception,  if other files  instantiate  generics from this //
+// unit, or you link  this unit with other files  to produce an executable, //
+// this  unit  does not  by itself cause  the resulting  executable  to  be //
+// covered  by the  GNU  General  Public  License.  This exception does not //
+// however invalidate  any other reasons why  the executable file  might be //
+// covered by the  GNU Public License.                                      //
+//                                                                          //
+//             AdaBroker is maintained by ENST Paris University.            //
+//                     (email: broker@inf.enst.fr)                          //
+//                                                                          //
+//--------------------------------------------------------------------------//
 #include <adabe.h>
 #include <stdio.h>
   
