0bce21d6346585993eb61f1e245f5f9a428741f6
Laurent Pautet <pautet@adacore.com>
1999-06-01 18:50:02 +0000

    Typo
    [imported from CVS by cvs2p4 at 2000/03/21 18:27:32]
    
    [Imported from Perforce change 647 at 2006-12-01 18:26:55]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31403

--- interfaceorb/Ada_exceptions.hh
+++ interfaceorb/Ada_exceptions.hh
@@ -1,42 +1,7 @@
-//--------------------------------------------------------------------------//
-//                                                                          //
-//                          ADABROKER COMPONENTS                            //
-//                                                                          //
-//                            A D A B R O K E R                             //
-//                                                                          //
-//                            $Revision: 1.12 $
-//                                                                          //
-//         Copyright (C) 1999-2000 ENST Paris University, France.           //
-//                                                                          //
-// AdaBroker is free software; you  can  redistribute  it and/or modify it  //
-// under terms of the  GNU General Public License as published by the  Free //
-// Software Foundation;  either version 2,  or (at your option)  any  later //
-// version. AdaBroker  is distributed  in the hope that it will be  useful, //
-// but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- //
-// TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public //
-// License  for more details.  You should have received  a copy of the GNU  //
-// General Public License distributed with AdaBroker; see file COPYING. If  //
-// not, write to the Free Software Foundation, 59 Temple Place - Suite 330, //
-// Boston, MA 02111-1307, USA.                                              //
-//                                                                          //
-// As a special exception,  if other files  instantiate  generics from this //
-// unit, or you link  this unit with other files  to produce an executable, //
-// this  unit  does not  by itself cause  the resulting  executable  to  be //
-// covered  by the  GNU  General  Public  License.  This exception does not //
-// however invalidate  any other reasons why  the executable file  might be //
-// covered by the  GNU Public License.                                      //
-//                                                                          //
-//             AdaBroker is maintained by ENST Paris University.            //
-//                     (email: broker@inf.enst.fr)                          //
-//                                                                          //
-//--------------------------------------------------------------------------//
 #ifndef __ADA_EXCEPTIONS_H__
 #define __ADA_EXCEPTIONS_H__
 
 #include <omniORB2/CORBA.h>
-
-extern void Lock_Occurrence_Table ();
-extern void Unlock_Occurrence_Table ();
 
 ///////////////////////////////////////////
 // handling od corba exception in C code //
@@ -81,8 +46,6 @@ extern void Unlock_Occurrence_Table ();
 } catch (CORBA::TRANSIENT &e) { \
   Raise_Corba_Exception (e); \
 } catch (CORBA::FREE_MEM &e) { \
-  Raise_Corba_Exception (e); \
-} catch (CORBA::INITIALIZE &e) { \
   Raise_Corba_Exception (e); \
 } catch (CORBA::INV_IDENT &e) { \
   Raise_Corba_Exception (e); \
