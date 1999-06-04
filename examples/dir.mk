3090b36dc15364908ee5be1ea81eab760e6a4bdb
Emmanuel Chavane <chavane@enst.fr>
1999-06-04 14:59:41 +0000

    *** empty log message ***
    [imported from CVS by cvs2p4 at 2000/03/21 18:28:23]
    
    [Imported from Perforce change 696 at 2006-12-01 18:27:15]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31452

--- examples/dir.mk
+++ examples/dir.mk
@@ -1,5 +1,5 @@
 SUBDIRS = echo all_types all_exceptions multiple_inheritance all_functions \
-forward naming_service tasking
+forward naming_service tasking dii_echo dii_args
 
 all::
 	@$(MakeSubdirs)
