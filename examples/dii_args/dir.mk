bcb16dab3bc16f3248df1b97f20a13da191b8c61
Emmanuel Chavane <chavane@enst.fr>
1999-06-04 14:58:07 +0000

    dii examples
    [imported from CVS by cvs2p4 at 2000/03/21 18:28:22]
    
    [Imported from Perforce change 695 at 2006-12-01 18:27:14]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31451

---
+++ examples/dii_args/dir.mk
@@ -0,0 +1,26 @@
+FLAGS = -g -A$(EXPORT_TREE)/$(LIBDIR) $(IMPORT_LIBRARY_FLAGS)
+
+all:: $(CORBA_LIB_DEPEND) $(ADABROKER_LIB_DEPEND) ada
+	gnatmake -gnatf -gnata -m -i server.adb $(FLAGS)
+	gnatmake -gnatf -gnata -m -i client.adb $(FLAGS)
+	gnatmake -gnatf -gnata -m -i dii_client.adb $(FLAGS)
+
+IDL_INTERFACE = args
+
+GENERATED_FILES = $(IDL_INTERFACE).ad*
+GENERATED_FILES += $(IDL_INTERFACE)-proxy.ad*
+GENERATED_FILES += $(IDL_INTERFACE)-stream.ad*
+GENERATED_FILES += $(IDL_INTERFACE)-skel.ad*
+GENERATED_FILES += $(IDL_INTERFACE)_idl_file.ad*
+GENERATED_FILES += $(IDL_INTERFACE)_idl_file-stream.ad*
+
+clean::
+	-rm -f *.o *.ali *~ server client dii_client b_*.c $(GENERATED_FILES)
+
+clean_dii_client:
+	rm dii_client dii_client.ali dii_client.o b_dii_client*
+
+args.ads: args.idl
+	$(EXPORT_TREE)/$(BINDIR)/adabroker args.idl
+
+ada:: args.ads
