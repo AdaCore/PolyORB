d9335ccfaed42305bf1b11abdd3a9f5c47a88a1a
Sébastien Ponce <ponce@enst.fr>
1999-08-06 16:46:55 +0000

    remove a undesirable modification of the config.mk
    [imported from CVS by cvs2p4 at 2000/03/21 18:30:28]
    
    [Imported from Perforce change 743 at 2006-12-01 18:28:15]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31499

--- broca/config/config.mk
+++ broca/config/config.mk
@@ -1,5 +1,4 @@
-#BROCA_ADASOCKET = $(BROCA_TOP)/adasockets-0.1.3/src
-BROCA_ADASOCKET = /usr/lib/adasockets
+BROCA_ADASOCKET = $(BROCA_TOP)/adasockets-0.1.3/src
 GNATMAKE = gnatmake
 ADA_FLAGS = -O2 -g -gnatf -gnatwu -gnatwl -I$(BROCA_ADASOCKET)
 BROCA_FLAGS = $(ADA_FLAGS) -I$(BROCA_TOP)/src -I..
