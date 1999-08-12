30334c87b1a34b63f8e70f5101e5d081ee5b35a5
Sébastien Ponce <ponce@enst.fr>
1999-08-12 20:09:21 +0000

    Adds broca.opt : list of the packages you want to display debug infos. Adds a line in the server to pass the Repository Id to the generic server.
    [imported from CVS by cvs2p4 at 2000/03/21 18:31:02]
    
    [Imported from Perforce change 759 at 2006-12-01 18:28:30]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31515

--- broca/examples/all_types/server.adb
+++ broca/examples/all_types/server.adb
@@ -4,6 +4,7 @@ with GenericServer; use GenericServer;
 procedure server is
 begin
    
+   Repository_Id := new String'("IDL:all_types:1.0");
    My_Obj := new All_Types.MyImpl.Object;
    GenericServer.Main(My_Obj);
 
--- broca/examples/echo/server.adb
+++ broca/examples/echo/server.adb
@@ -4,6 +4,7 @@ with GenericServer; use GenericServer;
 procedure Server is
 begin
    
+   Repository_Id := new String'("IDL:Echo:1.0");
    My_Obj := new Echo.My_Impl.Object;
    GenericServer.Main(My_Obj);
    
