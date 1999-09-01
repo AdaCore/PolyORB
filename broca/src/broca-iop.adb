bf3268169753b5fb48a5ca79917f65c0bc1c4024
Laurent Pautet <pautet@adacore.com>
1999-09-01 21:16:47 +0000

    Forgot to add a file
    [imported from CVS by cvs2p4 at 2000/03/21 18:34:25]
    
    [Imported from Perforce change 829 at 2006-12-01 18:29:55]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 31585

---
+++ broca/src/broca-iop.adb
@@ -0,0 +1,104 @@
+with Broca.Buffers;     use Broca.Buffers;
+with Broca.Marshalling; use Broca.Marshalling;
+with CORBA;             use CORBA;
+
+package body Broca.IOP is
+
+   type Profile_Record is
+      record
+         Encapsulate : Encapsulate_Profile_Type;
+         Decapsulate : Decapsulate_Profile_Type;
+      end record;
+
+   Callbacks : array (Tag_Internet_IOP .. Tag_Multiple_Components)
+     of Profile_Record;
+
+   ---------------------
+   -- Decapsulate_IOR --
+   ---------------------
+
+   procedure Decapsulate_IOR
+     (Buffer   : in out Buffers.Buffer_Descriptor;
+      Type_Id  : out CORBA.String;
+      Profiles : out Profile_Ptr_Array_Ptr)
+   is
+      Old_Endian : Boolean := Get_Endianess (Buffer);
+      New_Endian : Boolean;
+      N_Profiles : CORBA.Unsigned_Long;
+      Profile    : Profile_Id;
+   begin
+      Unmarshall    (Buffer, New_Endian);
+      Set_Endianess (Buffer, New_Endian);
+      Unmarshall    (Buffer, Type_Id);
+      Unmarshall    (Buffer, N_Profiles);
+      Profiles := new Profile_Ptr_Array (1 .. N_Profiles);
+
+      for N in Profiles'Range loop
+         Unmarshall  (Buffer, Profile);
+         Callbacks (N).Decapsulate (Buffer, Profiles (N));
+      end loop;
+      Set_Endianess (Buffer, Old_Endian);
+   end Decapsulate_IOR;
+
+   ---------------------
+   -- Encapsulate_IOR --
+   ---------------------
+
+   procedure Encapsulate_IOR
+     (Buffer   : in out Buffers.Buffer_Descriptor;
+      From     : in Buffers.Buffer_Index_Type;
+      Type_Id  : in CORBA.String;
+      Profiles : in Profile_Ptr_Array_Ptr)
+   is
+      Buffers  : array (Profiles'Range) of Buffer_Descriptor;
+   begin
+      Skip_Bytes          (Buffer, From);
+      Compute_New_Size    (Buffer, O_Size, O_Size);
+      Compute_New_Size    (Buffer, Type_Id);
+
+      --  Number of profiles
+      Compute_New_Size    (Buffer, UL_Size, UL_Size);
+
+      for N in Profiles'Range loop
+         --  Tag of profile
+         Compute_New_Size    (Buffer, UL_Size, UL_Size);
+
+         --  Length of profile
+         Compute_New_Size    (Buffer, UL_Size, UL_Size);
+
+         Callbacks (N).Encapsulate (Buffers (N), Size (Buffer), Profiles (N));
+
+         --  Skip space for profile
+         Skip_Bytes (Buffer, Size (Buffers (N)));
+      end loop;
+
+      Allocate_Buffer_And_Clear_Pos (Buffer, Size (Buffer));
+
+      Marshall (Buffer, Is_Little_Endian);
+      Marshall (Buffer, Type_Id);
+      Marshall (Buffer, Profiles'Last + 1);
+
+      for N in Profiles'Range loop
+         Rewind        (Buffers (N));
+         Skip_Bytes    (Buffers (N), Size (Buffer));
+         Marshall      (Buffer, Get_Profile_Id (Profiles (N).all));
+         Marshall      (Buffer, CORBA.Unsigned_Long (Size_Left (Buffers (N))));
+         Append_Buffer (Buffer, Buffers (N));
+         Destroy       (Buffers (N));
+      end loop;
+   end Encapsulate_IOR;
+
+   --------------
+   -- Register --
+   --------------
+
+   procedure Register
+     (Profile     : in Profile_Id;
+      Encapsulate : in Encapsulate_Profile_Type;
+      Decapsulate : in Decapsulate_Profile_Type) is
+   begin
+      Callbacks (Profile).Encapsulate := Encapsulate;
+      Callbacks (Profile).Decapsulate := Decapsulate;
+   end Register;
+
+end Broca.IOP;
