d46f5396080c3bfb50b99f11c9ad3677cf080451
Laurent Pautet <pautet@adacore.com>
1999-09-07 14:21:28 +0000

    * MANIFEST: Remove s-garloc.adb, s-garloc.ads, s-gartyp.adb and 	add s-gaprob.adb, s-gaprob.ads.
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 47281

--- Tests/Three/test_remote_2.adb
+++ Tests/Three/test_remote_2.adb
@@ -3,12 +3,22 @@
 --
 
 with Test_Remote_3; use Test_Remote_3;
+with System.RPC;    use System.RPC;
+with Ada.Text_IO;   use Ada.Text_IO;
 
 package body Test_Remote_2 is
 
    function Test_2 (S : String) return String is
    begin
-      return Test_3 (S) & " (via Test_2)";
+      loop
+         declare
+            Substring : String := Test_3 (S) & " (via Test_2)";
+         begin
+            return Substring;
+         exception when Communication_Error =>
+            Put_LIne ("Cannot execute Tests_3");
+         end;
+      end loop;
    end Test_2;
 
 end Test_Remote_2;
--- Tests/Three/test_three.adb
+++ Tests/Three/test_three.adb
@@ -3,9 +3,29 @@
 --
 
 with Ada.Text_IO; use Ada.Text_IO;
+with System.RPC;  use System.RPC;
 with Test_Remote_2; use Test_Remote_2;
+with Test_Remote_3; use Test_Remote_3;
 
 procedure Test_Three is
 begin
-   Put_Line (Test_2 ("Test string"));
+   loop
+      declare
+         Substring : String := Test_3 ("Test string");
+      begin
+         Put_Line (Substring);
+      exception when Communication_Error =>
+         Put_Line ("Cannot execute Test_3");
+      end;
+      delay 2.0;
+
+      declare
+         Substring : String := Test_2 ("Test string");
+      begin
+         Put_Line (Substring);
+      exception when Communication_Error =>
+         Put_Line ("Cannot execute Test_2");
+      end;
+      delay 2.0;
+   end loop;
 end Test_Three;
