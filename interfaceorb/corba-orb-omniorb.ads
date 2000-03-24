d893646d6aeb5e420e378d711c2f08ee9ef035c2
Thomas Quinot <quinot@adacore.com>
2000-03-24 11:01:47 +0000

    Created a branch for the automated CVS -> Perforce check-in reflector.
    [Imported from Perforce change 1034 at 2006-12-01 18:34:15]
    
    Subversion-branch: /branches/adabroker/cvs
    Subversion-revision: 31790

--- interfaceorb/corba-orb-omniorb.ads
+++
@@ -1,8 +0,0 @@
-with System;
-
-package CORBA.ORB.OmniORB is
-
-      The_ORB : System.Address;
-
-end CORBA.ORB.OmniORB;
-
--- interfaceorb/portableserver-idassignmentpolicy.ads
+++
@@ -1,8 +0,0 @@
-
-package PortableServer.IdAssignmentPolicy is
-
-   type Ref is new CORBA.Policy.Ref with null record;
-
-   function Get_Value (Self : Ref) return IdAssignmentPolicyValue;
-
-end PortableServer.IdAssignmentPolicy;
