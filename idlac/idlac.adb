b16321a38577ecd4dc9d80e389fb0dbcfccd8783
Samuel Tardieu <sam@inf.enst.fr>
2000-04-26 17:50:40 +0000

    Once again.
    [Imported from Perforce change 1413 at 2006-12-01 18:50:19]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 32001

--- idlac/idlac.adb
+++ idlac/idlac.adb
@@ -75,7 +75,7 @@ begin
 
       loop
          case Getopt ("I: i k p q") is
-            when Ascii.Nul => exit;
+            when ASCII.Nul => exit;
 
             when 'I' =>
                Idl_Fe.Lexer.Add_Argument ("-I");
