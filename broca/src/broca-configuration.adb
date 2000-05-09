e249ddd01e38468f04a112000475c94698276622
Laurent Pautet <pautet@adacore.com>
2000-05-09 10:59:59 +0000

    Same as 1529, but it is not possible to submit several files on the command line.
    [Imported from Perforce change 1530 at 2006-12-01 18:53:20]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 32102

---
+++ broca/src/broca-configuration.adb
@@ -0,0 +1,5 @@
+with Broca.Protected_Objects;
+pragma Elaborate (Broca.Protected_Objects);
+pragma Warnings (Off, Broca.Protected_Objects);
+
+package body Broca.Configuration is end Broca.Configuration;
