bb2073d7bbcb6ed580bf78f889cd5598f7d6e25b
Laurent Pautet <pautet@adacore.com>
2000-05-09 10:27:11 +0000

    Soft_Links, general interface whatever the synchronization service is. Protected_Objects, specific synchronization service. Basic_Startup, how to activate servant et server in two procedures.
    [Imported from Perforce change 1527 at 2006-12-01 18:53:19]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 32099

---
+++ broca/src/broca-protected_objects.ads
@@ -0,0 +1,123 @@
+with Broca.Soft_Links;
+
+package Broca.Protected_Objects is
+
+   procedure Initialize;
+
+   -------------------------------------------
+   -- Critical Section for ORB with Tasking --
+   -------------------------------------------
+
+   procedure Enter_Critical_Section;
+
+   procedure Leave_Critical_Section;
+
+   ----------------------------------
+   -- Barrier for ORB with Tasking --
+   ----------------------------------
+
+   type Protected_Barrier_Type is new Soft_Links.Barrier_Type with private;
+
+   function Create return Soft_Links.Barrier_Access;
+
+   procedure Destroy (B : in out Protected_Barrier_Type);
+
+   procedure Signal
+     (B : in Protected_Barrier_Type;
+      N : in Positive := 1);
+
+   procedure Signal_All
+     (B : in Protected_Barrier_Type;
+      P : in Boolean := True);
+
+   procedure Wait (B : in Protected_Barrier_Type);
+
+   --------------------------------
+   -- Mutex for PCS with Tasking --
+   --------------------------------
+
+   type Protected_Mutex_Type is new Soft_Links.Mutex_Type with private;
+
+   function Create return Soft_Links.Mutex_Access;
+
+   procedure Enter (M : in Protected_Mutex_Type);
+
+   procedure Destroy (M : in out Protected_Mutex_Type);
+
+   procedure Leave (M : in Protected_Mutex_Type);
+
+   ----------------------------------
+   -- Watcher for PCS with Tasking --
+   ----------------------------------
+
+   type Protected_Watcher_Type is new Soft_Links.Watcher_Type with private;
+
+   function Create return Soft_Links.Watcher_Access;
+
+   procedure Destroy (W : in out Protected_Watcher_Type);
+
+   procedure Differ
+     (W : in Protected_Watcher_Type;
+      V : in Soft_Links.Version_Id);
+
+   procedure Lookup
+     (W : in Protected_Watcher_Type;
+      V : out Soft_Links.Version_Id);
+
+   procedure Update (W : in Protected_Watcher_Type);
+
+   -----------------------------------------
+   -- Advanced Mutex for PCS with Tasking --
+   -----------------------------------------
+
+   type Protected_Adv_Mutex_Type is new Soft_Links.Adv_Mutex_Type with private;
+
+   function Create return Soft_Links.Adv_Mutex_Access;
+
+   procedure Enter (M : in Protected_Adv_Mutex_Type);
+
+   procedure Destroy (M : in out Protected_Adv_Mutex_Type);
+
+   procedure Leave (M : in Protected_Adv_Mutex_Type);
+
+private
+
+   type Barrier_PO;
+
+   type Barrier_PO_Access is access Barrier_PO;
+
+   type Protected_Barrier_Type is new Soft_Links.Barrier_Type
+     with record
+        X : Barrier_PO_Access;
+     end record;
+
+
+   type Mutex_PO;
+
+   type Mutex_PO_Access is access Mutex_PO;
+
+   type Protected_Mutex_Type is new Soft_Links.Mutex_Type
+     with record
+        X : Mutex_PO_Access;
+     end record;
+
+   type Watcher_PO;
+
+   type Watcher_PO_Access is access Watcher_PO;
+
+   type Protected_Watcher_Type is new Soft_Links.Watcher_Type
+     with record
+        X : Watcher_PO_Access;
+     end record;
+
+
+   type Adv_Mutex_PO;
+
+   type Adv_Mutex_PO_Access is access Adv_Mutex_PO;
+
+   type Protected_Adv_Mutex_Type is new Soft_Links.Adv_Mutex_Type
+     with record
+        X : Adv_Mutex_PO_Access;
+     end record;
+
+end Broca.Protected_Objects;
---
+++ broca/src/broca-soft_links.ads
@@ -0,0 +1,200 @@
+package Broca.Soft_Links is
+
+   pragma Elaborate_Body;
+
+   --  This package allows soft links to be defined and later called if they
+   --  have been installed. The purpose of this is to be able not to register
+   --  certain services that require tasking or other high-level features
+   --  in order to provide the user with a light run time system.
+
+   type Version_Id is mod 2 ** 8;
+   No_Version : constant Version_Id := 0;
+
+   function "<" (L, R : Version_Id) return Boolean;
+
+   -------------------
+   -- General types --
+   -------------------
+
+   type Parameterless_Procedure is access procedure;
+
+   -------------------------------
+   -- Critical section handling --
+   -------------------------------
+
+   procedure Register_Enter_Critical_Section
+     (P : in Parameterless_Procedure);
+   procedure Enter_Critical_Section;
+
+   procedure Register_Leave_Critical_Section
+     (P : in Parameterless_Procedure);
+   procedure Leave_Critical_Section;
+
+   -------------
+   -- Barrier --
+   -------------
+
+   type Barrier_Type is abstract tagged null record;
+
+   procedure Destroy (B : in out Barrier_Type) is abstract;
+
+   procedure Signal
+     (B : in Barrier_Type;
+      N : in Positive := 1) is abstract;
+   --  Release N processes waiting on the barrier.
+
+   procedure Signal_All
+     (B : in Barrier_Type;
+      P : in Boolean := True) is abstract;
+   --  Release all processes waiting on the barrier. If P is true,
+   --  this barrier is no longer blocking.
+
+   procedure Wait (B : in Barrier_Type) is abstract;
+
+   type Barrier_Access is access all Barrier_Type'Class;
+
+   type Barrier_Creation_Function is
+     access function return Barrier_Access;
+
+   procedure Register_Barrier_Creation_Function
+     (F : in Barrier_Creation_Function);
+
+   procedure Create (B : out Barrier_Access);
+   pragma Inline (Create);
+
+   procedure Destroy (B : in out Barrier_Access);
+   pragma Inline (Destroy);
+
+   procedure Signal
+     (B : in Barrier_Access;
+      N : in Positive := 1);
+   pragma Inline (Signal);
+   --  Release N processes waiting on the barrier.
+
+   procedure Signal_All
+     (B : in Barrier_Access;
+      P : in Boolean := True);
+   pragma Inline (Signal_All);
+   --  Release all processes waiting on the barrier. If P is true,
+   --  this barrier is no longer blocking.
+
+   procedure Wait (B : in Barrier_Access);
+   pragma Inline (Wait);
+
+   -----------
+   -- Mutex --
+   -----------
+
+   type Mutex_Type is abstract tagged null record;
+
+   procedure Enter (M : in Mutex_Type) is abstract;
+
+   procedure Destroy (M : in out Mutex_Type) is abstract;
+
+   procedure Leave (M : in Mutex_Type) is abstract;
+
+   type Mutex_Access is access all Mutex_Type'Class;
+
+   type Mutex_Creation_Function is
+     access function return Mutex_Access;
+
+   procedure Register_Mutex_Creation_Function
+     (F : in Mutex_Creation_Function);
+
+   procedure Create (M : out Mutex_Access);
+   pragma Inline (Create);
+
+   procedure Enter (M : in Mutex_Access);
+   pragma Inline (Enter);
+
+   procedure Destroy (M : in out Mutex_Access);
+   pragma Inline (Destroy);
+
+   procedure Leave (M : in Mutex_Access);
+   pragma Inline (Leave);
+
+   -------------
+   -- Watcher --
+   -------------
+
+   type Watcher_Type is abstract tagged null record;
+
+   procedure Destroy (W : in out Watcher_Type) is abstract;
+
+   procedure Differ
+     (W : in Watcher_Type;
+      V : in Version_Id) is abstract;
+   --  Await until W version differs from V
+
+   procedure Lookup
+     (W : in Watcher_Type;
+      V : out Version_Id) is abstract;
+   --  Fetch W version
+
+   procedure Update (W : in Watcher_Type) is abstract;
+   --  Increment W version
+
+   type Watcher_Access is access all Watcher_Type'Class;
+
+   type Watcher_Creation_Function is
+     access function return Watcher_Access;
+
+   procedure Register_Watcher_Creation_Function
+     (F : in Watcher_Creation_Function);
+
+   procedure Create (W : out Watcher_Access);
+   pragma Inline (Create);
+
+   procedure Destroy (W : in out Watcher_Access);
+   pragma Inline (Destroy);
+
+   procedure Differ (W : in Watcher_Access; V : in Version_Id);
+   pragma Inline (Differ);
+   --  Await until W version differs from V
+
+   procedure Lookup (W : in Watcher_Access; V : out Version_Id);
+   pragma Inline (Lookup);
+   --  Fetch W version
+
+   procedure Update (W : in Watcher_Access);
+   pragma Inline (Update);
+   --  Increment W version
+
+   --------------------
+   -- Advanced Mutex --
+   --------------------
+
+   --  This is a classical mutual exclusion object except that when a
+   --  task try to Enter a mutex several times without leaving it
+   --  first it is not blocked and can continue. Leave keeps track of
+   --  the number of times Enter has been successful.
+
+   type Adv_Mutex_Type is abstract tagged null record;
+
+   procedure Enter (M : in Adv_Mutex_Type) is abstract;
+
+   procedure Destroy (M : in out Adv_Mutex_Type) is abstract;
+
+   procedure Leave (M : in Adv_Mutex_Type) is abstract;
+
+   type Adv_Mutex_Access is access all Adv_Mutex_Type'Class;
+
+   type Adv_Mutex_Creation_Function is
+     access function return Adv_Mutex_Access;
+
+   procedure Register_Adv_Mutex_Creation_Function
+     (F : in Adv_Mutex_Creation_Function);
+
+   procedure Create (M : out Adv_Mutex_Access);
+   pragma Inline (Create);
+
+   procedure Enter (M : in Adv_Mutex_Access);
+   pragma Inline (Enter);
+
+   procedure Destroy (M : in out Adv_Mutex_Access);
+   pragma Inline (Destroy);
+
+   procedure Leave (M : in Adv_Mutex_Access);
+   pragma Inline (Leave);
+
+end Broca.Soft_Links;
