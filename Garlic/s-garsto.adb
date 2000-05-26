a1b8541ef38235d3e47b6cb9c0bceff13cd4f1ce
Laurent Pautet <pautet@adacore.com>
2000-05-26 17:28:14 +0000

    * s-garsto.adb: Catch properly error that occurs when the user  	tries to use a data storage support that is not available.
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 47473

--- Garlic/s-garsto.adb
+++ Garlic/s-garsto.adb
@@ -1,3 +1,38 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE COMPONENTS                              --
+--                                                                          --
+--               S Y S T E M . G A R L I C . S T O R A G E S                --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                            $Revision$
+--                                                                          --
+--         Copyright (C) 1996-2000 Free Software Foundation, Inc.           --
+--                                                                          --
+-- GARLIC is free software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 2,  or (at your option)  any later ver- --
+-- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License  distributed with GARLIC;  see file COPYING.  If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--               GLADE  is maintained by ACT Europe.                        --
+--               (email: glade-report@act-europe.fr)                        --
+--                                                                          --
+------------------------------------------------------------------------------
+
 with Ada.Exceptions; use Ada.Exceptions;
 with Ada.Streams;    use Ada.Streams;
 
@@ -285,6 +320,11 @@ package body System.Garlic.Storages is
 
       if SST.Get (Par_Name'Unrestricted_Access) = null then
          Master := Lookup_Storage (Location);
+         if Master = null then
+            Raise_Exception
+              (Program_Error'Identity,
+               "cannot find data storage for partition" & Partition'Img);
+         end if;
          Create_Storage (Master.all, Minor (Location), Storage);
          SST.Set (new String'(Par_Name), Storage);
       end if;
