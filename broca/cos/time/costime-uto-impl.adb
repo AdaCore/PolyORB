2a9f30879ddca91bfb7d446933cbf8f034ad5109
Samuel Tardieu <sam@inf.enst.fr>
2000-06-05 17:08:57 +0000

    Style and headers fixes.
    [Imported from Perforce change 1742 at 2006-12-01 18:57:44]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 32283

--- broca/cos/time/costime-uto-impl.adb
+++ broca/cos/time/costime-uto-impl.adb
@@ -1,58 +1,55 @@
-----------------------------------------------
---  This file has been generated automatically
---  by AdaBroker (http://adabroker.eu.org/)
-----------------------------------------------
+------------------------------------------------------------------------------
+--                                                                          --
+--                          ADABROKER COMPONENTS                            --
+--                                                                          --
+--                     C O S T I M E . U T O . I M P L                      --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--          Copyright (C) 1999-2000 ENST Paris University, France.          --
+--                                                                          --
+-- AdaBroker is free software; you  can  redistribute  it and/or modify it  --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. AdaBroker  is distributed  in the hope that it will be  useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with AdaBroker; see file COPYING. If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--             AdaBroker is maintained by ENST Paris University.            --
+--                     (email: broker@inf.enst.fr)                          --
+--                                                                          --
+------------------------------------------------------------------------------
 
-with TimeBase; use TimeBase;
-with CosTime.UTO.Skel;
-with CosTime.UTO.Helper;
-with CosTime.TIO.Impl;
-with CosTime.TIO.Helper;
-with Interfaces.C;
-with System;
-with Time_Utils; use Time_Utils;
 with Broca.Basic_Startup;
+with CosTime.TIO.Helper;
+with CosTime.TIO.Impl;
+with CosTime.UTO.Helper;
+with CosTime.UTO.Skel;
+with Interfaces.C;
 with PortableServer;
+with System;
+with TimeBase;           use TimeBase;
+with Time_Utils;         use Time_Utils;
 
 package body CosTime.UTO.Impl is
 
    type UTO_Ptr is access Object;
    type TIO_Ptr is access CosTime.TIO.Impl.Object;
 
-   function get_time
-     (Self : access Object)
-     return TimeT
-   is
-   begin
-      return Self.Time;
-   end get_time;
-
-   function get_inaccuracy
-     (Self : access Object)
-     return InaccuracyT
-   is
-   begin
-      return Self.Inaccuracy;
-   end get_inaccuracy;
-
-   function get_tdf
-     (Self : access Object)
-     return TdfT
-   is
-   begin
-      return Self.Tdf;
-   end get_tdf;
-
-   function get_utc_time
-     (Self : access Object)
-     return UtcT
-   is
-   begin
-      return (time    => Self.Time,
-              inacclo => CORBA.Unsigned_Long (Self.Inaccuracy rem (2 ** 32)),
-              inacchi => CORBA.Unsigned_Short (Self.Inaccuracy / (2 ** 32)),
-              tdf     => Self.Tdf);
-   end get_utc_time;
+   -------------------
+   -- absolute_time --
+   -------------------
 
    function absolute_time
      (Self : access Object)
@@ -68,6 +65,10 @@ package body CosTime.UTO.Impl is
         (PortableServer.Servant (Result), R);
       return Helper.To_Ref (R);
    end absolute_time;
+
+   ------------------
+   -- compare_time --
+   ------------------
 
    function compare_time
      (Self : access Object;
@@ -99,6 +100,80 @@ package body CosTime.UTO.Impl is
       end if;
    end compare_time;
 
+   --------------------
+   -- get_inaccuracy --
+   --------------------
+
+   function get_inaccuracy
+     (Self : access Object)
+     return InaccuracyT
+   is
+   begin
+      return Self.Inaccuracy;
+   end get_inaccuracy;
+
+   -------------
+   -- get_tdf --
+   -------------
+
+   function get_tdf
+     (Self : access Object)
+     return TdfT
+   is
+   begin
+      return Self.Tdf;
+   end get_tdf;
+
+   --------------
+   -- get_time --
+   --------------
+
+   function get_time
+     (Self : access Object)
+     return TimeT
+   is
+   begin
+      return Self.Time;
+   end get_time;
+
+   ------------------
+   -- get_utc_time --
+   ------------------
+
+   function get_utc_time
+     (Self : access Object)
+     return UtcT
+   is
+   begin
+      return (time    => Self.Time,
+              inacclo => CORBA.Unsigned_Long (Self.Inaccuracy rem (2 ** 32)),
+              inacchi => CORBA.Unsigned_Short (Self.Inaccuracy / (2 ** 32)),
+              tdf     => Self.Tdf);
+   end get_utc_time;
+
+   --------------
+   -- interval --
+   --------------
+
+   function interval
+     (Self : access Object)
+     return TIO_Forward.Ref
+   is
+      Result : constant TIO_Ptr := new CosTime.TIO.Impl.Object;
+      R      : CORBA.Object.Ref;
+   begin
+      Result.Interval.Lower_Bound := Self.Time - Self.Tdf;
+      Result.Interval.Upper_Bound := Self.Time + Self.Tdf;
+      Broca.Basic_Startup.Initiate_Servant
+        (PortableServer.Servant (Result), R);
+      return CosTime.TIO.Convert_Forward.To_Forward
+        (CosTime.TIO.Helper.To_Ref (R));
+   end interval;
+
+   ----------------------
+   -- time_to_interval --
+   ----------------------
+
    function time_to_interval
      (Self : access Object;
       uto : in Ref)
@@ -115,20 +190,5 @@ package body CosTime.UTO.Impl is
       return CosTime.TIO.Convert_Forward.To_Forward
         (CosTime.TIO.Helper.To_Ref (R));
    end time_to_interval;
-
-   function interval
-     (Self : access Object)
-     return TIO_Forward.Ref
-   is
-      Result : constant TIO_Ptr := new CosTime.TIO.Impl.Object;
-      R      : CORBA.Object.Ref;
-   begin
-      Result.Interval.Lower_Bound := Self.Time - Self.Tdf;
-      Result.Interval.Upper_Bound := Self.Time + Self.Tdf;
-      Broca.Basic_Startup.Initiate_Servant
-        (PortableServer.Servant (Result), R);
-      return CosTime.TIO.Convert_Forward.To_Forward
-        (CosTime.TIO.Helper.To_Ref (R));
-   end interval;
 
 end CosTime.UTO.Impl;
