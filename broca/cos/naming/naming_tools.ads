db0802846ea3979e1554e6b088800fac9ee27b72
Samuel Tardieu <sam@inf.enst.fr>
2000-06-23 13:41:12 +0000

    Add missing files
    [Imported from Perforce change 1934 at 2006-12-01 19:05:38]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 32458

---
+++ broca/cos/naming/naming_tools.ads
@@ -0,0 +1,17 @@
+with CORBA.Object;
+
+package Naming_Tools is
+
+   --  This package allows an object to be chosen either by its IOR or by
+   --  its name in the naming service.
+
+   function Locate (IOR_Or_Name : String) return CORBA.Object.Ref;
+   --  Locate an object by IOR or name
+
+   procedure Register
+     (Name   : in String;
+      Ref    : in CORBA.Object.Ref;
+      Rebind : in Boolean := False);
+   --  Register an object by its name by binding or rebinding
+
+end Naming_Tools;
