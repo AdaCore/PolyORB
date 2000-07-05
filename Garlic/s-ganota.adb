90653d8d6c0d2ffaf10ff60a903f4f885f75a242
Laurent Pautet <pautet@adacore.com>
2000-07-05 16:45:08 +0000

    * s-ganota.adb, s-ganota.ads: Add unit System.Garlic.No_Tasking  	which provides soft links for the light PCS - no tasking loaded in  	partition.
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 47492

---
+++ Garlic/s-ganota.adb
@@ -0,0 +1,220 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE COMPONENTS                              --
+--                                                                          --
+--              S Y S T E M . G A R L I C . N O _ T A S K I N G             --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                            $Revision$
+--                                                                          --
+--         Copyright (C) 1996-2000 Free Software Foundation, Inc.           --
+--                                                                          --
+-- GARLIC is free software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 2,  or (at your option)  any later ver- --
+-- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License  distributed with GARLIC;  see file COPYING.  If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--               GLADE  is maintained by ACT Europe.                        --
+--               (email: glade-report@act-europe.fr)                        --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with System.Garlic.Protocols;    use System.Garlic.Protocols;
+with System.Garlic.Soft_Links;   use System.Garlic.Soft_Links;
+with System.Garlic.Types;   use System.Garlic.Types;
+
+package body System.Garlic.No_Tasking is
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create (V : in Version_Id) return Watcher_Access is
+   begin
+      return new Unprotected_Watcher_Type'(Version => V);
+   end Create;
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create return Adv_Mutex_Access is
+   begin
+      return new Unprotected_Adv_Mutex_Type;
+   end Create;
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create return Mutex_Access is
+   begin
+      return new Unprotected_Mutex_Type;
+   end Create;
+
+   -------------
+   -- Destroy --
+   -------------
+
+   procedure Destroy (W : in out Unprotected_Watcher_Type) is
+   begin
+      null;
+   end Destroy;
+
+   -------------
+   -- Destroy --
+   -------------
+
+   procedure Destroy (M : in out Unprotected_Adv_Mutex_Type) is
+   begin
+      null;
+   end Destroy;
+
+   -------------
+   -- Destroy --
+   -------------
+
+   procedure Destroy (M : in out Unprotected_Mutex_Type) is
+   begin
+      null;
+   end Destroy;
+
+   ------------
+   -- Differ --
+   ------------
+
+   procedure Differ (W : in Unprotected_Watcher_Type; V : in Version_Id) is
+   begin
+      Receive_From_All_Protocols;
+   end Differ;
+
+   -----------
+   -- Enter --
+   -----------
+
+   procedure Enter (M : in Unprotected_Mutex_Type) is
+   begin
+      null;
+   end Enter;
+
+   -----------
+   -- Enter --
+   -----------
+
+   procedure Enter (M : in Unprotected_Adv_Mutex_Type) is
+   begin
+      null;
+   end Enter;
+
+   ----------------------------
+   -- Enter_Critical_Section --
+   ----------------------------
+
+   procedure Enter_Critical_Section is
+   begin
+      null;
+   end Enter_Critical_Section;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize is
+   begin
+      Register_Enter_Critical_Section (Enter_Critical_Section'Access);
+      Register_Leave_Critical_Section (Leave_Critical_Section'Access);
+      Register_Watcher_Creation_Function (Create'Access);
+      Register_Mutex_Creation_Function (Create'Access);
+      Register_Adv_Mutex_Creation_Function (Create'Access);
+      Register_Is_Environment_Task (Is_Environment_Task'Access);
+      Register_Env_Task_Awake_Count (Env_Task_Awake_Count'Access);
+      Register_Independent_Task_Count (Independent_Task_Count'Access);
+      Register_Get_Priority (Get_Priority'Access);
+      Register_Set_Priority (Set_Priority'Access);
+   end Initialize;
+
+   -----------
+   -- Leave --
+   -----------
+
+   procedure Leave (M : in Unprotected_Adv_Mutex_Type) is
+   begin
+      null;
+   end Leave;
+
+   -----------
+   -- Leave --
+   -----------
+
+   procedure Leave (M : in Unprotected_Mutex_Type) is
+   begin
+      null;
+   end Leave;
+
+   ----------------------------
+   -- Leave_Critical_Section --
+   ----------------------------
+
+   procedure Leave_Critical_Section is
+   begin
+      null;
+   end Leave_Critical_Section;
+
+   ------------
+   -- Lookup --
+   ------------
+
+   procedure Lookup (W : in Unprotected_Watcher_Type; V : out Version_Id) is
+   begin
+      V := W.Version;
+   end Lookup;
+
+   ------------
+   -- Update --
+   ------------
+
+   procedure Update (W : in out Unprotected_Watcher_Type) is
+   begin
+      W.Version := W.Version + 1;
+   end Update;
+
+   function Is_Environment_Task return Boolean is
+   begin
+      return True;
+   end Is_Environment_Task;
+
+   function Env_Task_Awake_Count return Natural is
+   begin
+      return 1;
+   end Env_Task_Awake_Count;
+
+   function Independent_Task_Count return Natural is
+   begin
+      return 0;
+   end Independent_Task_Count;
+
+   function Get_Priority return Natural is
+   begin
+      return Natural (System.Default_Priority);
+   end Get_Priority;
+
+   procedure Set_Priority (P : in Natural) is
+   begin
+      null;
+   end Set_Priority;
+
+end System.Garlic.No_Tasking;
---
+++ Garlic/s-ganota.ads
@@ -0,0 +1,126 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            GLADE COMPONENTS                              --
+--                                                                          --
+--               S Y S T E M . G A R L I C . N O _ T A S K I N G            --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--                            $Revision$
+--                                                                          --
+--         Copyright (C) 1996-2000 Free Software Foundation, Inc.           --
+--                                                                          --
+-- GARLIC is free software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 2,  or (at your option)  any later ver- --
+-- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
+-- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License  distributed with GARLIC;  see file COPYING.  If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--               GLADE  is maintained by ACT Europe.                        --
+--               (email: glade-report@act-europe.fr)                        --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with System.Garlic.Soft_Links;
+with System.Garlic.Types;
+
+package System.Garlic.No_Tasking is
+
+   procedure Initialize;
+
+   ----------------------------------------------
+   -- Critical Section for PCS without Tasking --
+   ----------------------------------------------
+
+   procedure Enter_Critical_Section;
+
+   procedure Leave_Critical_Section;
+
+   --------------------------------
+   -- Mutex for PCS with Tasking --
+   --------------------------------
+
+   type Unprotected_Mutex_Type is
+     new Soft_Links.Mutex_Type with private;
+
+   function Create return Soft_Links.Mutex_Access;
+
+   procedure Enter (M : in Unprotected_Mutex_Type);
+
+   procedure Destroy (M : in out Unprotected_Mutex_Type);
+
+   procedure Leave (M : in Unprotected_Mutex_Type);
+
+   -------------------------------------
+   -- Watcher for PCS without Tasking --
+   -------------------------------------
+
+   type Unprotected_Watcher_Type is
+     new Soft_Links.Watcher_Type with private;
+
+   function Create (V : in Types.Version_Id) return Soft_Links.Watcher_Access;
+
+   procedure Destroy (W : in out Unprotected_Watcher_Type);
+
+   procedure Differ
+     (W : in Unprotected_Watcher_Type;
+      V : in Types.Version_Id);
+
+   procedure Lookup
+     (W : in Unprotected_Watcher_Type;
+      V : out Types.Version_Id);
+
+   procedure Update (W : in out Unprotected_Watcher_Type);
+
+   --------------------------------------------
+   -- Advanced Mutex for PCS without Tasking --
+   --------------------------------------------
+
+   type Unprotected_Adv_Mutex_Type is
+     new Soft_Links.Adv_Mutex_Type with private;
+
+   function Create return Soft_Links.Adv_Mutex_Access;
+
+   procedure Enter (M : in Unprotected_Adv_Mutex_Type);
+
+   procedure Destroy (M : in out Unprotected_Adv_Mutex_Type);
+
+   procedure Leave (M : in Unprotected_Adv_Mutex_Type);
+
+   function Is_Environment_Task return Boolean;
+
+   function Env_Task_Awake_Count return Natural;
+
+   function Independent_Task_Count return Natural;
+
+   function Get_Priority return Natural;
+
+   procedure Set_Priority (P : in Natural);
+
+private
+
+   type Unprotected_Mutex_Type is
+     new Soft_Links.Mutex_Type with null record;
+
+   type Unprotected_Watcher_Type is
+     new Soft_Links.Watcher_Type with
+      record
+         Version : Types.Version_Id;
+      end record;
+
+   type Unprotected_Adv_Mutex_Type is
+     new Soft_Links.Adv_Mutex_Type with null record;
+
+end System.Garlic.No_Tasking;
