83d1cfb128857496da10c529146a6ab7802a7432
Laurent Pautet <pautet@adacore.com>
2000-12-14 17:48:20 +0000

    * 5lgarcon.ads: Compute SO_KEEPALIVE for Linux.
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 47610

--- Garlic/5lgarcon.ads
+++ Garlic/5lgarcon.ads
@@ -1,40 +1,7 @@
-------------------------------------------------------------------------------
---                                                                          --
---                            GLADE COMPONENTS                              --
---                                                                          --
---              S Y S T E M . G A R L I C . C O N S T A N T S               --
---                                                                          --
---                                 S p e c                                  --
---                                                                          --
---                            $Revision$
---                                                                          --
---         Copyright (C) 1996-2000 Free Software Foundation, Inc.           --
---                                                                          --
--- GARLIC is free software;  you can redistribute it and/or modify it under --
--- terms of the  GNU General Public License  as published by the Free Soft- --
--- ware Foundation;  either version 2,  or (at your option)  any later ver- --
--- sion.  GARLIC is distributed  in the hope that  it will be  useful,  but --
--- WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABI- --
--- LITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public  --
--- License  for more details.  You should have received  a copy of the GNU  --
--- General Public License  distributed with GARLIC;  see file COPYING.  If  --
--- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
--- Boston, MA 02111-1307, USA.                                              --
---                                                                          --
--- As a special exception,  if other files  instantiate  generics from this --
--- unit, or you link  this unit with other files  to produce an executable, --
--- this  unit  does not  by itself cause  the resulting  executable  to  be --
--- covered  by the  GNU  General  Public  License.  This exception does not --
--- however invalidate  any other reasons why  the executable file  might be --
--- covered by the  GNU Public License.                                      --
---                                                                          --
---               GLADE  is maintained by ACT Europe.                        --
---               (email: glade-report@act-europe.fr)                        --
---                                                                          --
-------------------------------------------------------------------------------
-
---  This is the version for Linux
-
+--  This package has been generated automatically on:
+--  Linux arlequin 2.2.17 #2 Wed Oct 18 18:43:33 CEST 2000 i686 unknown
+--  Generation date: Thu Dec 14 18:39:17 CET 2000
+--  Any change you make here is likely to be lost !
 package System.Garlic.Constants is
    Tcp_Nodelay          : constant := 16#0001#;
    Af_Inet              : constant := 16#0002#;
@@ -55,6 +22,7 @@ package System.Garlic.Constants is
    F_Setown             : constant := 16#0008#;
    So_Rcvbuf            : constant := 16#0008#;
    So_Reuseaddr         : constant := 16#0002#;
+   So_Keepalive         : constant := 16#0009#;
    Sol_Socket           : constant := 16#0001#;
    Sigterm              : constant := 16#000F#;
    Sigkill              : constant := 16#0009#;
@@ -72,7 +40,7 @@ package System.Garlic.Constants is
    Pollerr              : constant := 16#0008#;
    Pollhup              : constant := 16#0010#;
    Pollnval             : constant := 16#0020#;
-   I_Setsig             : constant := 16#0006#;
-   S_Rdnorm             : constant := 16#0001#;
-   S_Wrnorm             : constant := 16#0010#;
+   I_Setsig             : constant := 16#5309#;
+   S_Rdnorm             : constant := 16#0040#;
+   S_Wrnorm             : constant := 16#0004#;
 end System.Garlic.Constants;
