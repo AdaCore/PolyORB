e95aab4aabe8546a39b84c4f187684831de35a38
Laurent Pautet <pautet@adacore.com>
2001-01-25 16:25:37 +0000

    * s-gaprtc.adb: Pass Process_Blocking_IO to 	GNAT.Sockets.Initialize.
    Subversion-branch: /importfromcvs/trunk
    Subversion-revision: 47655

--- Garlic/s-gaprtc.adb
+++ Garlic/s-gaprtc.adb
@@ -44,6 +44,7 @@ with System.Garlic.Heart;                 use System.Garlic.Heart;
 with System.Garlic.Options;
 with System.Garlic.Partitions;            use System.Garlic.Partitions;
 with System.Garlic.Physical_Location;     use System.Garlic.Physical_Location;
+with System.Garlic.Platform_Specific;
 with System.Garlic.Protocols;             use System.Garlic.Protocols;
 with System.Garlic.Soft_Links;
 with System.Garlic.Streams;               use System.Garlic.Streams;
@@ -447,7 +448,7 @@ package body System.Garlic.Protocols.Tcp is
       if not Initialized then
          pragma Debug (D ("Initialize GNAT.sockets for protocol tcp"));
          Outgoings.Initialize;
-         GNAT.Sockets.Initialize;
+         GNAT.Sockets.Initialize (Platform_Specific.Process_Blocking_IO);
          Initialized := True;
       end if;
 
@@ -1038,7 +1039,7 @@ package body System.Garlic.Protocols.Tcp is
       if not Initialized then
          pragma Debug (D ("Initialize protocol tcp"));
          Outgoings.Initialize;
-         GNAT.Sockets.Initialize;
+         GNAT.Sockets.Initialize (Platform_Specific.Process_Blocking_IO);
          Initialized := True;
       end if;
 
