d6e8ead7d3872ddeefc60ab2ed43220eff4b3242
Sébastien Ponce <ponce@enst.fr>
2001-04-20 18:13:06 +0000

    fix to change 3010. p4 add corba-fixed.ad? was forgotten
    [Imported from Perforce change 3012 at 2006-12-01 19:16:53]
    
    Subversion-branch: /trunk/adabroker
    Subversion-revision: 32763

---
+++ broca/src/corba-fixed_point.ads
@@ -0,0 +1,47 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          ADABROKER COMPONENTS                            --
+--                                                                          --
+--                         C O R B A . O B J E C T                          --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--          Copyright (C) 1999-2001 ENST Paris University, France.          --
+--                                                                          --
+-- AdaBroker is free software; you  can  redistribute  it and/or modify it  --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. AdaBroker  is distributed  in the hope that it will be  useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with AdaBroker; see file COPYING. If  --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--             AdaBroker is maintained by ENST Paris University.            --
+--                     (email: broker@inf.enst.fr)                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  this subpackage is used for the implementation of the any type
+--  in case of a fixed point numeric value.
+--  It defines generic methods to manipulate this kind of any.
+
+generic
+   type F is delta <> digits <>;
+
+package CORBA.Fixed_Point is
+
+   function To_Any (Item : in F)  return Any;
+
+   function From_Any (Item : in Any) return F;
+
+end CORBA.Fixed_Point;
