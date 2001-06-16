a8bb3cd9223198a35c74bad39bba35a57940fc69
Thomas Quinot <quinot@adacore.com>
2001-06-16 01:49:03 +0000

    Update documentation.
    [Imported from Perforce change 3213 at 2006-12-01 19:19:21]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 32890

--- src/droopi-orb-interface.ads
+++ src/droopi-orb-interface.ads
@@ -23,8 +23,11 @@ package Droopi.ORB.Interface is
    --  Queue method invocation request Req for execution by Server
    --  on behalf of a remote caller. No reply expected.
    --  When the request is executed, a message will be sent
-   --  back to Requestor (asynchronously).
-   --  The client is the responsible of the destruction of
-   --  the Request.
+   --  back to Requestor (asynchronously). If this request comes
+   --  from a Session, Requestor must be set to that Session.
+   --  If the request is submitted directly by a local client task,
+   --  Requestor must be set to null.
+   --  The client the responsible of the destruction of
+   --  the Request after its execution is completed.
 
 end Droopi.ORB.Interface;
