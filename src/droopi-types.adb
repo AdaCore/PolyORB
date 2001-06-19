439aac00811eec7dbb33feaa7ab4625287de2309
Thomas Quinot <quinot@adacore.com>
2001-06-19 17:35:46 +0000

    This is necessary to build DROOPI. Thanks autobuilder for noticing.
    [Imported from Perforce change 3237 at 2006-12-01 19:19:42]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 32906

---
+++ src/droopi-types.adb
@@ -0,0 +1,47 @@
+--  Base data types for the whole middleware.
+
+--  $Id$
+
+package body Droopi.Types is
+
+   ---------------------------------
+   -- String conversion functions --
+   ---------------------------------
+
+   function To_Droopi_String
+     (Source : Standard.String)
+     return Types.String is
+   begin
+      return Types.String
+        (Ada.Strings.Unbounded.To_Unbounded_String
+         (Source));
+   end To_Droopi_String;
+
+   function To_Standard_String
+     (Source : Types.String)
+     return Standard.String is
+   begin
+      return Ada.Strings.Unbounded.To_String
+        (Ada.Strings.Unbounded.Unbounded_String (Source));
+   end To_Standard_String;
+
+   function To_Droopi_Wide_String
+     (Source : Standard.Wide_String)
+     return Types.Wide_String is
+   begin
+      return Types.Wide_String
+        (Ada.Strings.Wide_Unbounded.To_Unbounded_Wide_String
+         (Source));
+   end To_Droopi_Wide_String;
+
+   function To_Standard_Wide_String
+     (Source : Types.Wide_String)
+     return Standard.Wide_String is
+   begin
+      return Ada.Strings.Wide_Unbounded.To_Wide_String
+        (Ada.Strings.Wide_Unbounded.Unbounded_Wide_String
+         (Source));
+   end To_Standard_Wide_String;
+
+
+end Droopi.Types;
