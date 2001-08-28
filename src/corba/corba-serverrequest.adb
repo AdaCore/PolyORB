88e8a1dc3480ebf9e68c593d91f78f88eee7261e
Thomas Quinot <quinot@adacore.com>
2001-08-28 18:34:56 +0000

    Ooops, forgot this one.
    [Imported from Perforce change 3689 at 2006-12-01 19:23:21]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 33022

---
+++ src/corba/corba-serverrequest.adb
@@ -0,0 +1,34 @@
+--  Mapping for the standard ServerRequest interface
+
+--  $Id$
+
+with Droopi;
+with Droopi.Types;
+
+package body CORBA.ServerRequest is
+
+   function Operation (O : Object) return Identifier is
+   begin
+      return O.Operation;
+   end Operation;
+
+   procedure Arguments (O : Object; NV : in out NVList.Ref) is
+   begin
+      NV := CORBA.NVList.To_CORBA_Ref (O.Args);
+   end Arguments;
+
+   procedure Set_Result (O : Object; Val : Any) is
+   begin
+      O.Result :=
+        (Name      => Droopi.Types.To_Droopi_String ("result"),
+         Argument  => Val,
+         Arg_Modes => ARG_OUT);
+   end Set_Result;
+
+   procedure Set_Exception (O : Object; Val : Any) is
+   begin
+      --  O.Exception_Info := Val;
+      raise Droopi.Not_Implemented;
+   end Set_Exception;
+
+end CORBA.ServerRequest;
