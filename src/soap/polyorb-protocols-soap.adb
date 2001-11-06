de10f0bad6e4da4240d8a1f686631fa8d99959f3
Thomas Quinot <quinot@adacore.com>
2001-11-06 12:49:55 +0000

    Re-fix headers. droopi -> polyorb Update logging configuration.
    [Imported from Perforce change 3910 at 2006-12-01 19:28:33]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 33120

--- src/soap/polyorb-protocols-soap.adb
+++ src/soap/polyorb-protocols-soap.adb
@@ -1,3 +1,37 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           POLYORB COMPONENTS                             --
+--                                                                          --
+--              P O L Y O R B . P R O T O C O L S . S O A P                 --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                Copyright (C) 2001 Free Software Fundation                --
+--                                                                          --
+-- PolyORB is free software; you  can  redistribute  it and/or modify it    --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. PolyORB is distributed  in the hope that it will be  useful,    --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with PolyORB; see file COPYING. If    --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--              PolyORB is maintained by ENST Paris University.             --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  $Id$
+
 with Ada.Unchecked_Deallocation;
 with Ada.Streams; use Ada.Streams;
 
@@ -37,7 +71,7 @@ package body PolyORB.Protocols.SOAP  is
    use PolyORB.Representations.SOAP.Any;
    use PolyORB.Buffers;
 
-   package L is new PolyORB.Log.Facility_Log ("droopi.protocols.soap");
+   package L is new PolyORB.Log.Facility_Log ("polyorb.protocols.soap");
    procedure O (Message : in String; Level : Log_Level := Debug)
      renames L.Output;
 
