082a2cfe8c1143b991a35aa9c6c43285af1c6067
Thomas Quinot <quinot@adacore.com>
2001-11-14 16:23:04 +0000

    A GPerf-based keyword recognizer generator.
    [Imported from Perforce change 3961 at 2006-12-01 19:29:04]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 33152

---
+++ src/soap/polyorb-http_keywords.ads
@@ -0,0 +1,22 @@
+package PolyORB.HTTP_Keywords is
+
+   pragma Preelaborate;
+
+   type Methods is (
+      --  <ENUM>
+      OPTIONS,	--  >> "OPTIONS"
+      GET,	--  >> "GET"
+      HEAD,	--  >> "HEAD"
+      POST,	--  >> "HOST"
+      PUT,	--  >> "PUT"
+      DELETE,	--  >> "DELETE"
+      TRACE,	--  >> "TRACE"
+      CONNECT	--  >> "CONNECT"
+      --  </ENUM>
+   );
+   pragma Convention (C, Methods);
+
+   function To_String (Id : Methods) return String;
+   function In_Word_Set (S : String) return Methods;
+
+end PolyORB.HTTP_Keywords;
