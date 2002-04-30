7779bf57fff76ac42315517b5dbedc146a72a7a0
Laurent Pautet <pautet@adacore.com>
2002-04-30 13:16:41 +0000

    Polish comments
    [Imported from Perforce change 4602 at 2006-12-01 19:39:13]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 33520

--- src/polyorb-utils-htables.ads
+++ src/polyorb-utils-htables.ads
@@ -52,71 +52,33 @@ private
    --  to internal tables used by the perfect dynamic hash table
    --  algorithm.
 
-   --  XXXXX If (Used) <=> (Key /= null) then we do not need Used :
-   --  I'm not agree with you. In the algorithm we don't immediatly
-   --  deallocate the Key when an  Element is removed from the table.
-   --  Because if the Element is reinserted we've just to put the flag to
-   --  False. an unused Key is deallocated in this cases :
-   --                - insertion of a Key with the same hashcodes
-   --                - reorganization of the sub_table or the table
-
-
    type Subtable is record
       First  : Natural;
       Last   : Natural;
+      Count  : Natural;
       High   : Natural;
       Max    : Natural;
       K      : Natural;
    end record;
    --  First (resp. Last) is the first (resp. last) subtable index.
-   --  Max is the maximum size of the subtable. When Last - First + 1
+   --  Some slots between First and Last may be unused. Count
+   --  represents the actual number of used elements in the
+   --  subtable. Max is the maximum size of the subtable. When Count
    --  is greater than High, the algorithm reorganizes the subtable
    --  for algorithm purposes. K is a subtable attribute that ensures
    --  h (Key) = ((K * Key) mod Prime) mod Size.
 
-   -- XXXXX Last-First+1 is not equal to the number of elements stored
-   -- in the sub-table :
-   -- Ex :      ---------------- Table Elements (see below in Hash_Table)
-   --           -              -
-   --           ----------------
-   --           -    Key1      - <- First
-   --           ----------------
-   --           -    null      -
-   --           ----------------
-   --           -    Key2      -
-   --           ----------------
-   --           -    null      -
-   --           ----------------
-   --           -    null      - <- Last
-   --           ----------------
-   --
-   -- there the number of element is 2
-   -- the size of the sub_table is last-first+1 (Last and First fix the limits
-   -- of the sub_table in the table of elements : Elements)
-   -- and the condition might be :
-   --       reorganize table when number of elements > 3 (what you called
-   --                                                     High)
-
    type Table_Info is record
-      Prime        : Natural;
-      Length       : Natural;
-      High         : Natural;
-      K            : Natural;
+      Prime : Natural;
+      Count : Natural;
+      High  : Natural;
+      K     : Natural;
    end record;
    --  Prime is a prime number used by the algorithm. It can be
-   --  specified by the user. Length is the actual number of elements
-   --  in the table. When Length > High, the algorithm reorganizes all
-   --  the subtables. K is a table attribute that ensures h (Key) =
-   --  ((K * Key) mod Prime) mod Size.
-
-   -- Why it is duplicated :
-   --  All the hash function have  the form :
-   --  h (Key) = ((K * Key) mod Prime) mod Size.
-   --  but we need two hash functions when a lookup is performed
-   --       - the first to find in which sub_table the element is stored
-   --         (we use Table_Info.K)
-   --       - the second to find where the element is stored in the
-   --         sub_table (there we use Sub_Table.K of the sub_table find above)
+   --  specified by the user. Count is the actual number of used
+   --  elements in the table (see above). When Count > High, the
+   --  algorithm reorganizes all the subtables. K is a table attribute
+   --  that ensures h (Key) = ((K * Key) mod Prime) mod Size.
 
    type Element_Array is array (Natural range <>) of Element;
    type Element_Array_Ptr is access all Element_Array;
