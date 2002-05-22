10885700f34f26583416da31b325278e1aeea70b
Jérôme Hugues <hugues@enst.fr>
2002-05-22 12:08:10 +0000

    Modified tasking policy, required for oneway tests
    [Imported from Perforce change 4680 at 2006-12-01 19:40:13]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 33576

--- examples/all_functions/server.adb
+++ examples/all_functions/server.adb
@@ -7,9 +7,12 @@ with CORBA.ORB;
 with PolyORB.CORBA_P.Server_Tools; use PolyORB.CORBA_P.Server_Tools;
 pragma Elaborate (PolyORB.CORBA_P.Server_Tools);
 
-with PolyORB.Setup.No_Tasking_Server;
-pragma Elaborate_All (PolyORB.Setup.No_Tasking_Server);
-pragma Warnings (Off, PolyORB.Setup.No_Tasking_Server);
+with PolyORB.Setup.Thread_Pool_Server;
+pragma Elaborate_All (PolyORB.Setup.Thread_Pool_Server);
+pragma Warnings (Off, PolyORB.Setup.Thread_Pool_Server);
+
+--  Note : the server must execute two tasks concurrently
+--         to pass oneway tests
 
 with Ada.Text_IO;
 
