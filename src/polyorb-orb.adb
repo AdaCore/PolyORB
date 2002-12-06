a879e162d5fd17f9cf1d19bd9713f608f7c43028
Jérôme Hugues <hugues@enst.fr>
2002-12-06 14:32:01 +0000

    Added debug information.
    [Imported from Perforce change 5963 at 2006-12-01 19:51:11]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 34184

--- src/polyorb-orb.adb
+++ src/polyorb-orb.adb
@@ -430,6 +430,10 @@ package body PolyORB.ORB is
          end loop Check_Condition;
 
          --  ORB_Lock is held.
+         pragma Debug (O ("# of monitors:"
+                          & Integer'Image
+                          (Monitor_Seqs.Length (ORB.Monitors))));
+
 
          if May_Poll
            and then not ORB.Polling
