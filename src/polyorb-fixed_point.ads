267cfb33521b5492b0a8cb9b01967421aa23c600
Thomas Quinot <quinot@adacore.com>
2002-12-06 18:48:27 +0000

    Support routines for fixed point types.
    [Imported from Perforce change 5965 at 2006-12-01 19:51:12]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 34186

---
+++ src/polyorb-fixed_point.ads
@@ -0,0 +1,67 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           POLYORB COMPONENTS                             --
+--                                                                          --
+--                  P O L Y O R B . F I X E D _ P O I N T                   --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--             Copyright (C) 1999-2002 Free Software Fundation              --
+--                                                                          --
+-- PolyORB is free software; you  can  redistribute  it and/or modify it    --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. PolyORB is distributed  in the hope that it will be  useful,    --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with PolyORB; see file COPYING. If    --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--              PolyORB is maintained by ENST Paris University.             --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  Generic representation for fixed point types as an array
+--  of BCD nibbles followed by a sign indication.
+
+--  $Id: //droopi/main/src/polyorb-fixed_point.ads#1 $
+
+package PolyORB.Fixed_Point is
+
+   pragma Pure;
+
+   type Nibble is range 16#0# .. 16#f#;
+
+   subtype Decimal_Nibble is Nibble range 0 .. 9;
+
+   Positive_Zero : constant Nibble;
+   Negative      : constant Nibble;
+
+   type Nibbles is array (Integer range <>) of Nibble;
+   pragma Pack (Nibbles);
+
+   generic
+      type F is delta <> digits <>;
+   package Fixed_Point_Conversion is
+      function Fixed_To_Nibbles (Data : in F) return Nibbles;
+      function Nibbles_To_Fixed (Data : in Nibbles) return F;
+   end Fixed_Point_Conversion;
+
+private
+
+   Fixed_Positive_Zero : constant := 16#c#;
+   Fixed_Negative      : constant := 16#d#;
+
+   Positive_Zero : constant Nibble := Fixed_Positive_Zero;
+   Negative      : constant Nibble := Fixed_Negative;
+
+end PolyORB.Fixed_Point;
---
+++ testsuite/core/fixed_point/test000.adb
@@ -0,0 +1,39 @@
+with Ada.Exceptions;
+with PolyORB.Fixed_Point; use PolyORB.Fixed_Point;
+with Ada.Text_IO; use Ada.Text_IO;
+with Report; use Report;
+
+procedure Test000 is
+   type Megabucks is delta 0.01 digits 15;
+   package Megabucks_Conv is
+     new PolyORB.Fixed_Point.Fixed_Point_Conversion (Megabucks);
+   use Megabucks_Conv;
+
+   Values : array (Integer range <>) of Megabucks :=
+     (0.0, 0.01, 0.05, 1.23, -1.0, 12345.67, 123456.78, -0.01,
+      9999.99, 99999.99);
+
+   Hex_Digit : constant array (Nibble) of Character := "0123456789abcdef";
+begin
+   for I in Values'Range loop
+      begin
+         declare
+            R : constant Nibbles := Fixed_To_Nibbles (Values (I));
+         begin
+            Put (Values (I)'Img & " ->");
+            for J in R'Range loop
+               Put (" " & Hex_Digit (R (J)));
+            end loop;
+            Put_Line ("");
+            Output
+              ("test " & Megabucks'Image (Values (I)),
+               Values (I) = Nibbles_To_Fixed (R));
+         end;
+      exception
+         when E : others =>
+            Put_Line (Ada.Exceptions.Exception_Information (E));
+            Output ("test " & Megabucks'Image (Values (I)), False);
+      end;
+   end loop;
+   End_Report;
+end Test000;
