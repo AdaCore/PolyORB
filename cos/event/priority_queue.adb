a14fe84e9a9961e6c33cd4023e1c0e8814e26925
Jérémy Gibbons <gibbons@enst.fr>
2003-03-11 13:48:38 +0000

    Fix style problems and add comments in priority_queue files Add priority_queue.adb and .ads to MANIFEST
    [Imported from Perforce change 6448 at 2006-12-01 19:54:38]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 34430

--- cos/event/priority_queue.adb
+++ cos/event/priority_queue.adb
@@ -1,9 +1,44 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           POLYORB CORBA SERVICES                         --
+--                                                                          --
+--                                                                          --
+--                            PRIORITY_QUEUE.ADB                            --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--             Copyright (C) 1999-2002 Free Software Fundation              --
+--                                                                          --
+-- PolyORB is free software; you  can  redistribute  it and/or modify it    --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. PolyORB is distributed  in the hope that it will be  useful,    --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with PolyORB; see file COPYING. If    --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--              PolyORB is maintained by ENST Paris University.             --
+--                                                                          --
+------------------------------------------------------------------------------
+
+
 with Ada.Unchecked_Deallocation;
 with PolyORB.Tasking.Semaphores; use PolyORB.Tasking.Semaphores;
 with PolyORB.Tasking.Soft_Links; use PolyORB.Tasking.Soft_Links;
 
 package body Priority_Queue is
 
+   --  Procedures to free queue elements and the queue itself
    procedure Free_Queue_Elt is
       new Ada.Unchecked_Deallocation (Queue_Elt, Queue_Elt_Ptr);
 
@@ -11,9 +46,9 @@ package body Priority_Queue is
       new Ada.Unchecked_Deallocation
           (Priority_Queue_Type, Priority_Queue_Access);
 
-   ------------------------------
-   --           Create         --
-   ------------------------------
+   ------------
+   -- Create --
+   ------------
 
    procedure Create (PQ : out Priority_Queue_Access;
       The_Factor : in Factor_Range) is
@@ -29,9 +64,9 @@ package body Priority_Queue is
       Create (PQ.Queue_Lock);
    end Create;
 
-   ------------------------------
-   --           Destroy        --
-   ------------------------------
+   -------------
+   -- Destroy --
+   -------------
 
    procedure Destroy (PQ : in out Priority_Queue_Access)
    is
@@ -42,7 +77,7 @@ package body Priority_Queue is
          --  how should we treat elements still in
          --  the queue upon destruction of the queue
          --  for now, these elements are destroyed
-         --  (future QoS parameter ?)
+         --  (maybe a future QoS parameter ?)
          for i in 0 .. 2 ** (PQ.Factor) - 1 loop
             if PQ.Priority_Queues (i) /= null then
                Event := PQ.Priority_Queues (i);
@@ -59,9 +94,9 @@ package body Priority_Queue is
       end if;
    end Destroy;
 
-   ------------------------------
-   --           Insert         --
-   ------------------------------
+   ------------
+   -- Insert --
+   ------------
 
    procedure Insert
     (PQ : in out Priority_Queue_Access;
@@ -73,8 +108,11 @@ package body Priority_Queue is
       Event : Queue_Elt_Ptr;
       Result : Boolean;
    begin
+      --  Mapping of priority onto priority range subdivisions
       Queuenum := (Prio + 32767) / (2**(16-PQ.Factor));
 
+      --  Check for room in the queue
+      --  Discard an element if neccessary
       if State (PQ.Queue_Lock) = PQ.MaxSize and PQ.MaxSize > -1 then
          Discard (PQ, Result);
          if Result = False then
@@ -88,48 +126,60 @@ package body Priority_Queue is
       --  and there is room in the queue to place it
       Event.Data := Data;
       Event.P := Prio;
+      --  Event is of course the last element of its priority
       Event.Last := Event;
       Enter_Critical_Section;
       if PQ.Priority_Queues (Queuenum) = null then
-         --  this queue is empty
+         --  this queue is empty, so simple place Event
+         --  in first place
          PQ.Priority_Queues (Queuenum) := Event;
          Event.Next := null;
          Event.Prev := null;
          Event.Last_Higher := null;
       else
-         --  this queue already has elements
+         --  this queue already has elements, so follow the list
+         --  to the right place for insertion
          Elt := PQ.Priority_Queues (Queuenum);
          if Elt.P < Prio then
-            --  All current elements are of lesser priority
-            --  insert at head of queue
+            --  All current elements are of lesser priority,
+            --  so insert at head of queue
             Event.Next := Elt;
             Event.Prev := null;
             Event.Last_Higher := null;
             Elt.Last_Higher := Event;
             Elt.Last.Last_Higher := Event;
          else
-            --  There are elements of equal or higher priority in this queue
+            --  There are elements of equal or higher priority in this queue,
+            --  so look for right insertion point
             Elt := Elt.Last;
             while Elt.Next /= null and Elt.Next.P >= Prio loop
                Elt := Elt.Next.Last;
             end loop;
             --  Elt is now positioned at the last of element
             --  of priority higher or equal to Prio
+            --  so insert Event at this point
             Event.Next := Elt.Next;
             Event.Prev := Elt;
             Elt.Next := Event;
             if Elt.P = Prio then
-               --  Event is of the same priority as Elt
+               --  Event is of the same priority as Elt,
+               --  so Event is now the last element of that priority
                Elt.Last := Event;
+               --  The last element of higher priority hasn't changed
                Event.Last_Higher := Elt.Last_Higher;
+               --  now modify the Last pointer of the first element
+               --  of this priority
                if Elt.Last_Higher = null then
                   --  Elt is of the highest priority in this queue
+                  --  so the first element cannot be accessed by
+                  --  Last_Higher.Next
                   PQ.Priority_Queues (Queuenum).Last := Event;
                else
                   Elt.Last_Higher.Next.Last := Event;
                end if;
             else
                --  Event is of a different (and therefor new) priority
+               --  so things are much simpler
                Event.Last_Higher := Elt;
             end if;
             if Event.Next /= null then
@@ -170,9 +220,9 @@ package body Priority_Queue is
          return;
    end Insert;
 
-   ------------------------------
-   --           Pop            --
-   ------------------------------
+   ---------
+   -- Pop --
+   ---------
 
    procedure Pop (PQ : in out Priority_Queue_Access;
       Data : out CORBA.Any)
@@ -182,14 +232,17 @@ package body Priority_Queue is
       P (PQ.Queue_Lock);
       --  if you get to here, there is something in the queue
       Enter_Critical_Section;
+      --  Retrieve the relevant data
       Event := PQ.Priority_Queues (PQ.Highest_Priority);
       Data := Event.Data;
+      --  Update the queue's pointers
       PQ.Priority_Queues (PQ.Highest_Priority) := Event.Next;
       if Event.Next = null then
          --  oh no ! there are no other elements of this priority
          --  so find the next priority, if such a thing exists
          if State (PQ.Queue_Lock) > 0 then
-            --  the queue isn't empty yet
+            --  the queue isn't empty yet, so find the next
+            --  highest priority
             while PQ.Highest_Priority > 0 and
                PQ.Priority_Queues (PQ.Highest_Priority) = null loop
                   PQ.Highest_Priority := PQ.Highest_Priority - 1;
@@ -197,7 +250,7 @@ package body Priority_Queue is
             --  we should now have found the next highest priority element
          end if;
       else
-         --  there are other elements left in the queue
+         --  there are other elements left in this queue
          Event.Next.Last_Higher := null;
          Event.Next.Prev := null;
          if Event.Next.P = Event.P then
@@ -230,9 +283,9 @@ package body Priority_Queue is
       Free_Queue_Elt (Event);
    end Pop;
 
-   ------------------------------
-   --      Set_OrderPolicy     --
-   ------------------------------
+   ---------------------
+   -- Set_OrderPolicy --
+   ---------------------
 
    procedure Set_OrderPolicy
       (PQ : in out Priority_Queue_Access;
@@ -245,9 +298,9 @@ package body Priority_Queue is
    end Set_OrderPolicy;
 
 
-   ------------------------------
-   --     Set_MaxQueueSize     --
-   ------------------------------
+   ----------------------
+   -- Set_MaxQueueSize --
+   ----------------------
 
    procedure Set_MaxQueueSize (PQ : in out Priority_Queue_Access;
       Size : in Integer)
@@ -258,27 +311,32 @@ package body Priority_Queue is
       Leave_Critical_Section;
    end Set_MaxQueueSize;
 
-   ------------------------------
-   --          Discard         --
-   ------------------------------
+   -------------
+   -- Discard --
+   -------------
+
+   --  This procedure tries to discard an element
+   --  to make room for a new insertion. It returns true
+   --  if it was able to make room, and false if it couldn't
+   --  or if the discarded element should be the one we're
+   --  trying to insert
 
    procedure Discard (PQ : in out Priority_Queue_Access; Result : out Boolean)
          is
       Event : Queue_Elt_Ptr;
       QueueNum : Integer;
-      Data : CORBA.Any;
    begin
       if State (PQ.Queue_Lock) <= 0 then
          --  there is a problem : Discard was called on an empty queue
          --  so return false so as not to raise false hopes
          Result := False;
          return;
-      elsif PQ.Discard = Fifo and PQ.Order = Fifo then
-         Pop (PQ, Data);
-      elsif PQ.Discard = Fifo and PQ.Order = Priority then
+      elsif PQ.Discard = Fifo then
          --  Get rid of oldest element
-         Event := PQ.LastTime;
+         Event := PQ.FirstTime;
       elsif PQ.Discard = Lifo then
+         --  the easiest case : the newest element should be discarded,
+         --  and that's the one we're trying to insert
          Result := False;
          return;
       elsif PQ.Discard = Priority then
@@ -294,8 +352,10 @@ package body Priority_Queue is
       end if;
       --  we now have an Event to delete
 
+      --  figure out which queue it is in
       QueueNum := (Event.P + 32767) / (2 ** (16 - PQ.Factor));
 
+      --  Update time pointers
       if Event.PrevTime = null then
          PQ.FirstTime := Event.NextTime;
       else
@@ -310,18 +370,26 @@ package body Priority_Queue is
 
       --  Update Last pointers
       if Event.Prev = null or Event.Prev.P > Event.P then
+         --  the event is the first of its priority
          if Event.Next /= null and Event.Next.P = Event.P then
+            --  and there are others of its priority in the queue
+            --  who need updating to maintain queue integrity
             Event.Next.Last := Event.Last;
          end if;
       elsif Event.Next = null or Event.Next.P < Event.P then
+         --  the event is the last of its priority
          if Event.Prev /= null and Event.Prev.P = Event.P then
+            --  and there are others of its priority left
+            --  which also need updating to maintain queue integrity
             Event.Prev.Last := Event.Prev;
          end if;
+         --  Event.Prev is now the last of its priority
          if Event.Last_Higher /= null then
             Event.Last_Higher.Next.Last := Event.Prev.Last;
          end if;
       end if;
 
+      --  Update Next and Prev Pointers
       if Event.Prev /= null then
          Event.Prev.Next := Event.Next;
       else
@@ -331,8 +399,10 @@ package body Priority_Queue is
          Event.Next.Prev := Event.Prev;
       end if;
 
+
       if PQ.Priority_Queues (QueueNum) = null then
          --  we emptied that queue
+         --  so update Highest_- and Lowest_Priority pointers
          if QueueNum = PQ.Highest_Priority then
             while PQ.Highest_Priority > 0 and
                PQ.Priority_Queues (PQ.Highest_Priority) = null loop
@@ -346,7 +416,10 @@ package body Priority_Queue is
             end loop;
          end if;
       end if;
+
+      --  Delete the queue element
       Free_Queue_Elt (Event);
+      --  Tell the caller that there is now room in the queue
       Result := True;
    end Discard;
 
--- cos/event/priority_queue.ads
+++ cos/event/priority_queue.ads
@@ -1,16 +1,71 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           POLYORB CORBA SERVICES                         --
+--                                                                          --
+--                                                                          --
+--                            PRIORITY_QUEUE.ADS                            --
+--                                                                          --
+--                                 S P E C                                  --
+--                                                                          --
+--             Copyright (C) 1999-2002 Free Software Fundation              --
+--                                                                          --
+-- PolyORB is free software; you  can  redistribute  it and/or modify it    --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. PolyORB is distributed  in the hope that it will be  useful,    --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with PolyORB; see file COPYING. If    --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--              PolyORB is maintained by ENST Paris University.             --
+--                                                                          --
+------------------------------------------------------------------------------
 with CORBA;
 with PolyORB.Tasking.Semaphores;
 
+
+--  This package provides a priority-based queue, for use with
+--  a modified COS Event channel.
+--  The data in the queue is of type CORBA.Any, with an associated
+--  priority between -32767 and +32767. In order to provide
+--  performance independent of the number of elements in the queue,
+--  and to make possible use of different element discard policies,
+--  each queue element is linked 6 ways, to its immediate succesor
+--  and predecessor in the queue, to its predecessor and
+--  successor in arrival order, to the last element in the queue
+--  of the same priority, and to the last element of higher
+--  priority. This unfortunately makes the insertion and extraction
+--  procedures somewhat complicated. Procedures are provided for
+--  insertion of a new element, and extraction of the highest-priority
+--  element in the queue.
+
+
 package Priority_Queue is
 
+   --  Type to contain the actual queue
    type Priority_Queue_Type (MaxElt : Integer) is private;
    type Priority_Queue_Access is access all Priority_Queue_Type;
 
+   --  Possible values for a priority
    subtype Priority_Value is Integer range -32767 .. 32767;
 
+   --  Possible values for Order and Discard policies
+   --  according to COS Notification specification
    type Discard_Policy_Values is (Fifo, Lifo, Priority);
    type Order_Policy_Values is (Fifo, Priority);
 
+   --  Allowable values for Factor :
+   --  Controls sub-division of complete priority range
    subtype Factor_Range is Integer range 0 .. 16;
 
    --  Create a priority queue
@@ -40,34 +95,56 @@ package Priority_Queue is
 private
    use PolyORB.Tasking.Semaphores;
 
+   --  A single queue element
    type Queue_Elt;
    type Queue_Elt_Ptr is access Queue_Elt;
    type Queue_Elt is record
+      --  pointer to next queue element
       Next : Queue_Elt_Ptr;
+      --  pointer to most recently arrived element of same priority
       Last : Queue_Elt_Ptr;
+      --  pointer to previous queue element
       Prev : Queue_Elt_Ptr;
+      --  pointer to next queue element, in order of arrival
       NextTime : Queue_Elt_Ptr;
+      --  pointer to previous queue element, in order of arrival
       PrevTime : Queue_Elt_Ptr;
+      --  pointer to last element of strictly higher priority
       Last_Higher : Queue_Elt_Ptr;
+      --  the event data
       Data : CORBA.Any;
+      --  the event's priority
       P : Priority_Value;
    end record;
 
+   --  Array type containing the different queues
    type Priority_Queue_Array is array (Integer range <>) of Queue_Elt_Ptr;
 
    type Priority_Queue_Type (MaxElt : Integer) is record
+      --  The priority queues
       Priority_Queues : Priority_Queue_Array (0 .. MaxElt);
+      --  Highest priority currently present in the queue
       Highest_Priority : Integer := 0;
+      --  Lowest priority currently present in the queue
       Lowest_Priority : Integer := MaxElt;
+      --  Semaphore controling access to the queue
       Queue_Lock : Semaphore_Access := null;
+      --  Value controlling the size of priority range subdivisions
       Factor : Factor_Range := 0;
+      --  Maximum number of events allowed in the queue, -1 if unlimited
       MaxSize : Integer := -1;
+      --  Pointer to earliest arrived element
       FirstTime : Queue_Elt_Ptr := null;
+      --  Pointer to most recently arrived element
       LastTime : Queue_Elt_Ptr := null;
+      --  Current Discard policy
       Discard : Discard_Policy_Values := Lifo;
+      --  Current Order policy
       Order : Order_Policy_Values := Fifo;
    end record;
 
+   --  if the queue is full, this procedure decides which element
+   --  to eliminate to make more room.
    procedure Discard (PQ : in out Priority_Queue_Access; Result : out Boolean);
 
 end Priority_Queue;
