52add8b6a86058b1214d030de0e3349805115fad
Jérôme Hugues <hugues@enst.fr>
2003-04-24 17:20:18 +0000

    Updated Header
    [Imported from Perforce change 6659 at 2006-12-01 19:57:52]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 34612

--- src/srp/polyorb-setup-tcp_access_points-srp.ads
+++ src/srp/polyorb-setup-tcp_access_points-srp.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---             Copyright (C) 1999-2003 Free Software Fundation              --
+--         Copyright (C) 2002-2003 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -26,11 +26,14 @@
 -- however invalidate  any other reasons why  the executable file  might be --
 -- covered by the  GNU Public License.                                      --
 --                                                                          --
---              PolyORB is maintained by ENST Paris University.             --
+--                PolyORB is maintained by ACT Europe.                      --
+--                    (email: sales@act-europe.fr)                          --
 --                                                                          --
 ------------------------------------------------------------------------------
 
 --  Set up SRP TCP Access points.
+
+--  $Id$
 
 package PolyORB.Setup.TCP_Access_Points.SRP is
 
