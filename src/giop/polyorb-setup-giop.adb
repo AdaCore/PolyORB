b8f1fa6b801b221467cfb0c6b3158cfc3f2ccb38
Thomas Quinot <quinot@adacore.com>
2003-07-21 12:32:41 +0000

    Cleanup. Note that this year is called '2003'.
    [Imported from Perforce change 7065 at 2006-12-01 20:16:11]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 34900

--- src/giop/polyorb-setup-giop.adb
+++ src/giop/polyorb-setup-giop.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---            Copyright (C) 2002 Free Software Foundation, Inc.             --
+--            Copyright (C) 2003 Free Software Foundation, Inc.             --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -32,12 +32,13 @@
 ------------------------------------------------------------------------------
 
 pragma Warnings (Off);
+--  No entities referenced.
+
 with PolyORB.Protocols.GIOP.GIOP_1_0;
 with PolyORB.Protocols.GIOP.GIOP_1_1;
 with PolyORB.Protocols.GIOP.GIOP_1_2;
 pragma Warnings (On);
 
 package body PolyORB.Setup.GIOP is
-begin
-   null;
+
 end PolyORB.Setup.GIOP;
