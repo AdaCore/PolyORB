3c059e2af51e8ebf8a32d3f421c07332abc0e5f8
Bertrand Paquet <paquet@enst.fr>
2003-09-17 14:10:45 +0000

    Add GOA in CORBA personality
    [Imported from Perforce change 7299 at 2006-12-01 20:19:22]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 35099

---
+++ src/corba/portableserver-poa-goa.ads
@@ -0,0 +1,107 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           POLYORB COMPONENTS                             --
+--                                                                          --
+--              P O R T A B L E S E R V E R . P O A . G O A                 --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--         Copyright (C) 2001-2003 Free Software Foundation, Inc.           --
+--                                                                          --
+-- This specification is derived from the CORBA Specification, and adapted  --
+-- for use with PolyORB. The copyright notice above, and the license        --
+-- provisions that follow apply solely to the contents neither explicitely  --
+-- nor implicitely specified by the CORBA Specification defined by the OMG. --
+--                                                                          --
+-- PolyORB is free software; you  can  redistribute  it and/or modify it    --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. PolyORB is distributed  in the hope that it will be  useful,    --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with PolyORB; see file COPYING. If    --
+-- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--                PolyORB is maintained by ACT Europe.                      --
+--                    (email: sales@act-europe.fr)                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Exceptions;
+
+with CORBA.Object;
+
+package PortableServer.POA.GOA is
+
+   type Ref is new PortableServer.POA.Ref with null record;
+
+   function To_Ref
+     (Self : CORBA.Object.Ref'Class)
+     return Ref;
+
+   -----------------------
+   --  Group management --
+   -----------------------
+
+   NotAGroupObject : exception;
+
+   --  type sequence <ObjectId> IDs;
+   type ObjectId_Access is access ObjectId;
+   package Sequence_IDs
+     is new CORBA.Sequences.Unbounded (ObjectId_Access);
+
+   type IDs is new Sequence_IDs.Sequence;
+
+   function Create_Id_For_Reference
+     (Self    : in Ref;
+      The_Ref : in CORBA.Object.Ref)
+     return ObjectId;
+   --  raises (NotAGroupObject);
+   --  create a new objectid and associate it with group The_Ref
+
+   function Reference_To_Ids
+     (Self    : in Ref;
+      The_Ref : in CORBA.Object.Ref)
+     return IDs;
+   --  raises (NotAGroupObject);
+   --  user must free all object_id in list
+
+   procedure Associate_Reference_With_Id
+     (Self : in Ref;
+      Ref  : in CORBA.Object.Ref;
+      Oid  : in ObjectId);
+   --  raises(NotAGroupObject);
+
+   procedure Disassociate_Reference_With_Id
+     (Self : in Ref;
+      Ref  : in CORBA.Object.Ref;
+      Oid  : in ObjectId);
+   --  raises(NotAGroupObject);
+
+   --------------------------------------------------
+   -- PortableServer.POA.GOA Exceptions Management --
+   --------------------------------------------------
+
+   --  NotAGroupObjects
+
+   type NotAGroupObject_Members is new CORBA.IDL_Exception_Members
+     with null record;
+
+   procedure Get_Members
+     (From : in  Ada.Exceptions.Exception_Occurrence;
+      To   : out NotAGroupObject_Members);
+
+   procedure Raise_NotAGroupObject
+     (Excp_Memb : in NotAGroupObject_Members);
+   pragma No_Return (Raise_NotAGroupObject);
+
+end PortableServer.POA.GOA;
