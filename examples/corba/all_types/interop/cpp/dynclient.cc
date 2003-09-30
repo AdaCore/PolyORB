d8dbd34cc3f8f6d9b6eee2e61a17c3b31d963c97
Jérôme Hugues <hugues@enst.fr>
2003-09-30 21:35:08 +0000

    Modified dynamic tests to be compiled either with TAO or omniORB
    [Imported from Perforce change 7353 at 2006-12-01 20:20:17]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 35148

--- examples/corba/all_types/interop/cpp/dynclient.cc
+++ examples/corba/all_types/interop/cpp/dynclient.cc
@@ -1,8 +1,20 @@
+// $Id$
+// DII client, makes 10 000 calls to echoULong method
+
 #include <stdlib.h>
 #include <iostream>
-#include <omniORB4/CORBA.h>
+
+#ifdef __USE_TAO__
+#include "tao/DynamicInterface/Request.h"
+#include "tao/corba.h"
+#endif
+
+#ifdef __USE_OMNIORB__
+#include "omniORB4/CORBA.h"
+#endif
 
 using namespace std;
+using namespace CORBA;
 
 static void print(bool Pass)
 {
@@ -21,12 +33,11 @@ static void test(CORBA::Object_var p)
        CORBA::Request_var req1 = p->_request("echoULong");
        req1->add_in_arg() <<= arg;
        req1->set_return_type(CORBA::_tc_ulong);
-       req1->invoke(); // invocation de la methode
+       req1->invoke(); 
 
-
-       if( req1->env()->exception() ) {
-          cerr<< "dynclient: An exception was thrown!" << endl;
-          return;
+       if ( req1->exceptions() != NULL && req1->exceptions()->count() > 0) {
+         cerr<< "dynclient: An exception was thrown!" << endl;
+       	 return;
        }
 
        CORBA::ULong ret1;
@@ -51,7 +62,7 @@ int main(int argc, char** argv)
 
       CORBA::Object_var obj = orb->string_to_object(argv[1]);
 
-      if (CORBA::is_nil(obj))
+      if (CORBA::is_nil(obj.in ()))
         {
           cerr << "can't access object" << endl;
           exit(EXIT_FAILURE);
@@ -62,7 +73,6 @@ int main(int argc, char** argv)
       orb->destroy();
 
     }
-
   catch(CORBA::COMM_FAILURE& ex)
     {
       cerr << "Caught system exception COMM_FAILURE -- unable to contact the object" << endl;
@@ -74,13 +84,6 @@ int main(int argc, char** argv)
   catch(CORBA::Exception&)
     {
       cerr << "Caught CORBA::Exception" << endl;
-    }
-  catch(omniORB::fatalException& fe)
-    {
-      cerr << "Caught omniORB::fatalException:" << endl;
-      cerr << "  file : " << fe.file() << endl;
-      cerr << "  line : " << fe.line() << endl;
-      cerr << "  mesg : " << fe.errmsg() << endl;
     }
   catch(...)
     {
--- examples/corba/all_types/interop/cpp/dynserver_mt.cc
+++ examples/corba/all_types/interop/cpp/dynserver_mt.cc
@@ -1,20 +1,19 @@
 // $Id$
-// DSI server, implements echoULong method
+
+// DSI server, implements echoULong method. It uses TAO's worker thread pool.
+// NOTE: this server will compile only with TAO
 
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef __USE_TAO__
+#include "ace/pre.h"
 #include "tao/DynamicInterface/Dynamic_Implementation.h"
 #include "tao/DynamicInterface/Server_Request.h"
 #include "tao/PortableServer/PortableServer.h"
 #include "tao/corba.h"
-#endif
-
-#ifdef __USE_OMNIORB__
-#include <omniORB4/CORBA.h>
-#endif
+#include "ace/Task.h"
+#include "ace/post.h"
 
 using namespace std;
 using namespace CORBA;
@@ -29,29 +28,54 @@ public:
 
 CORBA::ORB_var orb;
 
+//////////////////////////////////////////////////////////////////////////
+// Worker thread implementation
+
+class Worker : public ACE_Task_Base
+{
+public:
+  Worker (CORBA::ORB_ptr orb);
+
+  virtual int svc (void);
+
+private:
+  CORBA::ORB_var orb_;
+};
+
+Worker::Worker (CORBA::ORB_ptr orb)
+  :  orb_ (CORBA::ORB::_duplicate (orb))
+{
+}
+
+int
+Worker::svc (void)
+{
+  ACE_DECLARE_NEW_CORBA_ENV;
+  ACE_TRY
+    {
+      this->orb_->run (ACE_ENV_SINGLE_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+    }
+  ACE_ENDTRY;
+  return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
 void
 MyDynImpl::invoke(CORBA::ServerRequest_ptr request)
 {
   try {
 
-    if (strcmp ("_is_a", request->operation ()) == 0)
+    if (ACE_OS::strcmp ("_is_a", request->operation ()) == 0)
       {
 	CORBA::NVList_ptr list;
 	orb->create_list (0, list);
-
-	// XXX It seems there is a subtle difference in the way
-	// anys are handled. This makes the compiler happy, but 
-	// is it correct ?
-
-#ifdef __USE_TAO__
-	CORBA::Any type_id (CORBA::_tc_string);
-#endif
-
-#ifdef __USE_OMNIORB__
-	CORBA::Any type_id;
-	type_id.replace(CORBA::_tc_string, 0);
-#endif
 	
+	CORBA::Any type_id (CORBA::_tc_string);
 	list->add_value ("type_id", type_id, CORBA::ARG_IN);
 	
 	request->arguments (list);
@@ -79,18 +103,8 @@ MyDynImpl::invoke(CORBA::ServerRequest_ptr request)
       {
 	CORBA::NVList_ptr args;
 	orb->create_list(0, args);
-
-	//  XXX See comment above
-
-#ifdef __USE_TAO__
+	
 	CORBA::Any a (CORBA::_tc_ulong);
-#endif
-
-#ifdef __USE_OMNIORB__
-	CORBA::Any a;
-	a.replace(CORBA::_tc_ulong, 0);
-#endif
-
 	args->add_value("", a, CORBA::ARG_IN);
 	request->arguments(args);
 	CORBA::ULong x;
@@ -102,14 +116,11 @@ MyDynImpl::invoke(CORBA::ServerRequest_ptr request)
       }
     else
       {
-	std::cout << "bad operation !" << endl;
-
 	throw CORBA::BAD_OPERATION(0, CORBA::COMPLETED_NO);
       }
   }
   catch(CORBA::SystemException& ex){
-    std::cout << "echo_dsiimpl: MyDynImpl::invoke - caught an system exception."
-	      << endl;
+    std::cout << "I'll be back" << std::endl;
     CORBA::Any a;
     a <<= ex;
     request->set_exception(a);
@@ -129,6 +140,9 @@ MyDynImpl::_primary_interface(const PortableServer::ObjectId&,
 {
   return CORBA::string_dup("IDL:all_types:1.0");
 }
+
+//////////////////////////////////////////////////////////////////////////
+// main function
 
 int main(int argc, char** argv)
 {
@@ -164,8 +178,15 @@ int main(int argc, char** argv)
     pman->activate();
 
     std::cerr << "Server ready" << std::endl;
-    orb->run();
 
+    Worker worker (orb.in ());
+      if (worker.activate (THR_NEW_LWP | THR_JOINABLE,
+                           4) != 0)
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "Cannot activate client threads\n"),
+                          1);
+
+      worker.thr_mgr ()->wait ();
   }
 
   catch(...)
