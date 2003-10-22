4ae4b172a3a4015437b480e9645417069a2b7daa
Jérôme Hugues <hugues@enst.fr>
2003-10-22 08:25:29 +0000

    Suppressed lines too long
    [Imported from Perforce change 7455 at 2006-12-01 20:22:44]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 35235

--- src/corba/rtcorba/rtcorba-prioritymodelpolicy.adb
+++ src/corba/rtcorba/rtcorba-prioritymodelpolicy.adb
@@ -31,8 +31,6 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  $Id$
-
 with PolyORB.RTCORBA_P.PriorityModelPolicy;
 
 package body RTCORBA.PriorityModelPolicy is
--- src/corba/rtcorba/rtcorba-prioritytransform.ads
+++ src/corba/rtcorba/rtcorba-prioritytransform.ads
@@ -36,8 +36,6 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  $Id$
-
 with PortableServer;
 
 package RTCORBA.PriorityTransform is
