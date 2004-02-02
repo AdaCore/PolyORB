d9069b774cfd0432dfc2da889a5e3d98e816eaa8
Thomas Vergnaud <vergnaud@adacore.com>
2004-02-02 17:24:57 +0000

    Components are not controlled types any more.
    [Imported from Perforce change 7835 at 2006-12-01 20:26:49]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 35507

--- src/polyorb-components.ads
+++ src/polyorb-components.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2001-2002 Free Software Foundation, Inc.           --
+--         Copyright (C) 2001-2004 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -53,9 +53,7 @@ package PolyORB.Components is
 
    type Null_Message is new Message with private;
 
-   type Component is
-     abstract new Ada.Finalization.Limited_Controlled
-     with private;
+   type Component is abstract tagged limited private;
    type Component_Access is access all Component'Class;
 
    Unhandled_Message : exception;
