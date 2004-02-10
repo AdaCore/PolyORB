8d7ef88dc5cccaa1a9d6b429a1462502bd9d733e
Jérôme Hugues <hugues@enst.fr>
2004-02-10 22:03:27 +0000

    Do not attempt to access arg list before it is completely initialized.
    [Imported from Perforce change 7858 at 2006-12-01 20:27:04]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 35527

--- src/moma/polyorb-moma_p-provider-routers.adb
+++ src/moma/polyorb-moma_p-provider-routers.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2003 Free Software Foundation, Inc.           --
+--         Copyright (C) 2003-2004 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -283,7 +283,7 @@ package body PolyORB.MOMA_P.Provider.Routers is
       use PolyORB.Exceptions;
 
       Args      : PolyORB.Any.NVList.Ref;
-      It        : Iterator := First (List_Of (Args).all);
+      It        : Iterator;
       Operation : constant String := To_Standard_String (Req.all.Operation);
       Error     : Error_Container;
    begin
@@ -301,6 +301,7 @@ package body PolyORB.MOMA_P.Provider.Routers is
 
       end if;
 
+      It := First (List_Of (Args).all);
 
       if Operation = "Publish" then
 
