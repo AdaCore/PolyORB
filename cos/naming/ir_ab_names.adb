94f576997b90d8a163f492903b067001a643b171
Jérôme Hugues <hugues@enst.fr>
2004-05-26 17:26:42 +0000

    ab_names is now po_cos_naming
    [Imported from Perforce change 8076 at 2006-12-01 20:29:25]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 35690

--- cos/naming/ir_ab_names.adb
+++ cos/naming/ir_ab_names.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---            Copyright (C) 2002 Free Software Foundation, Inc.             --
+--         Copyright (C) 2002-2004 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -33,7 +33,7 @@
 
 --  $Id$
 
-with AB_Names;
+with PO_COS_Naming;
 with PolyORB.CORBA_P.Server_Tools;
 with PolyORB.If_Descriptors;
 with PolyORB.If_Descriptors.CORBA_IR;
@@ -48,5 +48,5 @@ procedure IR_AB_Names is
 begin
    PolyORB.CORBA_P.Server_Tools.Initiate_Server_Hook
      := IR_AB_Names_Setup'Unrestricted_Access;
-   AB_Names;
+   PO_COS_Naming;
 end IR_AB_Names;
