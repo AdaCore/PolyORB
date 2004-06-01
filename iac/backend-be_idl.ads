ecf273a7701cb21a1a1b4d478f3a840bc79fb2d4
Thomas Vergnaud <vergnaud@adacore.com>
2004-06-01 13:18:56 +0000

    removed iac files, as they are now in parsers/iac
    [Imported from ENST Subversion rev. 6188]
    
    Subversion-branch: /import/ENST-iac-20061219
    Subversion-revision: 39785

--- iac/backend-be_idl.ads
+++
@@ -1,7 +0,0 @@
-with Types; use Types;
-
-package Backend.BE_IDL is
-
-   procedure Generate (E : Node_Id);
-
-end Backend.BE_IDL;
--- iac/parser.ads
+++
@@ -1,8 +0,0 @@
-with Types; use Types;
-
-package Parser is
-
-   procedure Process (Root : out Node_Id);
-   --  Process the IDL specification
-
-end Parser;
