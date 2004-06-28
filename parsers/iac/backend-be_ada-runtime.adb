4fa901ad93c3c2b98ea42b4db81e91773c944d35
Laurent Pautet <pautet@adacore.com>
2004-06-28 17:53:02 +0000

    * backend-be_ada-runtime.ads, backend-be_ada-runtime.adb: New 	files to deal with various API.
    	* scopes.adb, values.adb, backend-be_ada-generator.adb,
    	frontend-nutils.adb, mknodes.adb, lexer.adb : Reordering.
    
    	* backend-be_ada-nutils.adb, backend-be_ada-nutils.ads: Avoid
    	duplication of withed packages. Use rtsfind-like mechanism from
    	backend-be_ada-runtime.
    
    	* backend-be_ada.adb: Use rtsfind-like mechanism from
    	backend-be_ada-runtime. In particular concerning Designator
    	creations. Fix implementation of repository_id production since
    	their values were plain wrong.
    
    	* backend-be_a (all): Removed.
    
    	* MANIFEST: Updated.
    
    	* Makefile: Check that subprograms are still sorted.
    
    [Imported from ENST Subversion rev. 6427]
    
    Subversion-branch: /import/ENST-iac-20061219
    Subversion-revision: 40024

---
+++ parsers/iac/backend-be_ada-runtime.adb
@@ -0,0 +1,83 @@
+with Namet;   use Namet;
+with Utils;   use Utils;
+
+with Backend.BE_Ada.Nodes;  use Backend.BE_Ada.Nodes;
+with Backend.BE_Ada.Nutils; use Backend.BE_Ada.Nutils;
+
+package body Backend.BE_Ada.Runtime is
+
+   RUD : array (RU_Id) of Node_Id;
+   RED : array (RE_Id) of Node_Id;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize is
+      Position   : Integer;
+      Identifier : Name_Id;
+      Length     : Natural;
+
+   begin
+      for U in RU_Id loop
+         Set_Str_To_Name_Buffer (RU_Id'Image (U));
+         Set_Str_To_Name_Buffer (Name_Buffer (4 .. Name_Len));
+         Capitalize (Name_Buffer (1 .. Name_Len));
+         if Name_Buffer (1 .. Name_Len) = "Null" then
+            RUD (U) := No_Node;
+         else
+            Position   := 0;
+            Length     := 0;
+            Identifier := Name_Find;
+            Length     := Name_Len;
+            Set_Name_Table_Info (Identifier, RU_Id'Pos (U) + 1);
+
+            while Name_Len > 0 loop
+               if Name_Buffer (Name_Len) = '_' then
+                  Name_Len := Name_Len - 1;
+                  Position := Integer (Get_Name_Table_Info (Name_Find));
+                  exit when Position > 0;
+               else
+                  Name_Len := Name_Len - 1;
+               end if;
+            end loop;
+
+            RUD (U) := New_Node (K_Designator);
+            if Position > 0 then
+               Set_Str_To_Name_Buffer
+                 (Name_Buffer (Name_Len + 2 .. Length));
+               Identifier := Name_Find;
+               Set_Parent_Unit_Name (RUD (U), RUD (RU_Id'Val (Position - 1)));
+            end if;
+            Set_Defining_Identifier
+              (RUD (U), Make_Defining_Identifier (Identifier));
+         end if;
+      end loop;
+
+      for E in RE_Id loop
+         Set_Str_To_Name_Buffer (RE_Id'Image (E));
+         Set_Str_To_Name_Buffer (Name_Buffer (4 .. Name_Len));
+         Capitalize (Name_Buffer (1 .. Name_Len));
+         while Name_Buffer (Name_Len) in '0' .. '9'
+           or else Name_Buffer (Name_Len) = '_'
+         loop
+            Name_Len := Name_Len - 1;
+         end loop;
+         Identifier := Name_Find;
+         RED (E) := New_Node (K_Designator);
+         Set_Defining_Identifier
+           (RED (E), Make_Defining_Identifier (Identifier));
+         Set_Parent_Unit_Name (RED (E), RUD (RE_Unit_Table (E)));
+      end loop;
+   end Initialize;
+
+   --------
+   -- RE --
+   --------
+
+   function RE (Id : RE_Id) return Node_Id is
+   begin
+      return Copy_Designator (RED (Id));
+   end RE;
+
+end Backend.BE_Ada.Runtime;
---
+++ parsers/iac/backend-be_ada-runtime.ads
@@ -0,0 +1,77 @@
+with Types; use Types;
+
+package Backend.BE_Ada.Runtime is
+
+   --  Runtime Units
+
+   type RU_Id is
+     (RU_Null,
+      RU_CORBA,
+      RU_CORBA_AbstractBase,
+      RU_CORBA_Object,
+      RU_PolyORB,
+      RU_PolyORB_Any,
+      RU_PolyORB_Any_NVList,
+      RU_PolyORB_Requests,
+      RU_Standard);
+
+   --  Runtime Entities
+
+   type RE_Id is
+     (RE_Ref_0,                     --  Ref
+      RE_Float,                     --  CORBA.Float
+      RE_Double,                    --  CORBA.Double
+      RE_Long_Double,               --  CORBA.Long_Double
+      RE_Short,                     --  CORBA.Short
+      RE_Long,                      --  CORBA.Long
+      RE_Long_Long,                 --  CORBA.Long_Long
+      RE_Unsigned_Short,            --  CORBA.Unsigned_Short
+      RE_Unsigned_Long,             --  CORBA.Unsigned_Long
+      RE_Unsigned_Long_Long,        --  CORBA.Unsigned_Long_Long
+      RE_Char,                      --  CORBA.Char
+      RE_WChar,                     --  CORBA.WChar
+      RE_String_1,                  --  CORBA.String
+      RE_Wide_String,               --  CORBA.Wide_String
+      RE_Boolean,                   --  CORBA.Boolean
+      RE_Octet,                     --  CORBA.Boolean
+      RE_Object_Is_Nil,             --  CORBA.Object_Is_Nil
+      RE_To_CORBA_String,           --  CORBA.To_CORBA_String
+      RE_Ref_1,                     --  CORBA.AbstractBase.Ref
+      RE_Ref_2,                     --  CORBA.Object.Ref
+      RE_NamedValue,                --  PolyORB.Any
+      RE_Ref_3,                     --  PolyORB.Any.NVList.Ref
+      RE_Request_Access,            --  PolyORB.Requests.Request_Access
+      RE_String_2);                 --  Standard.String
+
+   RE_Unit_Table : array (RE_Id) of RU_Id
+     := (RE_Ref_0               => RU_Null,
+         RE_Float               => RU_CORBA,
+         RE_Double              => RU_CORBA,
+         RE_Long_Double         => RU_CORBA,
+         RE_Short               => RU_CORBA,
+         RE_Long                => RU_CORBA,
+         RE_Long_Long           => RU_CORBA,
+         RE_Unsigned_Short      => RU_CORBA,
+         RE_Unsigned_Long       => RU_CORBA,
+         RE_Unsigned_Long_Long  => RU_CORBA,
+         RE_Char                => RU_CORBA,
+         RE_WChar               => RU_CORBA,
+         RE_String_1            => RU_CORBA,
+         RE_Wide_String         => RU_CORBA,
+         RE_Boolean             => RU_CORBA,
+         RE_Octet               => RU_CORBA,
+         RE_Object_Is_Nil       => RU_CORBA,
+         RE_To_CORBA_String     => RU_CORBA,
+         RE_Ref_1               => RU_CORBA_AbstractBase,
+         RE_Ref_2               => RU_CORBA_Object,
+         RE_NamedValue          => RU_PolyORB_Any,
+         RE_Ref_3               => RU_PolyORB_Any_NVList,
+         RE_Request_Access      => RU_PolyORB_Requests,
+         RE_String_2            => RU_Standard);
+
+   procedure Initialize;
+
+   function RE (Id : RE_Id) return Node_Id;
+   --  Return a designator for entity Id
+
+end Backend.BE_Ada.Runtime;
