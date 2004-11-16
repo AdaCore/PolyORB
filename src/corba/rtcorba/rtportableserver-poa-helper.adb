d10a2549d1d197c0919ecf0742dfd12e84cba9b7
Jérôme Hugues <hugues@enst.fr>
2004-11-16 09:08:14 +0000

    Removed Id lines
    [Imported from Perforce change 8812 at 2006-12-01 20:38:39]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 36373

--- src/corba/rtcorba/rtportableserver-poa-helper.adb
+++ src/corba/rtcorba/rtportableserver-poa-helper.adb
@@ -31,8 +31,6 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  $Id$
-
 with PolyORB.RT_POA;
 
 package body RTPortableServer.POA.Helper is
