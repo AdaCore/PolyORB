bcc40d77d0ce7bb54b92ab4f494c0137a1c15423
Jérôme Hugues <hugues@enst.fr>
2004-11-17 16:34:51 +0000

    Remove id lines
    [Imported from Perforce change 8821 at 2006-12-01 20:38:50]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 36382

--- src/corba/rtcorba/rtcorba-threadpoolpolicy-helper.ads
+++ src/corba/rtcorba/rtcorba-threadpoolpolicy-helper.ads
@@ -36,8 +36,6 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  $Id$
-
 with CORBA.Object;
 
 package RTCORBA.ThreadpoolPolicy.Helper is
