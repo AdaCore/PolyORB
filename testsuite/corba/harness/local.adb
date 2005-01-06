456b4425207584d4a7357ca3484fa326e7990f8d
Jérôme Hugues <hugues@enst.fr>
2005-01-06 09:54:19 +0000

    Enhance test output
    [Imported from Perforce change 8992 at 2006-12-01 21:11:16]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 36531

--- testsuite/corba/harness/local.adb
+++ testsuite/corba/harness/local.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---            Copyright (C) 2004 Free Software Foundation, Inc.             --
+--         Copyright (C) 2004-2005 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -26,8 +26,8 @@
 -- however invalidate  any other reasons why  the executable file  might be --
 -- covered by the  GNU Public License.                                      --
 --                                                                          --
---                PolyORB is maintained by ACT Europe.                      --
---                    (email: sales@act-europe.fr)                          --
+--                  PolyORB is maintained by AdaCore                        --
+--                     (email: sales@adacore.com)                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 
@@ -54,9 +54,9 @@ procedure Local is
    use PolyORB.CORBA_P.Server_Tools;
 
 begin
-
-   Ada.Text_IO.Put_Line ("Server starting.");
+   New_Test ("Harness");
    CORBA.ORB.Initialize ("ORB");
+   Output ("Initialization", True);
 
    declare
       Obj : constant CORBA.Impl.Object_Ptr := new Harness.Impl.Object;
