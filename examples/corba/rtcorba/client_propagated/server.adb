d005e6074dceb9561de60a9d5f8ffb26995c49d0
Jérôme Hugues <hugues@enst.fr>
2005-04-07 15:37:37 +0000

    Use PolyORB.Setup.Base instead of withing specific setup files.
    [Imported from Perforce change 9248 at 2006-12-01 21:17:31]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 36759

--- examples/corba/rtcorba/client_propagated/server.adb
+++ examples/corba/rtcorba/client_propagated/server.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---            Copyright (C) 2004 Free Software Foundation, Inc.             --
+--         Copyright (C) 2004-2005 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -26,8 +26,8 @@
 -- however invalidate  any other reasons why  the executable file  might be --
 -- covered by the  GNU Public License.                                      --
 --                                                                          --
---                PolyORB is maintained by ACT Europe.                      --
---                    (email: sales@act-europe.fr)                          --
+--                  PolyORB is maintained by AdaCore                        --
+--                     (email: sales@adacore.com)                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 
@@ -95,9 +95,9 @@ pragma Elaborate_All
 pragma Warnings
   (Off, PolyORB.Tasking.Profiles.Full_Tasking.Condition_Variables);
 
-with PolyORB.Parameters.File;
-pragma Warnings (Off, PolyORB.Parameters.File);
-pragma Elaborate_All (PolyORB.Parameters.File);
+with PolyORB.Setup.Base;
+pragma Warnings (Off, PolyORB.Setup.Base);
+pragma Elaborate_All (PolyORB.Setup.Base);
 
 with PolyORB.Setup.OA.Basic_RT_POA;
 pragma Warnings (Off, PolyORB.Setup.OA.Basic_RT_POA);
