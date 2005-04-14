d7399e1cde16f1886e949c5dfa105f74a4f72584
Thomas Quinot <quinot@adacore.com>
2005-04-14 12:40:37 +0000

    Minor documentation fix.
    [Imported from Perforce change 9260 at 2006-12-01 21:17:49]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 36767

--- src/polyorb-utils-htables-perfect.ads
+++ src/polyorb-utils-htables-perfect.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2002-2003 Free Software Foundation, Inc.           --
+--         Copyright (C) 2002-2005 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -154,7 +154,7 @@ package PolyORB.Utils.HTables.Perfect is
 
    function Last (I : Iterator)
                  return Boolean;
-   --  True if I is on the last element of the Table_Instance on which
+   --  True if I is past the last element of the Table_Instance on which
    --  it operates.
 
    procedure Next (I : in out Iterator);
