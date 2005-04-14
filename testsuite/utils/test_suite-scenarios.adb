75dc67e28a5e261770f949c5cb023678430fc02a
Thomas Quinot <quinot@adacore.com>
2005-04-14 15:14:53 +0000

    Remove unnecessary, obsolete call to PolyORB.Parameters.Reset.
    [Imported from Perforce change 9264 at 2006-12-01 21:17:50]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 36771

--- testsuite/utils/test_suite-scenarios.adb
+++ testsuite/utils/test_suite-scenarios.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2003-2004 Free Software Foundation, Inc.           --
+--         Copyright (C) 2003-2005 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -122,7 +122,6 @@ package body Test_Suite.Scenarios is
          Separator (Output);
 
          Close_Scenario_Output_Context (Output, Result);
-         PolyORB.Parameters.Reset;
       end;
 
    exception
