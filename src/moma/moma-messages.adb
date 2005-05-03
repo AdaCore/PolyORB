9a9680bb24820a29c49e7e97b788e0f62e8ded65
Jérôme Hugues <hugues@enst.fr>
2005-05-03 00:23:21 +0000

    Added missing dependency on PolyORB.Any
    [Imported from Perforce change 9304 at 2006-12-01 21:19:47]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 36810

--- src/moma/moma-messages.adb
+++ src/moma/moma-messages.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2004 Free Software Foundation, Inc.           --
+--         Copyright (C) 2002-2005 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -26,8 +26,8 @@
 -- however invalidate  any other reasons why  the executable file  might be --
 -- covered by the  GNU Public License.                                      --
 --                                                                          --
---                PolyORB is maintained by ACT Europe.                      --
---                    (email: sales@act-europe.fr)                          --
+--                  PolyORB is maintained by AdaCore                        --
+--                     (email: sales@adacore.com)                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 
@@ -689,7 +689,8 @@ begin
         (Module_Info'
          (Name      => +"MOMA.Messages",
           Conflicts => Empty,
-          Depends   => +"MOMA.Destinations",
+          Depends   => +"MOMA.Destinations"
+          & "any",
           Provides  => Empty,
           Implicit  => False,
           Init      => Initialize'Access));
