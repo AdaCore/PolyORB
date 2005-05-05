73c57c403190563faadb8108ac7a142a3698e7b2
Jérôme Hugues <hugues@enst.fr>
2005-05-05 17:11:57 +0000

    Defence against invalid stringified reference
    [Imported from Perforce change 9316 at 2006-12-01 21:19:56]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 36822

--- tools/po_catref/po_catref.adb
+++ tools/po_catref/po_catref.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---            Copyright (C) 2004 Free Software Foundation, Inc.             --
+--         Copyright (C) 2004-2005 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -26,8 +26,8 @@
 -- however invalidate  any other reasons why  the executable file  might be --
 -- covered by the  GNU Public License.                                      --
 --                                                                          --
---                PolyORB is maintained by ACT Europe.                      --
---                    (email: sales@act-europe.fr)                          --
+--                  PolyORB is maintained by AdaCore                        --
+--                     (email: sales@adacore.com)                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 
@@ -83,7 +83,14 @@ begin
    Put_Line ("Parsing stringified reference", Ada.Command_Line.Argument (1));
    New_Line;
 
-   String_To_Object (Ada.Command_Line.Argument (1), Obj_Ref);
+   begin
+      String_To_Object (Ada.Command_Line.Argument (1), Obj_Ref);
+   exception
+      when others =>
+         Put_Line ("Error", "Invalid reference !");
+         Flush;
+         return;
+   end;
 
    if Is_Nil (Obj_Ref) then
       Put_Line ("Error", "Null reference !");
