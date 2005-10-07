1b8a52e3d1d6f2fe6a387e2f88e02b02b3b586dd
Jérôme Hugues <hugues@enst.fr>
2005-10-07 11:15:32 +0000

    (Add_IOR_Component): if there is no QoS parameter associated with object adapter, then create a new one.
    Patch submitted by Vadim Godunko, reviewed by Jerome Hugues
    
    [Imported from Perforce change 9618 at 2006-12-01 21:47:27]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 37098

--- src/corba/portableinterceptor/portableinterceptor-iorinfo-impl.adb
+++ src/corba/portableinterceptor/portableinterceptor-iorinfo-impl.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---            Copyright (C) 2004 Free Software Foundation, Inc.             --
+--         Copyright (C) 2004-2005 Free Software Foundation, Inc.           --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -16,8 +16,8 @@
 -- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
 -- License  for more details.  You should have received  a copy of the GNU  --
 -- General Public License distributed with PolyORB; see file COPYING. If    --
--- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
--- Boston, MA 02111-1307, USA.                                              --
+-- not, write to the Free Software Foundation, 51 Franklin Street, Fifth    --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
 --                                                                          --
 -- As a special exception,  if other files  instantiate  generics from this --
 -- unit, or you link  this unit with other files  to produce an executable, --
@@ -26,8 +26,8 @@
 -- however invalidate  any other reasons why  the executable file  might be --
 -- covered by the  GNU Public License.                                      --
 --                                                                          --
---                PolyORB is maintained by ACT Europe.                      --
---                    (email: sales@act-europe.fr)                          --
+--                  PolyORB is maintained by AdaCore                        --
+--                     (email: sales@adacore.com)                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 
@@ -54,11 +54,17 @@ package body PortableInterceptor.IORInfo.Impl is
       use PolyORB.QoS.Tagged_Components;
       use PolyORB.QoS.Tagged_Components.GIOP_Tagged_Component_Lists;
 
-      QoS : constant QoS_GIOP_Tagged_Components_Parameter_Access
+      QoS : QoS_GIOP_Tagged_Components_Parameter_Access
         := QoS_GIOP_Tagged_Components_Parameter_Access
         (Get_Object_Adapter_QoS (Self.POA, GIOP_Tagged_Components));
 
    begin
+      if QoS = null then
+         QoS := new QoS_GIOP_Tagged_Components_Parameter;
+         Set_Object_Adapter_QoS
+           (Self.POA, GIOP_Tagged_Components, QoS_Parameter_Access (QoS));
+      end if;
+
       Append
         (QoS.Components,
          (Component_Id (A_Component.Tag),
