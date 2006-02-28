cf15eb13871d4585b32a1dd1d81f3f0e57a60c8c
Jérôme Hugues <hugues@enst.fr>
2006-02-28 13:55:56 +0000

    Use consistent stack size for tasks, added formal parameter names to ease reading the test.
    Solve current test breakage with latest GNAT wavefront
    
    [Imported from Perforce change 9902 at 2006-12-01 22:22:14]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 37367

--- testsuite/corba/rtcorba/rtorb/test000.adb
+++ testsuite/corba/rtcorba/rtorb/test000.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2004-2005 Free Software Foundation, Inc.           --
+--         Copyright (C) 2004-2006, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -16,8 +16,8 @@
 -- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
 -- License  for more details.  You should have received  a copy of the GNU  --
 -- General Public License distributed with PolyORB; see file COPYING. If    --
--- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
--- Boston, MA 02111-1307, USA.                                              --
+-- not, write to the Free Software Foundation, 51 Franklin Street, Fifth    --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
 --                                                                          --
 -- As a special exception,  if other files  instantiate  generics from this --
 -- unit, or you link  this unit with other files  to produce an executable, --
@@ -155,13 +155,13 @@ begin
       begin
          Thread_Pool_Id := RTCORBA.RTORB.Create_Threadpool
            (RT_ORB,
-            1,
-            2,
-            0,
-            10,
-            False,
-            1,
-            0);
+            Stacksize               => 262_144,
+            Static_Threads          => 2,
+            Dynamic_Threads         => 0,
+            Default_Priority        => 10,
+            Allow_Request_Buffering => False,
+            Max_Buffered_Requests   => 1,
+            Max_Request_Buffer_Size => 0);
 
          Output ("Thread Pool created with id"
                  & RTCORBA.ThreadpoolId'Image (Thread_Pool_Id), True);
@@ -225,12 +225,12 @@ begin
 
          Thread_Pool_Id := Create_Threadpool_With_Lanes
            (RT_ORB,
-            1,
-            Lanes,
-            False,
-            False,
-            1,
-            0);
+            Stacksize               => 262_144,
+            Lanes                   => Lanes,
+            Allow_Borrowing         => False,
+            Allow_Request_Buffering => False,
+            Max_Buffered_Requests   => 1,
+            Max_Request_Buffer_Size => 0);
 
          Output ("Thread Pool created with id"
                  & RTCORBA.ThreadpoolId'Image (Thread_Pool_Id), True);
@@ -274,13 +274,13 @@ begin
 
          Thread_Pool_Id := RTCORBA.RTORB.Create_Threadpool
            (RT_ORB,
-            1,
-            2,
-            0,
-            10,
-            False,
-            1,
-            0);
+            Stacksize               => 262_144,
+            Static_Threads          => 2,
+            Dynamic_Threads         => 0,
+            Default_Priority        => 10,
+            Allow_Request_Buffering => False,
+            Max_Buffered_Requests   => 1,
+            Max_Request_Buffer_Size => 0);
 
          Output ("Thread Pool created with id"
                  & RTCORBA.ThreadpoolId'Image (Thread_Pool_Id), True);
