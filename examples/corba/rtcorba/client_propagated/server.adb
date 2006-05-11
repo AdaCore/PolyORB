032c89c370358ff46a8c7616921c3edf187bc332
Jérôme Hugues <hugues@enst.fr>
2006-05-11 15:42:25 +0000

    Adjust parameter value to actual tested ones, fix test output
    [Imported from Perforce change 10038 at 2006-12-01 22:23:46]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 37500

--- examples/corba/rtcorba/client_propagated/server.adb
+++ examples/corba/rtcorba/client_propagated/server.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2004-2005 Free Software Foundation, Inc.           --
+--         Copyright (C) 2004-2006, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -16,8 +16,8 @@
 -- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
 -- License  for more details.  You should have received  a copy of the GNU  --
 -- General Public License distributed with PolyORB; see file COPYING. If    --
--- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
--- Boston, MA 02111-1307, USA.                                              --
+-- not, write to the Free Software Foundation, 51 Franklin Street, Fifth    --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
 --                                                                          --
 -- As a special exception,  if other files  instantiate  generics from this --
 -- unit, or you link  this unit with other files  to produce an executable, --
@@ -153,7 +153,7 @@ begin
 
       Ref_Server_1 : CORBA.Object.Ref;
 
-      Base_Priority      : constant RTCORBA.Priority := 1_000;
+      Base_Priority      : constant RTCORBA.Priority := 0;
       Default_Priority_1 : constant RTCORBA.Priority := 10_000;
       Default_Priority_2 : constant RTCORBA.Priority := 20_000;
 
