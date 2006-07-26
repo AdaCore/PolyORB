46945121ecd4ddb9f7521dd39afff37c203c1418
Thomas Quinot <quinot@adacore.com>
2006-07-26 15:23:37 +0000

    Add missing files from previous checkin. For F713-011.
    [Imported from Perforce change 10273 at 2006-12-01 22:52:35]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 37733

---
+++ compilers/idlac/string_sets.ads
@@ -0,0 +1,51 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           POLYORB COMPONENTS                             --
+--                                                                          --
+--                          S T R I N G _ S E T S                           --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--           Copyright (C) 2006, Free Software Foundation, Inc.             --
+--                                                                          --
+-- PolyORB is free software; you  can  redistribute  it and/or modify it    --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. PolyORB is distributed  in the hope that it will be  useful,    --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with PolyORB; see file COPYING. If    --
+-- not, write to the Free Software Foundation, 51 Franklin Street, Fifth    --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--                  PolyORB is maintained by AdaCore                        --
+--                     (email: sales@adacore.com)                           --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Containers.Indefinite_Hashed_Sets;
+with Ada.Strings.Hash;
+
+package String_Sets is
+
+   package Hashed_String_Sets is new Ada.Containers.Indefinite_Hashed_Sets
+     (Element_Type        => String,
+      Hash                => Ada.Strings.Hash,
+      Equivalent_Elements => "=");
+   subtype Set is Hashed_String_Sets.Set;
+
+   function Contains (Container : Set; Element : String) return Boolean;
+   --  Determine whether Element is in Container
+
+   procedure Insert (Container : in out Set; Element : String);
+   --  Insert Element into Container
+
+end String_Sets;
