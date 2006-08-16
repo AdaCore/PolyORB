d431c2431794806f229f306a1298c1be1dd9dea8
Thomas Quinot <quinot@adacore.com>
2006-08-16 11:36:25 +0000

    Display CORBALOC URL of server object.
    [Imported from Perforce change 10326 at 2006-12-01 22:54:09]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 37786

--- examples/corba/all_functions/server.adb
+++ examples/corba/all_functions/server.adb
@@ -38,23 +38,27 @@ with CORBA.Object;
 with CORBA.ORB;
 
 with PolyORB.CORBA_P.Server_Tools; use PolyORB.CORBA_P.Server_Tools;
+with PolyORB.CORBA_P.CORBALOC;
 
 with PolyORB.Setup.Thread_Pool_Server;
 pragma Warnings (Off, PolyORB.Setup.Thread_Pool_Server);
 
---  Note : the server must execute two tasks concurrently
---         to pass oneway tests
+--  Note : the server must execute two tasks concurrently to pass oneway tests
 
 with Ada.Text_IO;
 
 procedure Server is
    Ref : CORBA.Object.Ref;
-
 begin
    CORBA.ORB.Initialize ("ORB");
    Initiate_Servant (new all_functions.Impl.Object, Ref);
+
    Ada.Text_IO.Put_Line
      ("'" & CORBA.To_Standard_String (CORBA.Object.Object_To_String (Ref)) &
       "'");
+   Ada.Text_IO.Put_Line
+     ("'" & CORBA.To_Standard_String
+              (PolyORB.CORBA_P.CORBALOC.Object_To_Corbaloc (Ref)) & "'");
+
    Initiate_Server;
 end Server;
