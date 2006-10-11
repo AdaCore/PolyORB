293049e0aea843dbd8520800328f27cd22dda114
Jérôme Hugues <hugues@enst.fr>
2006-10-11 08:47:26 +0000

    Remove debug from this package, since PolyORB's logging facility depends on PolyORB.Utils.HTables.Perfect (through PolyORB.Dynamic_Dict), this was causing some crashes when using debug, and causes the testsuite to fail
    [Imported from Perforce change 10422 at 2006-12-01 22:55:29]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 37881

--- src/polyorb-utils-htables-perfect.adb
+++ src/polyorb-utils-htables-perfect.adb
@@ -33,21 +33,9 @@
 
 with Ada.Unchecked_Deallocation;
 
-with System.Address_Image;
-
-with PolyORB.Log;
-
 package body PolyORB.Utils.HTables.Perfect is
 
-   use PolyORB.Log;
    use PolyORB.Utils.Strings;
-
-   package L is new PolyORB.Log.Facility_Log ("polyorb.utils.htables.perfect");
-   procedure O (Message : String; Level : Log_Level := Debug)
-     renames L.Output;
-   function C (Level : Log_Level := Debug) return Boolean
-     renames L.Enabled;
-   pragma Unreferenced (C); --  For conditional pragma Debug
 
    ------------------------
    -- Utility procedures --
@@ -572,8 +560,6 @@ package body PolyORB.Utils.HTables.Perfect is
       Subtables : Dynamic_Subtable_Array.Table_Ptr renames T.Subtables.Table;
 
    begin
-      pragma Debug (O ("Initialize: @" & System.Address_Image (T'Address)));
-
       --  Initialization of T.Info
 
       T.Info.Count        := 0;
@@ -583,7 +569,6 @@ package body PolyORB.Utils.HTables.Perfect is
 
       --  Allocation of T.Elements
 
-      pragma Debug (O ("Initializing elements"));
       Initialize (T.Elements);
       Dynamic_Element_Array.Set_Last (T.Elements, 15 * T.Info.High);
       for J in First (T.Elements) .. Last (T.Elements) loop
@@ -593,8 +578,6 @@ package body PolyORB.Utils.HTables.Perfect is
 
       --  Allocation of T.Subtables
 
-      pragma Debug (O ("Initializing"
-        & T.Info.N_Subtables'Img & " subtables"));
       Initialize (T.Subtables);
       Set_Last (T.Subtables, T.Info.N_Subtables - 1);
 
@@ -647,8 +630,6 @@ package body PolyORB.Utils.HTables.Perfect is
       Old_Last    : Natural;
 
    begin
-      pragma Debug (O ("Insert: @" & System.Address_Image (T'Address)
-                       & ": """ & Key & """"));
       if T.Info.Count = T.Info.High then
 
          --  Extend the table and Rehash_All,
