3f66aea0cdfa44de7fe4f984020cb284c96b1b5a
Jérôme Hugues <hugues@enst.fr>
2006-10-25 14:49:54 +0000

    Follow-up to change #10443: remplace Element_Of with Get_Element
    [Imported from Perforce change 10447 at 2006-12-01 22:55:56]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 37906

--- examples/corba/all_types/all_types-impl.adb
+++ examples/corba/all_types/all_types-impl.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2005 Free Software Foundation, Inc.           --
+--         Copyright (C) 2002-2006, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -381,7 +381,7 @@ package body all_types.Impl is
       Ada.Text_IO.Put_Line ("echoUsequence: len =" & Length (arg)'Img);
       for J in 1 .. Length (arg) loop
          Ada.Text_IO.Put_Line ("Item" & J'Img & ": " &
-           Element_Of (arg, J)'Img);
+                               Get_Element (arg, J)'Img);
       end loop;
       return arg;
    end echoUsequence;
