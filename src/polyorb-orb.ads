b0bc05183092b18460890e7c09ea10fcbfe31461
Bob Duff <duff@adacore.com>
2007-01-11 15:21:55 +0000

    Minor reformatting
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 41733

--- src/polyorb-orb.ads
+++ src/polyorb-orb.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2001-2006, Free Software Foundation, Inc.          --
+--         Copyright (C) 2001-2007, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -34,7 +34,7 @@
 --  The ORB core module: main loop and scheduler.
 --  Role: * to coordinate operation of the various subsystems.
 --        * to gateway asynchronous external events to the
---          syncrhonous messaging architecture used within PolyORB.
+--          synchronous messaging architecture used within PolyORB.
 
 with PolyORB.Annotations;
 with PolyORB.Asynch_Ev;
