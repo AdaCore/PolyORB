72a302bfb9e197f4c64091c8229ca737bd4c142e
Thomas Quinot <quinot@adacore.com>
2007-01-12 16:06:14 +0000

    Add missing box comment for function Wrap. Submitted by:	Vadim Godunko
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 41776

--- src/polyorb-sequences-helper.adb
+++ src/polyorb-sequences-helper.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2003-2006, Free Software Foundation, Inc.          --
+--         Copyright (C) 2003-2007, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -215,6 +215,10 @@ package body PolyORB.Sequences.Helper is
       end loop;
       return Result;
    end To_Any;
+
+   ----------
+   -- Wrap --
+   ----------
 
    function Wrap (X : access Sequence) return Any.Content'Class is
    begin
