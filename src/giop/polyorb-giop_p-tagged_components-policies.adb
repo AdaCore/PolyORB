49776300cfd600f6d691fde0e77c4e5da4f61081
Jérôme Hugues <hugues@enst.fr>
2007-02-06 12:33:28 +0000

    (Duplicate): do deep copy of the list of policies.
    Fix for [G107-005]
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 42355

--- src/giop/polyorb-giop_p-tagged_components-policies.adb
+++ src/giop/polyorb-giop_p-tagged_components-policies.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2003-2006, Free Software Foundation, Inc.          --
+--         Copyright (C) 2003-2007, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -65,9 +65,17 @@ package body PolyORB.GIOP_P.Tagged_Components.Policies is
 
    function Duplicate (C : TC_Policies) return Tagged_Component_Access is
       Result : constant Tagged_Component_Access := new TC_Policies;
+      Iter : Policy_Value_Seq.Iterator := Policy_Value_Seq.First (C.Policies);
+
    begin
-      TC_Policies (Result.all).Policies
-        := Policy_Value_Seq.Duplicate (C.Policies);
+      while not Policy_Value_Seq.Last (Iter) loop
+         Policy_Value_Seq.Append
+           (TC_Policies (Result.all).Policies,
+            Policy_Value'
+            (Policy_Value_Seq.Value (Iter).P_Type,
+             new Encapsulation'(Policy_Value_Seq.Value (Iter).P_Value.all)));
+         Policy_Value_Seq.Next (Iter);
+      end loop;
 
       return Result;
    end Duplicate;
