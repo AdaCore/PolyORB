01f9ccdfe575e9f84d5496fe2d34147d83950243
Thomas Quinot <quinot@adacore.com>
2007-02-07 10:22:02 +0000

    Minor reformatting.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 42379

--- src/polyorb-parameters-file.adb
+++ src/polyorb-parameters-file.adb
@@ -93,13 +93,13 @@ package body PolyORB.Parameters.File is
 
    function Get_Conf
      (Source       : access File_Source;
-      Section, Key : String)
-      return         String
+      Section, Key : String) return String
    is
       pragma Unreferenced (Source);
 
       V : constant String_Ptr :=
-         Lookup (Configuration_Table, Make_Global_Key (Section, Key), null);
+            Lookup (Configuration_Table, Make_Global_Key (Section, Key), null);
+
    begin
       if V /= null then
          return V.all;
