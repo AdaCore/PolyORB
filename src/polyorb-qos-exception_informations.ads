f3543c81c39be36cd13859a3c70079c7adbcf8f1
Thomas Quinot <quinot@adacore.com>
2007-03-01 16:02:14 +0000

    (Set_Exception_Information, Get_Exception_Information, Get_Exception_Message);  New neutral layer subprograms to set and get the AdaExceptionInformation  service context associated with a request. For G228-024
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 42894

--- src/polyorb-qos-exception_informations.ads
+++ src/polyorb-qos-exception_informations.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---           Copyright (C) 2006, Free Software Foundation, Inc.             --
+--         Copyright (C) 2006-2007, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -31,6 +31,9 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
+with Ada.Exceptions;
+
+with PolyORB.Requests;
 with PolyORB.Types;
 
 package PolyORB.QoS.Exception_Informations is
@@ -45,5 +48,22 @@ package PolyORB.QoS.Exception_Informations is
 
    type QoS_Ada_Exception_Information_Parameter_Access is
      access all QoS_Ada_Exception_Information_Parameter'Class;
+
+   procedure Set_Exception_Information
+     (Request    : PolyORB.Requests.Request_Access;
+      Occurrence : Ada.Exceptions.Exception_Occurrence);
+   --  Add additional exception information to Ada_Exception_Information reply
+   --  service context.
+
+   function Get_Exception_Information
+     (R : Requests.Request_Access) return String;
+   --  Utility function to extract the above QoS parameter from a request.
+   --  A zero-length string is returned if no such QoS parameter is present.
+
+   function Get_Exception_Message
+     (R : Requests.Request_Access) return String;
+   --  Utility function to extract just the Exception_Message part from the
+   --  Exception_Information. Like the above, returns a zero length string
+   --  if no suitable QoS parameter is available.
 
 end PolyORB.QoS.Exception_Informations;
