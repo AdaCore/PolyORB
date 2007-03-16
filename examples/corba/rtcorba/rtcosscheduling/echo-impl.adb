67bea3161ed509b3c3eb81b1e740bd790a0f2933
Bob Duff <duff@adacore.com>
2007-03-16 19:11:49 +0000

    Remove spurious blank line at end of file.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 43189

--- examples/corba/rtcorba/rtcosscheduling/echo-impl.adb
+++ examples/corba/rtcorba/rtcosscheduling/echo-impl.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---            Copyright (C) 2002 Free Software Foundation, Inc.             --
+--            Copyright (C) 2002-2007, Free Software Foundation, Inc.       --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -93,4 +93,3 @@ package body Echo.Impl is
    end EchoString;
 
 end Echo.Impl;
-
