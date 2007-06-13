7559a1f3a90936c8d689f2614cc4893060dc4247
Thomas Quinot <quinot@adacore.com>
2007-06-13 14:29:54 +0000

    Remove junk pragma Inline on Is_Initialized. This removes unnecessary dependencies of all modules upon the body of PolyORB.Initialization.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 108136

--- src/polyorb-initialization.ads
+++ src/polyorb-initialization.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2002-2006, Free Software Foundation, Inc.          --
+--         Copyright (C) 2002-2007, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -106,6 +106,4 @@ package PolyORB.Initialization is
    --  retrieve configuration values. This trick is used so PolyORB.Log
    --  and PolyORB.Initialization can be preelaborale.
 
-private
-   pragma Inline (Is_Initialized);
 end PolyORB.Initialization;
