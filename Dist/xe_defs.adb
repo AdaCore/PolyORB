db10985a25deb94fa74425d5c1bb328d0f3de237
Thomas Quinot <quinot@adacore.com>
2008-02-05 18:19:04 +0000

    Remove junk executable property.
    Subversion-branch: /trunk/GLADE
    Subversion-revision: 121638

--- Dist/xe_defs.adb
+++ Dist/xe_defs.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 1995-2005 Free Software Foundation, Inc.           --
+--         Copyright (C) 1995-2008, Free Software Foundation, Inc.          --
 --                                                                          --
 -- GNATDIST is  free software;  you  can redistribute  it and/or  modify it --
 -- under terms of the  GNU General Public License  as published by the Free --
