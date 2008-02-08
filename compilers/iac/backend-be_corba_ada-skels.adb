b727a00d4ccd513d38001b80380d804b05f9a52c
Jérôme Hugues <hugues@enst.fr>
2008-02-08 13:24:15 +0000

    Correct generation of optimized CDR marshallers, due to a regression after Ada tree reorganization
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 121739

--- compilers/iac/backend-be_corba_ada-skels.adb
+++ compilers/iac/backend-be_corba_ada-skels.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2005-2007, Free Software Foundation, Inc.          --
+--         Copyright (C) 2005-2008, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -1405,8 +1405,7 @@ package body Backend.BE_CORBA_Ada.Skels is
                Append_Node_To_List (N, L);
 
                C := Make_Selected_Component
-                 (PN (P_Dependent_Binding_Object),
-                  VN (V_Request));
+                 (VN (V_Request), PN (P_Dependent_Binding_Object));
 
                --  Request binding object
 
