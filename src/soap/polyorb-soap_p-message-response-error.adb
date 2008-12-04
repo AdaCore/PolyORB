a8657c9dcfc49790368d87951fcdd580d12187cb
Thomas Quinot <quinot@adacore.com>
2008-12-04 15:24:41 +0000

    Changes merged from trunk:   132764   132804   132807   132808   132809 for HB25-017.
    Subversion-branch: /branches/polyorb/2.5
    Subversion-revision: 133022

--- src/soap/polyorb-soap_p-message-response-error.adb
+++ src/soap/polyorb-soap_p-message-response-error.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2000-2006, Free Software Foundation, Inc.          --
+--         Copyright (C) 2000-2008, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -208,7 +208,7 @@ package body PolyORB.SOAP_P.Message.Response.Error is
                  (Message_Body,
                   "   "
                   & SOAP.Utils.Tag (P_Name, Start => True)
-                  & Types.Image (P_K)
+                  & Types.Value_Image (P_K)
                   & SOAP.Utils.Tag (P_Name, Start => False)
                   & NL);
             end;
