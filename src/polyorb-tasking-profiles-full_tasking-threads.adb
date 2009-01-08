55170e229b1f35186b41bb98e41b5e6a0625a8d8
Thomas Quinot <quinot@adacore.com>
2009-01-08 15:47:47 +0000

    (Reaper): Reduce message level to Notice so that it does not show up in fixed bugs runs. Fixes pseudo regression of 0008-303 on Windows.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 135305

--- src/polyorb-tasking-profiles-full_tasking-threads.adb
+++ src/polyorb-tasking-profiles-full_tasking-threads.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2008, Free Software Foundation, Inc.          --
+--         Copyright (C) 2002-2009, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -141,7 +141,7 @@ package body PolyORB.Tasking.Profiles.Full_Tasking.Threads is
             if Terminated_Task /= null then
                O ("Reaper: giving up on non-terminating task "
                  & Ada.Task_Identification.Image (Terminated_Task'Identity),
-                  Error);
+                  Notice);
             end if;
          or
             terminate;
