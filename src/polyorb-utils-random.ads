5ab84c3e70ec6c2bc329099576438d9c5ca68b12
Thomas Quinot <quinot@adacore.com>
2009-02-10 14:32:47 +0000

    Minor reformatting
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 139797

--- src/polyorb-utils-random.ads
+++ src/polyorb-utils-random.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2003-2007, Free Software Foundation, Inc.          --
+--         Copyright (C) 2003-2009, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -31,8 +31,8 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  A pseudo-random generator based on Makoto Matsumoto and Takuji
---  Nishimura `Mersenne Twister' random number generator MT19937.
+--  A pseudo-random generator based on Makoto Matsumoto and Takuji Nishimura
+--  `Mersenne Twister' random number generator MT19937.
 
 --  Note: we cannot depend on Ada.Numerics.Discrete_Random because of
 --  its wrong categorization. PolyORB components require a preelaborable
