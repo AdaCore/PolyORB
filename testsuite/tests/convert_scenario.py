0bae3cecafd2af45e2c6e2f403a0863a18d15b6c
Olivier Ramonat <ramonat@adacore.com>
2009-02-23 14:19:29 +0000

    Initial version of python based polyord testsuite
     - testsuite.py: the main testsuite driver
     - test_utils.py: a support modules for the test cases
     - convert_scenario.py: a script that convert a scenario configuration
       file in a tree of test-cases:
      examples/
        |-- corba-all_functions
        |   |-- ALL_FUNCTIONS_0
        |   |   `-- test.py
        |   |-- ALL_FUNCTIONS_1
        |   |   `-- test.py
        `-- corba-all_types
            |-- ALL_TYPES_0
            |   `-- test.py
            |-- ALL_TYPES_1
            |   |-- test.opt
            |   `-- test.py
    
    Part of H829-021
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 140260

---
+++ testsuite/tests/convert_scenario.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env gnatpython
+
+import os
+import re
+import sys
+
+from gnatpython.fileutils import mkdir, cd
+
+CLIENT_SERVER_TEMPLATE = """
+from test_utils import *
+import sys
+
+if not client_server(r'%(client_cmd)s',
+                     r'%(server_cmd)s'):
+    sys.exit(1)
+
+"""
+
+LOCAL_TEMPLATE = """
+from test_utils import *
+import sys
+
+if not local(r'%(command)s'):
+    sys.exit(1)
+
+"""
+
+SCENARIO_SECTION = 'scenario'
+TEST_SECTION     = 'test'
+CLIENT_SECTION   = 'client'
+SERVER_SECTION   = 'server'
+
+def parse_scenario(filename):
+    """Parse a scenario file and create the corresponding test directories"""
+    scenario = open(filename)
+
+    test_dict = {}
+    current_section = SCENARIO_SECTION
+    current_test = ""
+
+    for line in scenario:
+        if line.startswith('['):
+            test_name = re.match(r'\[(.*) (.*)\]', line)
+            if test_name:
+                current_section = test_name.group(1)
+                current_test = test_name.group(2)
+                if current_section == TEST_SECTION:
+                    test_dict[current_test] = {}
+
+        elif current_section != SCENARIO_SECTION:
+            # Do not parse scenario section.
+            line_def = re.match(r'(.*)=(.*)', line)
+            if line_def:
+                left  = line_def.group(1)
+                right = line_def.group(2)
+                if not current_section in test_dict[current_test]:
+                    test_dict[current_test][current_section] = {}
+                test_dict[current_test][current_section][left] = right
+
+    scenario.close()
+    full_name = os.path.basename(filename)
+    sep = full_name.find('-')
+
+    parent_dir = full_name[:sep]
+    mkdir(parent_dir)
+
+    scenario_dir = full_name[sep + 1:-5]
+    mkdir(os.path.join(parent_dir, scenario_dir))
+    cd(os.path.join(parent_dir, scenario_dir))
+
+    for test_name in test_dict:
+        test_type = test_dict[test_name][TEST_SECTION]['type']
+        if test_type == 'client_server':
+            mkdir(test_name)
+            f = open(os.path.join(test_name, 'test.py'), 'w')
+            f.write(CLIENT_SERVER_TEMPLATE % 
+                    {'client_cmd' :
+                     test_dict[test_name][CLIENT_SECTION]['command'],
+                     'server_cmd' :
+                     test_dict[test_name][SERVER_SECTION]['command'],
+                    })
+            f.close()
+
+            if 'expected_failure' in test_dict[test_name][TEST_SECTION]:
+                f = open(os.path.join(test_name, 'test.opt'), 'w')
+                f.write('ALL XFAIL\n')
+                f.close()
+
+        elif test_type == 'local':
+            mkdir(test_name)
+            f = open(os.path.join(test_name, 'test.py'), 'w')
+            f.write(LOCAL_TEMPLATE % 
+                    {'command' :
+                     test_dict[test_name][TEST_SECTION]['command']
+                    })
+            f.close()
+
+            if 'expected_failure' in test_dict[test_name][TEST_SECTION]:
+                f = open(os.path.join(test_name, 'test.opt'), 'w')
+                f.write('ALL XFAIL\n')
+                f.close()
+        else:
+            print 'unknown type for test: ' + test_name
+
+if __name__ == "__main__":
+    parse_scenario(sys.argv[1])
---
+++ testsuite/tests/test_utils.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env gnatpython
+
+"""test utils
+
+This module is imported by all testcase. It parse the command lines options
+and provide some usefull functions.
+
+You should never call this module directly. To run a single testcase, use
+ ./testsuite.py NAME_OF_TESTCASE
+"""
+
+from gnatpython.ex import Run, PIPE, STDOUT
+from gnatpython.main import Main
+
+import expect # this is in gnatpython only
+import os
+import re
+
+PWD = os.getcwd()
+BASE_DIR = os.path.join(PWD, os.pardir)
+
+def client_server(client_cmd, server_cmd):
+    """Run a client server testcase
+
+    Run server_cmd and extract the IOR string.
+    Run client_cmd with the server IOR string
+    Check for "END TESTS................   PASSED"
+    if found return True
+    """
+    client = os.path.join(BASE_DIR, client_cmd)
+    server = os.path.join(BASE_DIR, server_cmd)
+
+    # Run the server command and retrieve the IOR string
+    server_pid = expect.non_blocking_spawn(server, [])
+    if not server_pid:
+        print "Error when running " + server
+        return False
+
+    result = expect.expect (server_pid, [r"IOR:([a-z0-9]+)['|\n]"], 2.0)
+    if result != 0:
+        print "Expect error"
+        expect.close(server_pid)
+        return False
+
+    IOR_str = expect.expect_out (server_pid, 2)
+
+    # Run the client with the IOR argument
+    p = Run([client, IOR_str], output=PIPE, error=STDOUT,
+            timeout=options.timeout)
+
+    # Kill the server process
+    expect.close(server_pid)
+
+    if re.search(r"END TESTS.*PASSED", p.out):
+        print p.out
+        return True
+    else:
+        print p.out
+        return False
+
+def local(cmd):
+    """Run a local test
+
+    Execute the give command.
+    Check for "END TESTS................   PASSED"
+    if found return True
+    """
+    command = os.path.join(BASE_DIR, cmd)
+    p = Run([command], output=PIPE, error=STDOUT,
+            timeout=options.timeout)
+
+    if re.search(r"END TESTS.*PASSED", p.out):
+        print p.out
+        return True
+    else:
+        print p.out
+        return False
+
+def parse_cmd_line():
+    """Parse command line
+
+    Returns options object
+    """
+    main = Main(require_docstring=False)
+    main.add_option('--timeout', dest='timeout', type=int,
+                    default=None)
+    main.parse_args()
+    return main.options
+
+# Parse command lines options
+options = parse_cmd_line()
+
