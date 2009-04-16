444141148b4bd25b5b0c1ab9ae182b95dfc945a2
Thomas Quinot <quinot@adacore.com>
2009-04-16 14:38:56 +0000

    New DSA demo: simple inter-partition messaging, using RACW and Ada 2005 interfaces. Prompted by I415-015.
    ----
    date: 2009/04/16 15:27:16;  author: quinot;
    Simple test case for AI05-060 remote interfaces (H401-016)
    
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 142909

---
+++ examples/dsa/mailboxes/mailboxes.ads
@@ -0,0 +1,47 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           POLYORB COMPONENTS                             --
+--                                                                          --
+--                            M A I L B O X E S                             --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--           Copyright (C) 2009, Free Software Foundation, Inc.             --
+--                                                                          --
+-- PolyORB is free software; you  can  redistribute  it and/or modify it    --
+-- under terms of the  GNU General Public License as published by the  Free --
+-- Software Foundation;  either version 2,  or (at your option)  any  later --
+-- version. PolyORB is distributed  in the hope that it will be  useful,    --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details.  You should have received  a copy of the GNU  --
+-- General Public License distributed with PolyORB; see file COPYING. If    --
+-- not, write to the Free Software Foundation, 51 Franklin Street, Fifth    --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+--                  PolyORB is maintained by AdaCore                        --
+--                     (email: sales@adacore.com)                           --
+--                                                                          --
+------------------------------------------------------------------------------
+
+package Mailboxes is
+   pragma Remote_Types;
+
+   subtype Message_Type is String;
+
+   type Mailbox is limited interface;
+   procedure Send_Message (Recipient : access Mailbox; Message : Message_Type)
+     is abstract;
+
+   type Remote_Mailbox is access all Mailbox'Class;
+   pragma Asynchronous (Remote_Mailbox);
+   --  Remote access to mailbox
+
+end Mailboxes;
