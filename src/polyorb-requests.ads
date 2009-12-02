52f5ecf2099f97f85b678eec3c9063482ecb3825
Thomas Quinot <quinot@adacore.com>
2009-12-02 11:08:31 +0000

    (PolyORB.Requests.Request.Exception_Info): Make component aliased. For IB23-017
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 153669

--- src/polyorb-requests.ads
+++ src/polyorb-requests.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2001-2008, Free Software Foundation, Inc.          --
+--         Copyright (C) 2001-2009, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -153,9 +153,9 @@ package PolyORB.Requests is
       --  XXX This is client-side info. How do we construct it
       --      on a proxy object?
 
-      Exception_Info : Any.Any;
-      --  If non-empty, information relative to an exception
-      --  raised during execution of this request.
+      Exception_Info : aliased Any.Any;
+      --  If non-empty, information relative to an exception raised during
+      --  execution of this request.
 
       --  Ctxt_List  : CORBA.ContextList.Ref;
 
