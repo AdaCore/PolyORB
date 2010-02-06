4a489026c881b3cafa4d16762293b41f91c263aa
Arnaud Charlet <charlet@adacore.com>
2010-02-06 14:24:18 +0000

    Fix casing
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 156234

--- src/polyorb-services-naming-tools.adb
+++ src/polyorb-services-naming-tools.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2006, Free Software Foundation, Inc.          --
+--         Copyright (C) 2002-2010, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -235,9 +235,9 @@ package body PolyORB.Services.Naming.Tools is
                end if;
                Append
                  (Result, NameComponent'
-                  (Id   => To_PolyORB_String
+                  (id   => To_PolyORB_String
                    (Unescaped (First .. Last_Unescaped_Period - 1)),
-                   Kind => To_PolyORB_String
+                   kind => To_PolyORB_String
                    (Unescaped (Last_Unescaped_Period + 1 .. Last))));
                Last_Unescaped_Period := Last;
                First := Last + 1;
