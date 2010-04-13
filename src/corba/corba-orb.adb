f39116dbd4dad9f5313fd6cd6e290f3780c337a9
Bob Duff <duff@adacore.com>
2010-04-13 16:42:43 +0000

    Minor: Write CORBA.String instead of plain String, to emphasize this is not Standard.String.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 158840

--- src/corba/corba-orb.adb
+++ src/corba/corba-orb.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2001-2009, Free Software Foundation, Inc.          --
+--         Copyright (C) 2001-2010, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -69,7 +69,7 @@ package body CORBA.ORB is
 
    procedure Register_Initial_Reference
      (Identifier : ObjectId;
-      IOR        : String);
+      IOR        : CORBA.String);
    --  Register an initial reference from an IOR given
    --  through the configuration subsystem.
 
@@ -506,7 +506,7 @@ package body CORBA.ORB is
 
    procedure Register_Initial_Reference
      (Identifier : ObjectId;
-      IOR        : String)
+      IOR        : CORBA.String)
    is
       Ref : CORBA.Object.Ref;
    begin
