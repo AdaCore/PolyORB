29aff4bc0742041861da98bb1b655a5f24cc0d38
Thomas Quinot <quinot@adacore.com>
2011-01-04 17:32:35 +0000

    There is no local mode in the dynamic all_types client.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 168750

--- testsuite/tests/examples/corba-all_types/ALL_TYPES_3/test.py
+++ testsuite/tests/examples/corba-all_types/ALL_TYPES_3/test.py
@@ -2,16 +2,10 @@
 from test_utils import *
 import sys
 
-r1, r2 = (
-    client_server(
+if not client_server(
         r'../examples/corba/all_types/dynclient',
         r'soap.conf',
         r'../examples/corba/all_types/server',
-        r'soap.conf'),
-    local(
-        r'../examples/corba/all_types/dynclient',
-        r'soap.conf',
-        args=['local']))
-if not r1 or not r2:
+        r'soap.conf'):
     fail()
 
