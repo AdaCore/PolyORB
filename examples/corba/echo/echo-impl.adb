09f6085dee9487cfe5fd817ba48012694e031f8e
Thomas Quinot <quinot@adacore.com>
2011-07-04 17:39:09 +0000

    Support for variable message length and variable calls count.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 175838

--- examples/corba/echo/echo-impl.adb
+++ examples/corba/echo/echo-impl.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2008, Free Software Foundation, Inc.          --
+--         Copyright (C) 2002-2011, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -16,8 +16,8 @@
 -- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
 -- License  for more details.  You should have received  a copy of the GNU  --
 -- General Public License distributed with PolyORB; see file COPYING. If    --
--- not, write to the Free Software Foundation, 59 Temple Place - Suite 330, --
--- Boston, MA 02111-1307, USA.                                              --
+-- not, write to the Free Software Foundation, 51 Franklin Street, Fifth    --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
 --                                                                          --
 -- As a special exception,  if other files  instantiate  generics from this --
 -- unit, or you link  this unit with other files  to produce an executable, --
@@ -26,8 +26,8 @@
 -- however invalidate  any other reasons why  the executable file  might be --
 -- covered by the  GNU Public License.                                      --
 --                                                                          --
---                PolyORB is maintained by ACT Europe.                      --
---                    (email: sales@act-europe.fr)                          --
+--                  PolyORB is maintained by AdaCore                        --
+--                     (email: sales@adacore.com)                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 
@@ -51,10 +51,16 @@ package body Echo.Impl is
       pragma Warnings (Off);
       pragma Unreferenced (Self);
       pragma Warnings (On);
+
+      S : String := CORBA.To_Standard_String (Mesg);
+      L : Natural := S'Last;
    begin
+      if S'Length > 13 then
+         L := S'First + 12;
+         S (L - 2 .. L) := (others => '.');
+      end if;
       Ada.Text_IO.Put_Line
-        ("Echoing string: « " & CORBA.To_Standard_String (Mesg)
-         & " »");
+        ("Echoing string: « " & S (S'First .. L) & " »");
       return Mesg;
    end EchoString;
 
