f9dc75db7d3193e910cea353425d6579d9acdc6e
Thomas Quinot <quinot@adacore.com>
2011-07-04 13:40:28 +0000

    Minor reformatting
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 175778

--- src/polyorb-asynch_ev-sockets.adb
+++ src/polyorb-asynch_ev-sockets.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2001-2010, Free Software Foundation, Inc.          --
+--         Copyright (C) 2001-2011, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -141,6 +141,7 @@ package body PolyORB.Asynch_Ev.Sockets is
                      Socket_Event_Source (AES.all).Socket)
       then
          Success := False;
+
       else
          Clear (AEM.Monitored_Set, S_AES.Socket);
          Source_Lists.Remove_Element (AEM.Sources, S_AES'Access);
