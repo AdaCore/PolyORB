8bd297c61b85fd970fc829cd70b0dc0c2fd65f36
Thomas Quinot <quinot@adacore.com>
2011-08-24 10:58:57 +0000

    Minor cleanup
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 177618

--- src/polyorb-log-stderr.adb
+++ src/polyorb-log-stderr.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2004-2010, Free Software Foundation, Inc.          --
+--         Copyright (C) 2004-2011, Free Software Foundation, Inc.          --
 --                                                                          --
 -- PolyORB is free software; you  can  redistribute  it and/or modify it    --
 -- under terms of the  GNU General Public License as published by the  Free --
@@ -138,8 +138,6 @@ package body PolyORB.Log.Stderr is
          P := P + 1;
 
          if R = -1 then
-            --  Operation was failed.
-
             return Failed;
          end if;
 
