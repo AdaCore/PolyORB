8536297911eb8ee8f46b2db2c2707bc3f7419f98
Thomas Quinot <quinot@adacore.com>
2012-01-03 10:33:44 +0000

    Use shutil.copy (not copyfile) as we specify only a target directory.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 183008

--- testsuite/tests/examples/moma/MOMA_1/test.py
+++ testsuite/tests/examples/moma/MOMA_1/test.py
@@ -1,10 +1,10 @@
 from test_utils import *
 import os, shutil, sys
 
-shutil.copyfile(os.path.join(SRC_DIR,
-                             '..',
-                             'examples', 'moma', 'destinations.conf'),
-                '.')
+shutil.copy(os.path.join(SRC_DIR,
+                         '..',
+                         'examples', 'moma', 'destinations.conf'),
+            '.')
 
 if not client_server(r'../examples/moma/client_call_back', r'',
                      r'../examples/moma/server', r''):
