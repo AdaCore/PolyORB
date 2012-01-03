bcdf1cf779fa43e300ea9c6204f2d34c9e062d37
Thomas Quinot <quinot@adacore.com>
2012-01-03 10:32:27 +0000

    Copy configuration file from example source dir to test dir.
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 183007

--- testsuite/tests/examples/moma/MOMA_0/test.py
+++ testsuite/tests/examples/moma/MOMA_0/test.py
@@ -1,6 +1,10 @@
-
 from test_utils import *
-import sys
+import os, shutil, sys
+
+shutil.copyfile(os.path.join(SRC_DIR,
+                             '..',
+                             'examples', 'moma', 'destinations.conf'),
+                '.')
 
 if not client_server(r'../examples/moma/client', r'',
                      r'../examples/moma/server', r''):
