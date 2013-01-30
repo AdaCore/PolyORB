c5c65f2a5a3bd9a4491e43fc6a2058c53d7bcc44
Thomas Quinot <quinot@adacore.com>
2013-01-30 11:41:48 +0000

    Minor reformatting
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 201776

--- src/polyorb-binding_data.ads
+++ src/polyorb-binding_data.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2001-2012, Free Software Foundation, Inc.          --
+--         Copyright (C) 2001-2013, Free Software Foundation, Inc.          --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -125,7 +125,7 @@ package PolyORB.Binding_Data is
    type Profile_Factory_Access is access all Profile_Factory'Class;
 
    procedure Create_Factory
-     (PF : out Profile_Factory;
+     (PF  : out Profile_Factory;
       TAP : Transport.Transport_Access_Point_Access;
       ORB : Components.Component_Access)
       is abstract;
