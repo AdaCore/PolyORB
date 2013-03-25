19edbad322945c6ef20e2a2c152ae180d1a9064b
Thomas Quinot <quinot@adacore.com>
2013-03-25 15:17:05 +0000

    Minor reformatting
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 204706

--- src/polyorb-servants.adb
+++ src/polyorb-servants.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2012, Free Software Foundation, Inc.          --
+--         Copyright (C) 2002-2013, Free Software Foundation, Inc.          --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -62,7 +62,7 @@ package body PolyORB.Servants is
       --  the RR.Req.Upcall_Abortable_Mutex to clean RR.Req.Upcall_Abortable
       --  (see Abortable_Execute_Servant).
 
-      RR.Aborted   := False;
+      RR.Aborted := False;
    end Run;
 
    -------------------------------
