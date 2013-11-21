4849efeee7add41f25a88c3bfd9af61ce2c51df1
Bob Duff <duff@adacore.com>
2013-11-21 20:05:35 +0000

    Minor reformatting: put 'then' on separate line. MB20-030
    Subversion-branch: /trunk/polyorb
    Subversion-revision: 219721

--- testsuite/core/dynamic_dict/test000.adb
+++ testsuite/core/dynamic_dict/test000.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2012, Free Software Foundation, Inc.          --
+--         Copyright (C) 2002-2013, Free Software Foundation, Inc.          --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -84,8 +84,8 @@ procedure Test000 is
             Result := My_Dict.Lookup (Values (K).all, Default => null);
 
             if Result = null
-              or else Result.all /= "foo" then
-
+              or else Result.all /= "foo"
+            then
                Output ("Regression occured for key "
                        & Values (K).all
                        & " at stage #"
