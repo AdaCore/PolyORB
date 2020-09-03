db95f4e98719ee44c1ebda0a0c101fe50653a4e7
Vadim Godunko <godunko@adacore.com>
2020-09-03 08:04:37 +0300

    T825-011 Reflect changes in random generator API.
    Change-Id: I63caa9c766306c8dbe4d34dfcbc851d3fea3154e

--- testsuite/core/random/test000.adb
+++ testsuite/core/random/test000.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2003-2012, Free Software Foundation, Inc.          --
+--         Copyright (C) 2003-2020, Free Software Foundation, Inc.          --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -42,15 +42,15 @@ procedure Test000 is
    use PolyORB.Utils.Random;
    use PolyORB.Utils.Report;
 
-   G : aliased PolyORB.Utils.Random.Generator;
+   G : PolyORB.Utils.Random.Generator;
 
 begin
    New_Test ("Pseudo-random generator");
 
-   Reset (G'Access, 42);
+   Reset (G, 42);
 
    for J in 1 .. 10 loop
-      Put_Line (Random (G'Access)'Img);
+      Put_Line (Random (G)'Img);
    end loop;
 
    End_Report;
