6d1d7f2619e6f0cbbd402c45d4ba5798d64ba9eb
Arnaud Charlet <charlet@adacore.com>
2020-12-16 17:33:12 +0100

    Address new GNAT warning from S911-017 #3
    Change-Id: I76df3f0a15fed071251f1d622021ac0f574dc738

--- cos/naming/cosnaming-bindingiterator-impl.ads
+++ cos/naming/cosnaming-bindingiterator-impl.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2001-2012, Free Software Foundation, Inc.          --
+--         Copyright (C) 2001-2020, Free Software Foundation, Inc.          --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -37,7 +37,7 @@ with PolyORB.Tasking.Mutexes;
 
 package CosNaming.BindingIterator.Impl is
 
-   type Object;
+   type Object is tagged;
    type Object_Ptr is access all Object'Class;
 
    package Bindings renames IDL_SEQUENCE_CosNaming_Binding;
