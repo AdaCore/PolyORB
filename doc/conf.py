637f02e303b1b0fbe5575bc224c65fe49ace7823
Joel Brobecker <brobecker@adacore.com>
2022-11-25 19:24:33 +0400

    switch the documentation's them to the RTD them
    This commit adjust the sphinx configuration to use the "Read The Docs"
    theme, which has the advantage of allowing the navigation bar
    (containing among other things a search bar, and the TOC) to stay
    fixed while scrolling the contents of the page being read. This is
    particularly useful to allow access to those features while reading
    a long page, for instance.
    
    TN: VB25-026
    Change-Id: Ibfebbf1d66ed34e72c4f17a90f0272ed7af787b7

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ import polyorb_version
 # ones.
 # Temporarily disable bibtex, plugin not installed yet
 # extensions = ['sphinxcontrib.bibtex']
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -104,7 +104,7 @@ pygments_style = None
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
