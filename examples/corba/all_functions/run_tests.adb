93bd14c5ce4fc901834490450fa5b9654b651645
Vadim Godunko <godunko@adacore.com>
2023-11-15 07:57:02 +0000

    Add subprogram's specification to MANIFEST.
    Enable style checks.

--- examples/corba/all_functions/run_tests.adb
+++ examples/corba/all_functions/run_tests.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 2002-2012, Free Software Foundation, Inc.          --
+--         Copyright (C) 2002-2023, Free Software Foundation, Inc.          --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -31,9 +31,7 @@ with all_functions; use all_functions;
 with PolyORB.Utils.Report;
 
 --  Remove no previous spec warning
-pragma Style_Checks (Off);
 procedure Run_Tests (MyObj : all_functions.Ref) is
-   pragma Style_Checks (On);
    use PolyORB.Utils.Report;
    I, J, K, L, M : CORBA.Short;
    Ok : Boolean;
