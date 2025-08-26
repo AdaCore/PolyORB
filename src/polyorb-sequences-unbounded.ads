a1e90546ebc2f7ce8f56be8a33c9c44e11b47685
Vadim Godunko <godunko@adacore.com>
2025-08-26 15:42:38 +0400

    Remove unused code.

--- src/polyorb-sequences-unbounded.ads
+++ src/polyorb-sequences-unbounded.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---         Copyright (C) 2002-2012, Free Software Foundation, Inc.          --
+--         Copyright (C) 2002-2025, Free Software Foundation, Inc.          --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -250,8 +250,6 @@ package PolyORB.Sequences.Unbounded is
 private
 
    pragma Inline (Null_Sequence);
-
-   Prealloc_Length : constant := 5;
 
    type Sequence is new Ada.Finalization.Controlled with record
       Length  : Natural;
