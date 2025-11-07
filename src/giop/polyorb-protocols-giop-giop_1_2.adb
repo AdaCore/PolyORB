bca65606babc727ba5bb89feadc4b316ea7d9882
Code Architect <code_architect@refactorteam.local>
2025-11-07 01:33:26 -0800

    fix(phase1a): Break long lines in polyorb-protocols-giop-giop_1_2.adb
    - Lines 93, 95: Break long lines into 3 lines each
    
    Progress: 4/20 files fixed (20% complete)

--- src/giop/polyorb-protocols-giop-giop_1_2.adb
+++ src/giop/polyorb-protocols-giop-giop_1_2.adb
@@ -90,9 +90,13 @@ package body PolyORB.Protocols.GIOP.GIOP_1_2 is
    Permitted_Sync_Scopes : constant PolyORB.Requests.Flags :=
      Sync_None or Sync_With_Transport or Sync_With_Server or Sync_With_Target;
 
-   procedure Free is new PolyORB.Utils.Unchecked_Deallocation.Free (Object => GIOP_1_2_CDR_Representation, Name => GIOP_1_2_CDR_Representation_Access);
+   procedure Free is new PolyORB.Utils.Unchecked_Deallocation.Free
+     (Object => GIOP_1_2_CDR_Representation,
+      Name => GIOP_1_2_CDR_Representation_Access);
 
-   procedure Free is new PolyORB.Utils.Unchecked_Deallocation.Free (Object => Target_Address, Name => Target_Address_Access);
+   procedure Free is new PolyORB.Utils.Unchecked_Deallocation.Free
+     (Object => Target_Address,
+      Name => Target_Address_Access);
 
    --  Msg_Type
 
